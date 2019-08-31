4c4e659136721fd1021a88dcda86cfc27eaeec8f
piotrkowandy <piotr.kowandy@o2.pl>
2019-08-31 23:48:10 +0200

    reset test added

--- smartPtr/tests/smartPtrTests.cpp
+++ smartPtr/tests/smartPtrTests.cpp
@@ -1,8 +1,32 @@
 #include <gtest/gtest.h>
 #include "smartPtr.hpp"
 
+class smartPointerTests : public ::testing::Test
+{
+public:
+    smartPointerTests()
+    {
+    }
+    ~smartPointerTests()
+    {
+    }
+private:
+};
+
 TEST(smartPtrTests, canConstructSmartPtr)
 {
     smartPointer<int> smartPtr(new int(11));
+}
+
+TEST_F(smartPointerTests, resetPtr1)
+{
+    smartPointer<int> smPtr1(new int(1));
+    smPtr1.reset();
+}
+
+TEST_F(smartPointerTests, resetPtr2)
+{
+    smartPointer<int> smPtr1(new int(1));
+    smPtr1.reset(new int(2));
 }
 
