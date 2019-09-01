6e0d7bf9c07d1d9b60417819911c9ffb7aad7169
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-09-01 20:14:38 +0200

    Refactoring of /tests/smartPtrTests.cpp

--- smartPtr/tests/smartPtrTests.cpp
+++ smartPtr/tests/smartPtrTests.cpp
@@ -5,47 +5,86 @@
 
 class smartPointerTests : public ::testing::Test {
 public:
-    smartPointerTests()
+    smartPointerTests() :
+        bool1(true),
+        char1('a'), 
+        int1(11), 
+        flt1(1.41421356237), 
+        dbl1(3.14159265359), 
+        str1("Lorem ipsum dolor sit amet"), 
+        AInt1(17),
+        A1(17),
+        
+        bool2(false), 
+        char2('b'), 
+        int2(13), 
+        flt2(1.73205080757),
+        dbl2(2.718281828459), 
+        str2("Consectetur adipiscing elit"),
+        AInt2(29),
+        A2(29),
+        
+        sPtrBool1(new bool(true)),
+        sPtrChar1(new char('a')),
+        sPtrInt1(new int(11)),
+        sPtrFloat1(new float(1.41421356237)),
+        sPtrDouble1(new double(3.14159265359)),
+        sPtrString1(new std::string("Lorem ipsum dolor sit amet")),
+        sPtrA1(new A(17)),
+        
+        sPtrBool2(new bool(false)),
+        sPtrChar2(new char('b')),
+        sPtrInt2(new int(13)),
+        sPtrFloat2(new float(1.73205080757)),
+        sPtrDouble2(new double(2.718281828459)),
+        sPtrString2(new std::string("Consectetur adipiscing elit")),
+        sPtrA2(new A(29)) 
     {
     }
+    
     ~smartPointerTests()
     {
     }
-    void SetUp()
-    {    
-    }
-    void TearDown()
-    {  
-    }
-
-    bool bool1 = true;
-    bool bool2 = false;
-    char char1 = 'a';
-    char char2 = 'b';
-    int int1 = 11;
-    int int2 = 13;
-    float flt1 = 1.41421356237;
-    float flt2 = 1.73205080757;
-    double dbl1 = 3.14159265359;
-    double dbl2 = 2.718281828459;
-    std::string str1 = "Lorem ipsum dolor sit amet";
-    std::string str2 = "Consectetur adipiscing elit";
-    int AInt1 = 17;
-    int AInt2 = 29;
+   
+    bool bool1;
+    char char1;
+    int int1;
+    float flt1;
+    double dbl1;
+    std::string str1;
+    int AInt1;
+    A A1;
+    
+    bool bool2;
+    char char2;
+    int int2;
+    float flt2;
+    double dbl2;
+    std::string str2;
+    int AInt2;
+    A A2;
+    
+    smartPointer<bool> sPtrBool1;
+    smartPointer<char> sPtrChar1;
+    smartPointer<int> sPtrInt1;
+    smartPointer<float> sPtrFloat1;
+    smartPointer<double> sPtrDouble1;
+    smartPointer<std::string> sPtrString1;
+    smartPointer<A> sPtrA1;
+    
+    smartPointer<bool> sPtrBool2;
+    smartPointer<char> sPtrChar2;
+    smartPointer<int> sPtrInt2;
+    smartPointer<float> sPtrFloat2;
+    smartPointer<double> sPtrDouble2;
+    smartPointer<std::string> sPtrString2;
+    smartPointer<A> sPtrA2;      
 };
 
 TEST_F(smartPointerTests, canProperlyConstructAndDereferenceSmartPtr)
 {
-    // ARRANGE
-    // ACT
-    smartPointer<bool> sPtrBool1(new bool(bool1));
-    smartPointer<char> sPtrChar1(new char(char1));
-    smartPointer<int> sPtrInt1(new int(int1));
-    smartPointer<float> sPtrFloat1(new float(flt1));
-    smartPointer<double> sPtrDouble1(new double(dbl1));
-    smartPointer<std::string> sPtrString1(new std::string(str1));
-    smartPointer<A> sPtrA1(new A(AInt1));
-    
+    // ARRANGE (done in initialization list) 
+    // ACT (done in initialization list) 
     // ASSERT
     ASSERT_TRUE(*sPtrBool1 == bool1);
     ASSERT_TRUE(*sPtrChar1 == char1);
@@ -58,46 +97,29 @@ TEST_F(smartPointerTests, canProperlyConstructAndDereferenceSmartPtr)
 
 TEST_F(smartPointerTests, canConstructSmartPtrWithMoveConstructor)
 {
-    // ARRANGE
-    smartPointer<bool> sPtrBool2(new bool(bool2));
-    smartPointer<char> sPtrChar2(new char(char2));
-    smartPointer<int> sPtrInt2(new int(int2));
-    smartPointer<float> sPtrFloat2(new float(flt2));
-    smartPointer<double> sPtrDouble2(new double(dbl2));
-    smartPointer<std::string> sPtrString2(new std::string(str2));
-    smartPointer<A> sPtrA2(new A(AInt2));  
-
+    // ARRANGE (done in initialization list) 
     // ACT
-    smartPointer<bool> sPtrBool1(std::move(sPtrBool2));
-    smartPointer<char> sPtrChar1(std::move(sPtrChar2));
-    smartPointer<int> sPtrInt1(std::move(sPtrInt2));
-    smartPointer<float> sPtrFloat1(std::move(sPtrFloat2));
-    smartPointer<double> sPtrDouble1(std::move(sPtrDouble2));
-    smartPointer<std::string> sPtrString1(std::move(sPtrString2));
-    smartPointer<A> sPtrA1(std::move(sPtrA2));
+    smartPointer<bool> sPtrBool3(std::move(sPtrBool2));
+    smartPointer<char> sPtrChar3(std::move(sPtrChar2));
+    smartPointer<int> sPtrInt3(std::move(sPtrInt2));
+    smartPointer<float> sPtrFloat3(std::move(sPtrFloat2));
+    smartPointer<double> sPtrDouble3(std::move(sPtrDouble2));
+    smartPointer<std::string> sPtrString3(std::move(sPtrString2));
+    smartPointer<A> sPtrA3(std::move(sPtrA2));
 
     // ASSERT
-    ASSERT_TRUE(*sPtrBool1 == bool2);
-    ASSERT_TRUE(*sPtrChar1 == char2);
-    ASSERT_TRUE(*sPtrInt1 == int2);
-    ASSERT_TRUE(*sPtrFloat1 == flt2);
-    ASSERT_TRUE(*sPtrDouble1 == dbl2);
-    ASSERT_TRUE(*sPtrString1 == str2);
-    ASSERT_TRUE(*sPtrA1 == A(AInt2));
+    ASSERT_TRUE(*sPtrBool3 == bool2);
+    ASSERT_TRUE(*sPtrChar3 == char2);
+    ASSERT_TRUE(*sPtrInt3 == int2);
+    ASSERT_TRUE(*sPtrFloat3 == flt2);
+    ASSERT_TRUE(*sPtrDouble3 == dbl2);
+    ASSERT_TRUE(*sPtrString3 == str2);
+    ASSERT_TRUE(*sPtrA3 == A(AInt2));
 }
 
 TEST_F(smartPointerTests, canChangeSmartPtrValueWithDereferenceOperator)
 {
-    // ARRANGE
-    smartPointer<bool> sPtrBool1(new bool(bool1));
-    smartPointer<char> sPtrChar1(new char(char1));
-    smartPointer<int> sPtrInt1(new int(int1));
-    smartPointer<float> sPtrFloat1(new float(flt1));
-    smartPointer<double> sPtrDouble1(new double(dbl1));
-    smartPointer<std::string> sPtrString1(new std::string(str1));
-    smartPointer<A> sPtrA1(new A(AInt1));
-    A A2(23);
-
+    // ARRANGE (done in initialization list)
     // ACT
     *sPtrBool1 = bool2;
     *sPtrChar1 = char2;
@@ -123,12 +145,12 @@ TEST_F(smartPointerTests, canUseArrowOperatorOnUnderlyingClassInstance)
     const int input1 = 31;
     const int input2 = 37;
     int output1, output2;
-    smartPointer<A> sPtrA1(new A(input1));
-    smartPointer<A> sPtrA2(new A(input2));
+    smartPointer<A> APointer1 (new A(input1));
+    smartPointer<A> APointer2 (new A(input2));
 
     // ACT
-    output1 = sPtrA1->bar();
-    output2 = sPtrA2->bar();
+    output1 = APointer1->bar();
+    output2 = APointer2->bar();
 
     // ASSERT:
     ASSERT_TRUE(2*input1==output1);
@@ -137,14 +159,7 @@ TEST_F(smartPointerTests, canUseArrowOperatorOnUnderlyingClassInstance)
 
 TEST_F(smartPointerTests, checkIfGetMethodReturnsUnderlyingPointer)
 {
-    // ARRANGE
-    smartPointer<bool> sPtrBool1(new bool(bool1));
-    smartPointer<char> sPtrChar1(new char(char1));
-    smartPointer<int> sPtrInt1(new int(int1));
-    smartPointer<float> sPtrFloat1(new float(flt1));
-    smartPointer<double> sPtrDouble1(new double(dbl1));
-    smartPointer<std::string> sPtrString1(new std::string(str1));
-    smartPointer<A> sPtrA1(new A(AInt1));
+    // ARRANGE (partially done in initialization list)
     bool* rPtrBool1 = nullptr;
     char* rPtrChar1 = nullptr;
     int* rPtrInt1 = nullptr;
@@ -163,13 +178,6 @@ TEST_F(smartPointerTests, checkIfGetMethodReturnsUnderlyingPointer)
     rPtrA1 = sPtrA1.get();
 
     // ASSERT
-    ASSERT_FALSE(nullptr==rPtrBool1);
-    ASSERT_FALSE(nullptr==rPtrChar1);
-    ASSERT_FALSE(nullptr==rPtrInt1);
-    ASSERT_FALSE(nullptr==rPtrFloat1);
-    ASSERT_FALSE(nullptr==rPtrDouble1);
-    ASSERT_FALSE(nullptr==rPtrString1);
-    ASSERT_FALSE(nullptr==rPtrA1);
     ASSERT_TRUE(rPtrBool1==sPtrBool1.get());
     ASSERT_TRUE(rPtrChar1==sPtrChar1.get());
     ASSERT_TRUE(rPtrInt1==sPtrInt1.get());
@@ -181,21 +189,14 @@ TEST_F(smartPointerTests, checkIfGetMethodReturnsUnderlyingPointer)
 
 TEST_F(smartPointerTests, boolOperatorTest)
 {
-    // ARRANGE
-    smartPointer<bool> sPtrBool2(new bool(bool2));
-    smartPointer<char> sPtrChar2(new char(char2));
-    smartPointer<int> sPtrInt2(new int(int2));
-    smartPointer<float> sPtrFloat2(new float(flt2));
-    smartPointer<double> sPtrDouble2(new double(dbl2));
-    smartPointer<std::string> sPtrString2(new std::string(str2));
-    smartPointer<A> sPtrA2(new A(AInt2));  
-    smartPointer<bool> sPtrBool1(std::move(sPtrBool2));
-    smartPointer<char> sPtrChar1(std::move(sPtrChar2));
-    smartPointer<int> sPtrInt1(std::move(sPtrInt2));
-    smartPointer<float> sPtrFloat1(std::move(sPtrFloat2));
-    smartPointer<double> sPtrDouble1(std::move(sPtrDouble2));
-    smartPointer<std::string> sPtrString1(std::move(sPtrString2));
-    smartPointer<A> sPtrA1(std::move(sPtrA2));
+    // ARRANGE (partially done in initialization list)
+    smartPointer<bool> sPtrBool3(std::move(sPtrBool1));
+    smartPointer<char> sPtrChar3(std::move(sPtrChar1));
+    smartPointer<int> sPtrInt3(std::move(sPtrInt1));
+    smartPointer<float> sPtrFloat3(std::move(sPtrFloat1));
+    smartPointer<double> sPtrDouble3(std::move(sPtrDouble1));
+    smartPointer<std::string> sPtrString3(std::move(sPtrString1));
+    smartPointer<A> sPtrA3(std::move(sPtrA1));
     
     // ACT
     bool bBool1 = sPtrBool1;
@@ -205,42 +206,34 @@ TEST_F(smartPointerTests, boolOperatorTest)
     bool bDouble1 = sPtrDouble1;
     bool bString1 = sPtrString1;
     bool bA1 = sPtrA1;
-    bool bBool2 = sPtrBool2;
-    bool bChar2 = sPtrChar2;
-    bool bInt2 = sPtrInt2;
-    bool bFloat2 = sPtrFloat2;
-    bool bDouble2 = sPtrDouble2;
-    bool bString2 = sPtrString2;
-    bool bA2 = sPtrA2;
+    bool bBool3 = sPtrBool3;
+    bool bChar3 = sPtrChar3;
+    bool bInt3 = sPtrInt3;
+    bool bFloat3 = sPtrFloat3;
+    bool bDouble3 = sPtrDouble3;
+    bool bString3 = sPtrString3;
+    bool bA3 = sPtrA3;
 
     // ASSERT
-    ASSERT_TRUE(bBool1);
-    ASSERT_TRUE(bChar1);
-    ASSERT_TRUE(bInt1);
-    ASSERT_TRUE(bFloat1);
-    ASSERT_TRUE(bDouble1);
-    ASSERT_TRUE(bString1);
-    ASSERT_TRUE(bA1);
-    ASSERT_FALSE(bBool2);
-    ASSERT_FALSE(bChar2);
-    ASSERT_FALSE(bInt2);
-    ASSERT_FALSE(bFloat2);
-    ASSERT_FALSE(bDouble2);
-    ASSERT_FALSE(bString2);
-    ASSERT_FALSE(bA2);        
+    ASSERT_TRUE(bBool3);
+    ASSERT_TRUE(bChar3);
+    ASSERT_TRUE(bInt3);
+    ASSERT_TRUE(bFloat3);
+    ASSERT_TRUE(bDouble3);
+    ASSERT_TRUE(bString3);
+    ASSERT_TRUE(bA3);
+    ASSERT_FALSE(bBool1);
+    ASSERT_FALSE(bChar1);
+    ASSERT_FALSE(bInt1);
+    ASSERT_FALSE(bFloat1);
+    ASSERT_FALSE(bDouble1);
+    ASSERT_FALSE(bString1);
+    ASSERT_FALSE(bA1);        
 }
 
 TEST_F(smartPointerTests, checkIfReleaseSetsUnderlyingPointerNullptr)
 { 
-    // ARRANGE
-    smartPointer<bool> sPtrBool1(new bool(bool1));
-    smartPointer<char> sPtrChar1(new char(char1));
-    smartPointer<int> sPtrInt1(new int(int1));
-    smartPointer<float> sPtrFloat1(new float(flt1));
-    smartPointer<double> sPtrDouble1(new double(dbl1));
-    smartPointer<std::string> sPtrString1(new std::string(str1));
-    smartPointer<A> sPtrA1(new A(AInt1));
-
+    // ARRANGE (done in initialization list)
     // ACT
     sPtrBool1.release();
     sPtrChar1.release();
@@ -262,15 +255,7 @@ TEST_F(smartPointerTests, checkIfReleaseSetsUnderlyingPointerNullptr)
 
 TEST_F(smartPointerTests, checkIfResetReplacesManagedObject)
 {
-    // ARRANGE
-    smartPointer<bool> sPtrBool1(new bool(bool1));
-    smartPointer<char> sPtrChar1(new char(char1));
-    smartPointer<int> sPtrInt1(new int(int1));
-    smartPointer<float> sPtrFloat1(new float(flt1));
-    smartPointer<double> sPtrDouble1(new double(dbl1));
-    smartPointer<std::string> sPtrString1(new std::string(str1));
-    smartPointer<A> sPtrA1(new A(AInt1));
-    
+    // ARRANGE (done in initialization list)
     // ACT
     sPtrBool1.reset(new bool(bool2));
     sPtrChar1.reset(new char(char2));
