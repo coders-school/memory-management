a4baad086a8b0acd47aa9e84c14a743a643a4017
majkel84 <michal.sobotka.84@gmail.com>
2020-09-13 22:12:33 +0200

    unique_ptr - template class, constructor

--- homework/unique_ptr/unique_ptr.hpp
+++ homework/unique_ptr/unique_ptr.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+template <typename T>
 class unique_ptr {
-
+public:
+    unique_ptr(T* ptr);
+private:
+    T* ptr_;
 };
+
+template <typename T>
+unique_ptr<T>::unique_ptr(T* ptr)
+    : ptr_(ptr) {}
