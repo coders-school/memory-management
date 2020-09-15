9cc141553fcd7212d9ce0d6349b6861291e3aea8
Kuba C <jakub.cieplinski@outlook.com>
2020-09-15 12:52:33 +0200

    replaced tsets with fixture tests

--- homework/unique_ptr/test.cpp
+++ homework/unique_ptr/test.cpp
@@ -2,45 +2,48 @@
 #include "gtest/gtest.h"
 #include "unique_ptr.hpp"
 
-TEST(PointerTest, ShouldProperlyDereference) {
-    cs::unique_ptr<std::string> testPointer(new std::string{"Ala ma kota"});
-    auto stringToCompare = *testPointer;
+const std::string testString{"Ala ma kota"};
+
+struct TestingPtr : ::testing::Test {
+    TestingPtr()
+        : ptr(new std::string("Ala ma kota")) {
+    }
+
+    cs::unique_ptr<std::string> ptr;
+};
+
+TEST_F(TestingPtr, ShouldProperlyDereference) {
     std::string testString{"Ala ma kota"};
 
-    ASSERT_EQ(stringToCompare, *testPointer);
+    ASSERT_EQ(testString, *ptr);
 }
-TEST(PointerTest, ShouldProperlyReturnPointerViaGet) {
-    cs::unique_ptr<std::string> testPointer(new std::string{"Ala ma kota"});
-    auto ptr = testPointer.get();
-    ASSERT_EQ(ptr, testPointer.get());
+
+TEST_F(TestingPtr, ShouldProperlyReturnPointerViaGet) {
+    auto testPtr = ptr.get();
+    ASSERT_EQ(testPtr, ptr.get());
 }
-TEST(PointerTest, ShouldProperlyReset) {
-    cs::unique_ptr<std::string> testPointer(new std::string{"Ala ma kota"});
+
+TEST_F(TestingPtr, ShouldProperlyReset) {
     std::string valueToResetTo{"Kota ma Ale"};
-    testPointer.reset(new std::string(valueToResetTo));
+    ptr.reset(new std::string(valueToResetTo));
 
-    ASSERT_EQ(valueToResetTo, *testPointer);
-}
-TEST(PointerTest, ShouldProperlyMovePointer) {
-    cs::unique_ptr<std::string> testPointerToBeMoved(new std::string{"Ala ma kota"});
-    std::string valueMoved{"Ala ma kota"};
-    cs::unique_ptr<std::string> testPointer(std::move(testPointerToBeMoved));
-
-    ASSERT_EQ(valueMoved, *testPointer);
+    ASSERT_EQ(valueToResetTo, *ptr);
 }
 
-TEST(PointerTest, MovedPointerShouldBeNullptr) {
-    cs::unique_ptr<std::string> testPointerToBeMoved(new std::string{"Ala ma kota"});
-    cs::unique_ptr<std::string> testPointer(std::move(testPointerToBeMoved));
+TEST_F(TestingPtr, ShouldProperlyMovePointer) {
+    cs::unique_ptr<std::string> testPointer(std::move(ptr));
 
-    ASSERT_EQ(nullptr, testPointerToBeMoved.get());
+    ASSERT_EQ(testString, *testPointer);
 }
 
-TEST(PointerTest, ShouldProperlyUseArrowOperator) {
-    cs::unique_ptr<std::string> testPointer(new std::string{"Ala ma kota"});
-    std::string testString{"Ala ma kota"};
+TEST_F(TestingPtr, MovedPointerShouldBeNullptr) {
+    cs::unique_ptr<std::string> testPointer(std::move(ptr));
 
-    ASSERT_EQ(testPointer->at(1), testString.at(1));
+    ASSERT_EQ(nullptr, ptr.get());
+}
+
+TEST_F(TestingPtr, ShouldProperlyUseArrowOperator) {
+    ASSERT_EQ(ptr->at(1), testString.at(1));
 }
 
 TEST(PointerTest, DereferencingANullptrShouldThrowAnExeption) {
