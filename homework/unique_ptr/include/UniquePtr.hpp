d381036612bc0cd574b29bd11e14ccf667149be4
jakjan95 <jakubjanusz95@gmail.com>
2020-09-15 13:31:29 +0200

    UniuePtr code cleaned

--- homework/unique_ptr/include/UniquePtr.hpp
+++ homework/unique_ptr/include/UniquePtr.hpp
@@ -9,56 +9,19 @@ public:
 template <typename T>
 class UniquePtr {
 public:
-    UniquePtr(T* ptr)
-        : ptr_{ptr} {}
+    UniquePtr(T* ptr);
 
     UniquePtr(const UniquePtr<T>&) = delete;
-
-    UniquePtr(UniquePtr<T>&& ptr)
-        : ptr_{ptr.release()} {}
-
     UniquePtr<T>& operator=(const UniquePtr<T>&) = delete;
-    UniquePtr<T>& operator=(UniquePtr<T>&& ptr) {
-        if (!ptr_) {
-            return *this;
-        }
-        delete ptr_;
-        ptr_ = ptr.ptr_;
-        ptr.ptr_ = nullptr;
-        return *this;
-    }
 
-    T operator*() const {
-        if (!ptr_) {
-            throw NullptrDereferenceError();
-        }
-        return *ptr_;
-    }
+    UniquePtr(UniquePtr<T>&& ptr);
+    UniquePtr<T>& operator=(UniquePtr<T>&& ptr);
 
-    T* operator->() const {
-        return ptr_;
-    }
-
-    T* get() {
-        if (ptr_) {
-            return ptr_;
-        }
-        return nullptr;
-    }
-
-    T* release() {
-        T* ptr = ptr_;
-        delete ptr_;
-        ptr_ = nullptr;
-        return ptr;
-    }
-
-    void reset(T* ptr) {
-        if (ptr_) {
-            delete ptr_;
-        }
-        ptr_ = ptr;
-    }
+    T operator*() const;
+    T* operator->() const;
+    T* get();
+    T* release();
+    void reset(T* ptr);
 
     ~UniquePtr() {
         delete ptr_;
@@ -67,3 +30,59 @@ public:
 private:
     T* ptr_;
 };
+
+template <typename T>
+UniquePtr<T>::UniquePtr(T* ptr)
+    : ptr_{ptr} {}
+
+template <typename T>
+UniquePtr<T>::UniquePtr(UniquePtr<T>&& ptr)
+    : ptr_{ptr.release()} {}
+
+template <typename T>
+UniquePtr<T>& UniquePtr<T>::operator=(UniquePtr<T>&& ptr) {
+    if (!ptr_) {
+        return *this;
+    }
+    delete ptr_;
+    ptr_ = ptr.ptr_;
+    ptr.ptr_ = nullptr;
+    return *this;
+}
+
+template <typename T>
+T UniquePtr<T>::operator*() const {
+    if (!ptr_) {
+        throw NullptrDereferenceError();
+    }
+    return *ptr_;
+}
+
+template <typename T>
+T* UniquePtr<T>::operator->() const {
+    return ptr_;
+}
+
+template <typename T>
+T* UniquePtr<T>::get() {
+    if (ptr_) {
+        return ptr_;
+    }
+    return nullptr;
+}
+
+template <typename T>
+T* UniquePtr<T>::release() {
+    T* ptr = ptr_;
+    delete ptr_;
+    ptr_ = nullptr;
+    return ptr;
+}
+
+template <typename T>
+void UniquePtr<T>::reset(T* ptr) {
+    if (ptr_) {
+        delete ptr_;
+    }
+    ptr_ = ptr;
+}
