c643c500bbc4e7004267afd97887b36381a94ef8
Vicx95 <wiktor.zyla31@gmail.com>
2020-09-16 18:31:19 +0200

    Add unique_ptr declaration

---
+++ homework/unique_ptr/UniquePtr.hpp
@@ -0,0 +1,18 @@
+template <typename T>
+class UniquePtr {
+public:
+    UniquePtr(T* ptr);
+
+    UniquePtr(const UniquePtr<T>&) = delete;
+    UniquePtr<T>& operator=(const UniquePtr<t>) = delete;
+
+    UniquePtr(UniquePtr<T>&& ptr);
+    UniquePtr<T>& operator=(UniquePtr<T>&& ptr);
+
+    T operator*() const;
+    T* operator->() const;
+    T* get();
+    T* release();
+    void reset(T* ptr);
+    ~UniquePtr();
+};
