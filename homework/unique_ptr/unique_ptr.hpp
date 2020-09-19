ad3a11e7dfd5c72f3540242475be9d40316ff08f
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-09-19 20:38:45 +0200

    implementation of unique_ptr

---
+++ homework/unique_ptr/unique_ptr.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+namespace coders {
+
+template <typename T>
+class unique_ptr{
+public:
+    unique_ptr() = delete; //default constructor
+    unique_ptr(T* ptr); //constructor
+    unique_ptr(const T&) = delete; //copy constructor not allowed
+    unique_ptr(unique_ptr<T>&& ptr) noexcept; //move constructor
+    ~unique_ptr();
+
+    T* get() const noexcept;
+    T* release() noexcept;
+    void reset(T* ptr) noexcept;
+
+    unique_ptr<T>& operator=(unique_ptr<T>& ptr) = delete;
+    unique_ptr<T>& operator=(unique_ptr<T>&& ptr) noexcept;
+    T& operator*() const;
+    T* operator->() const noexcept;
+
+
+private:
+    T* rawPtr_{nullptr};
+};
+
+template <typename T>
+unique_ptr<T>::unique_ptr(T* ptr) {
+    rawPtr_ = ptr;
+}
+
+template <typename T>
+unique_ptr<T>::unique_ptr(unique_ptr<T>&& ptr) noexcept {
+    rawPtr_ = ptr.rawPtr_;
+    ptr.rawPtr_ = nullptr;
+}
+
+template <typename T>
+unique_ptr<T>::~unique_ptr() {
+    if (rawPtr_) {
+        delete rawPtr_; 
+    }
+}
+
+template <typename T>
+T* unique_ptr<T>::get() const noexcept{ 
+    return rawPtr_; 
+}
+
+template <typename T>
+T* unique_ptr<T>::release() noexcept {
+    T* tempPtr = rawPtr_;
+    rawPtr_ = nullptr;
+    return tempPtr;
+}
+
+template <typename T>
+void unique_ptr<T>::reset(T* ptr) noexcept {
+    T* oldPtr = rawPtr_;
+    rawPtr_ = ptr;
+    if (oldPtr) {
+        delete oldPtr;  
+    }
+}
+
+template <typename T>
+unique_ptr<T>& unique_ptr<T>::operator=(unique_ptr<T>&& ptr) noexcept {
+    if (this != &ptr) {
+        delete rawPtr_;
+        rawPtr_ = ptr.rawPtr_;
+        ptr.rawPtr_ = nullptr;
+    }
+    return *this;
+}
+
+template <typename T>
+T& unique_ptr<T>::operator*() const { 
+    return *get(); 
+}
+
+template <typename T>
+T* unique_ptr<T>::operator->() const noexcept {
+    return get();
+}
+
+}
