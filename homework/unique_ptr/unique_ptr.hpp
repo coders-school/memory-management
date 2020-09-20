fa53e6e41696fa9cbcb12204bff4b2d2bb449bc2
kari0036 <57415454+kari0036@users.noreply.github.com>
2020-09-20 13:24:20 +0200

    Update unique_ptr.hpp

--- homework/unique_ptr/unique_ptr.hpp
+++ homework/unique_ptr/unique_ptr.hpp
@@ -9,7 +9,8 @@ public:
     unique_ptr(T* ptr)
         : ptr_(ptr) {}
     unique_ptr(const unique_ptr& otherPtr) = delete;
-    unique_ptr(unique_ptr&& otherPtr);
+    unique_ptr(unique_ptr&& otherPtr)
+        :ptr_(otherPtr.release()) {};
 
     ~unique_ptr();
 
@@ -19,15 +20,13 @@ public:
 
     T& operator*() const;
     T* operator->() const { return ptr_; }
+    
+    unique_ptr<T>& operator=(const unique_ptr<T>&) = delete;
+    unique_ptr<T>& operator=(unique_ptr<T>&& otherPtr);
 
 private:
     T* ptr_ = nullptr;
 };
-
-template <typename T>
-unique_ptr<T>::unique_ptr(unique_ptr&& otherPtr) {
-    ptr_ = otherPtr.release();
-}
 
 template <typename T>
 T* unique_ptr<T>::release() {
@@ -38,18 +37,13 @@ T* unique_ptr<T>::release() {
 
 template <typename T>
 void unique_ptr<T>::reset(T* newPtr) {
-    if (ptr_) {
-        delete ptr_;
-    }
+    delete ptr_;
     ptr_ = newPtr;
 }
 
-
 template <typename T>
 unique_ptr<T>::~unique_ptr() {
-    if (ptr_) {
-        delete ptr_;
-    }
+    delete ptr_;
 }
 
 template <typename T>
@@ -58,4 +52,12 @@ T& unique_ptr<T>::operator*() const {
         throw PtrIsNullptr{};
     }
     return *ptr_;
+}
+
+template <typename T>
+unique_ptr<T>& unique_ptr<T>::operator=(unique_ptr<T>&& otherPtr){
+    delete ptr_;
+    ptr_ = otherPtr.release();
+    otherPtr.ptr_ = nullptr;
+    return *this;
 }
