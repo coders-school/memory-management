1e28e0d255049507b3fef5b5ab2777d9b4c40b3b
Marcinati <marcin.kapusta.pk@gmail.com>
2020-09-21 19:41:23 +0200

    file handler

---
+++ exercises/FileHandler.hpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <iostream>
+
+class FileOpeningError : public std::runtime_error {
+public:
+    FileOpeningError(std::string fileDirectory) 
+        : std::runtime_error("File opening failed: " + fileDirectory)
+    {}
+};
+
+class FileHandler {
+    FILE* fp_;
+public:
+    FileHandler(std::string fileDirectory)
+        : fp_(std::fopen(fileDirectory.c_str(), "r"))
+    {
+        if (!fp_) {
+            throw FileOpeningError{fileDirectory};
+        }
+    }
+
+    FileHandler(const char * fileDirectory)
+        : fp_(std::fopen(fileDirectory, "r"))
+    {}
+
+
+    ~FileHandler() {
+        std::fclose(fp_);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const FileHandler& fh);
+};
+
+std::ostream& operator<<(std::ostream& os, const FileHandler& fh) {
+    int c;
+    while ((c = std::fgetc(fh.fp_)) != EOF) {
+        os << c;
+    }
+    return os;
+}
+
+int main() {
+    try {
+        FileHandler fh{"../ResourdeD.cpp"};
+
+    } catch (FileOpeningError& foe) {
+        std::cerr << foe.what() << '/n';
+    }
+    return 0;
+}
