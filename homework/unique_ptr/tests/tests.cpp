fa030bc950b80ba73f23e26e115e7e4a87e7503c
Karol Dulemba <kdulemba@gmail.com>
2020-09-22 20:15:05 +0200

    Test fixes

--- homework/unique_ptr/tests/tests.cpp
+++ homework/unique_ptr/tests/tests.cpp
@@ -21,11 +21,38 @@ SCENARIO("Working on home implemented unique_ptr") {
       }
     }
 
-    WHEN("Reseted uniqueptr with nullptr") {
+    WHEN("Reseted unique_ptr with nullptr") {
       firstUnique.reset(nullptr);
       THEN("unique_ptr is nullptr") { REQUIRE(firstUnique.get() == nullptr); }
     }
-    WHEN("Moving into another container") {
+    WHEN("Reseted unique_ptr with reset()") {
+      firstUnique.reset();
+      THEN("unique_ptr is nullptr") { REQUIRE(firstUnique.get() == nullptr); }
+    }
+    WHEN("Releasing owned pointer without assigning return") {
+      firstUnique.release();
+      THEN("unique_ptr is nullptr") { REQUIRE(firstUnique.get() == nullptr); }
+    }
+    WHEN("Releasing owned pointer with assigning return") {
+      unique_ptr<TestClass> SecondUnique(firstUnique.release());
+      THEN("unique_ptr is nullptr") {
+        REQUIRE(firstUnique.get() == nullptr);
+        REQUIRE(SecondUnique.get()->data_ == input1);
+      }
+    }
+    WHEN("Dereferencing unique_ptr (not nullptr)") {
+      double output = (*firstUnique).data_;
+      THEN("comparing data from unique_ptr with original value") {
+        REQUIRE(output == input1);
+      }
+    }
+    WHEN("Dereferencing unique_ptr which is null") {
+      firstUnique.reset();
+      THEN("comparing data from unique_ptr with original value") {
+        REQUIRE_THROWS((*firstUnique).data_);
+      }
+    }
+    WHEN("Moving reseted unique_ptr into another container") {
       firstUnique.reset(new TestClass(input2));
       PtrCollection.push_back(std::move(firstUnique));
       THEN("Check if there are new parameters") {
@@ -33,11 +60,27 @@ SCENARIO("Working on home implemented unique_ptr") {
         REQUIRE(firstUnique.get() == nullptr);
       }
     }
-    WHEN("Moving into another null_ptr ") {
+    WHEN("Moving into another unique_ptr") {
       unique_ptr<TestClass> secondUnique = std::move(firstUnique);
       THEN("Check if there are new parameters") {
         REQUIRE(firstUnique.get() == nullptr);
         REQUIRE(secondUnique.get()->data_ == input1);
+      }
+    }
+    WHEN("Moving unique_ptr(with nullptr inside) into another unique_ptr") {
+      unique_ptr<TestClass> thirdUnique(nullptr);
+      unique_ptr<TestClass> SecondUnique = std::move(thirdUnique);
+      THEN("Check if there are new parameters") {
+        REQUIRE(SecondUnique.get() == nullptr);
+      }
+    }
+    WHEN("Releasing owned pointer with assigning return") {
+      double init3 = 4.0;
+      unique_ptr<TestClass> thirdUnique(new TestClass(init3));
+      unique_ptr<TestClass> SecondUnique = std::move(thirdUnique);
+      THEN("unique_ptr is nullptr") {
+        REQUIRE(firstUnique.get() != nullptr);
+        REQUIRE(SecondUnique.get()->data_ == init3);
       }
     }
   }
--- homework/unique_ptr/uniqueptr.hpp
+++ homework/unique_ptr/uniqueptr.hpp
@@ -6,19 +6,20 @@
 template <typename T> class unique_ptr {
 
 public:
-  unique_ptr(T *rawPtr);                              // creation constructor
+  unique_ptr(T *rawPtr = nullptr);                              // creation constructor
   unique_ptr(unique_ptr<T> &&other) noexcept;         // move constructor
-  unique_ptr(const unique_ptr &) = delete;            // copy constructor
-  unique_ptr &operator=(const unique_ptr &) = delete; // copy assigment operator
+  unique_ptr(const unique_ptr<T> &) = delete;            // copy constructor
+  unique_ptr<T> &operator = (const unique_ptr<T> &) = delete; // copy assigment operator
+  unique_ptr<T> &operator = (unique_ptr<T> && otherUniquePtr) noexcept;
   ~unique_ptr();
 
   T* get() const;
-  T release();
-  void reset(T *rawPtr) noexcept;
+  T* release();
+  void reset(T* NewRawPtr = nullptr);
   T &operator*();
 
 private:
-  T *rawPtr_;
+  T *rawPtr_{nullptr};
 };
 
 template <typename T>
@@ -27,9 +28,7 @@ unique_ptr<T>::unique_ptr(T *rawPtr)
 
 template <typename T>
 unique_ptr<T>::unique_ptr(unique_ptr<T> &&other) noexcept // move constructor
-    : rawPtr_(other.rawPtr_)
-
-{
+    : rawPtr_(other.rawPtr_){
   other.rawPtr_ = nullptr;
 }
 
@@ -40,19 +39,29 @@ unique_ptr<T>::~unique_ptr() // destructor
 }
 
 template <typename T>
+unique_ptr<T> & unique_ptr<T>::operator = (unique_ptr<T> && otherUniquePtr) noexcept{ 
+  if(this != & otherUniquePtr){
+    delete rawPtr_;
+    rawPtr_ = otherUniquePtr.rawPtr_;
+    otherUniquePtr = nullptr;
+  } return *this;
+}
+
+template <typename T>
 T* unique_ptr<T>::get() const { /* if(rawPtr_ == nullptr)
                                  throw std::range_error("Trying to access empty
                                  unique_ptr using .get()"); else  */
   return rawPtr_;
 }
 
-template <typename T> T unique_ptr<T>::release() {
+template <typename T> T* unique_ptr<T>::release() {
   T* result = rawPtr_;
   rawPtr_ = nullptr;
   return result;
 }
 
-template <typename T> void unique_ptr<T>::reset(T *NewRawPtr) noexcept {
+template <typename T> 
+void unique_ptr<T>::reset(T* NewRawPtr){  
   delete rawPtr_;
   rawPtr_ = NewRawPtr;
 }
