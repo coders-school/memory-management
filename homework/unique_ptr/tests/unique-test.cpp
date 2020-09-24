6eca118e1adff30ae985fab7466cb8b9ad080ed2
WojtekMs <wojtek.atmo@gmail.com>
2020-09-24 21:33:29 +0200

    canBeMovedByMoveOperator test added

--- homework/unique_ptr/tests/unique-test.cpp
+++ homework/unique_ptr/tests/unique-test.cpp
@@ -9,9 +9,16 @@ TEST(unique, canBeCreatedWithValue) {
     cs::unique_ptr<int> ptr{new int{3}};
 }
 
-TEST(unique, canBeMoved) {
+TEST(unique, canBeMovedByCtor) {
     cs::unique_ptr<int> ptr{cs::unique_ptr<int>(new int{3})};
     cs::unique_ptr<int> ptr2{std::move(ptr)};
 }
+
+TEST(unique, canBeMovedByMoveOperator) {
+    cs::unique_ptr<int> ptr{new int{3}};
+    cs::unique_ptr<int> ptr2 = std::move(ptr);
+}
+
+
 
 
