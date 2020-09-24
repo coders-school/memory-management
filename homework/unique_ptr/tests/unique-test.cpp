050a69433c0f60ef3eb59aaf8d73f4c72fcf77ad
WojtekMs <wojtek.atmo@gmail.com>
2020-09-24 23:19:54 +0200

    magic numbers removed

--- homework/unique_ptr/tests/unique-test.cpp
+++ homework/unique_ptr/tests/unique-test.cpp
@@ -1,59 +1,80 @@
 #include "gtest/gtest.h"
 #include "unique_ptr.hpp"
 
-TEST(unique, canBeDefaultCreated) {
+class unique : public ::testing::Test {
+   protected:
+    int default_value{3};
+};
+
+TEST_F(unique, canBeDefaultCreated) {
     cs::unique_ptr<int> ptr{};
+    EXPECT_EQ(ptr.get(), nullptr);
 }
 
-TEST(unique, canBeCreatedWithValue) {
-    cs::unique_ptr<int> ptr{new int{3}};
+TEST_F(unique, canBeCreatedWithValue) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
+    EXPECT_EQ(*ptr, default_value);
 }
 
-TEST(unique, canBeMovedByCtor) {
-    cs::unique_ptr<int> ptr{cs::unique_ptr<int>(new int{3})};
+TEST_F(unique, canBeMovedByCtor) {
+    cs::unique_ptr<int> ptr{cs::unique_ptr<int>(new int{default_value})};
     cs::unique_ptr<int> ptr2{std::move(ptr)};
+    EXPECT_EQ(*ptr2, default_value);
+    EXPECT_EQ(ptr.get(), nullptr);
 }
 
-TEST(unique, canBeMovedByMoveOperator) {
-    cs::unique_ptr<int> ptr{new int{3}};
+TEST_F(unique, canBeMovedByMoveOperator) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
     cs::unique_ptr<int> ptr2 = std::move(ptr);
+    EXPECT_EQ(*ptr2, default_value);
+    EXPECT_EQ(ptr.get(), nullptr);
 }
 
-TEST(unique, canBeDereferenced) {
-    cs::unique_ptr<int> ptr{new int{3}};
-    EXPECT_EQ(*ptr, 3);
+TEST_F(unique, canBeDereferenced) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
+    EXPECT_EQ(*ptr, default_value);
 }
 
-TEST(unique, arrowOperatorCanBeUsed) {
+TEST_F(unique, arrowOperatorCanBeUsed) {
     struct foo {
         void use(){};
     };
     cs::unique_ptr<foo> ptr{new foo()};
     ptr->use();
-    
 }
 
-TEST(unique, getMethodShouldReturnPointerWithoutReleasingOwnership) {
-    cs::unique_ptr<int> ptr{new int{5}};
+TEST_F(unique, getMethodShouldReturnPointerWithoutReleasingOwnership) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
     int* raw_ptr = ptr.get();
     EXPECT_EQ(*ptr, *raw_ptr);
+    EXPECT_EQ(*raw_ptr,  default_value);
 }
 
-TEST(unique, releaseMethodShouldReturnPointerAndReleaseOwnership) {
-    cs::unique_ptr<int> ptr{new int{5}};
+TEST_F(unique, releaseMethodShouldReturnPointerAndReleaseOwnership) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
     int* new_owner_ptr = ptr.release();
     EXPECT_EQ(*ptr, *new_owner_ptr);
+    EXPECT_EQ(ptr.get(), nullptr);
     delete new_owner_ptr;
 }
 
-TEST(unique, resetMethodShouldSetAsNullptrByDefault) {
-    cs::unique_ptr<int> ptr{new int{5}};
+TEST_F(unique, resetMethodShouldSetAsNullptrByDefault) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
     ptr.reset();
     EXPECT_EQ(ptr.get(), nullptr);
 }
 
-TEST(unique, resetMethodShouldSetPtrAsEqualToArgument) {
-    cs::unique_ptr<int> ptr{new int{5}};
-    ptr.reset(new int{10});
-    EXPECT_EQ(*ptr, 10);
+TEST_F(unique, resetMethodShouldSetPtrAsEqualToArgument) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
+    int new_value{10};
+    ptr.reset(new int{new_value});
+    EXPECT_EQ(*ptr, new_value);
+}
+
+TEST_F(unique, resetShouldSetOwnershipAgain) {
+    cs::unique_ptr<int> ptr{new int{default_value}};
+    delete ptr.release();
+    int new_value{10};
+    ptr.reset(new int{new_value});
+    EXPECT_EQ(*ptr, new_value);
 }
