a90e44be71d19ef72168b2fae47954e27fd84aab
Pawellos <pablox377@gmail.com>
2020-09-24 18:45:33 +0200

    Fixed memory leaks, add valgrind and code imrovement

--- homework/unique_ptr/UniquePointer.hpp
+++ homework/unique_ptr/UniquePointer.hpp
@@ -8,65 +8,35 @@ public:
 template <typename T>
 class UniquePointer {
 public:
-    UniquePointer(T* ptr);
-    UniquePointer();
+    UniquePointer(T* ptr = nullptr) : ptr_(ptr) {}
     ~UniquePointer();
     UniquePointer(const UniquePointer&) = delete;
-    UniquePointer(UniquePointer&& anotherPtr);
+    UniquePointer(UniquePointer&& anotherPtr) noexcept;
 
     T* get();
     T* release();
-    void reset(T* ptr);
+    void reset(T* ptr = nullptr);
 
-    T operator*() const;
+    T& operator*() const;
     T* operator->() const;
     UniquePointer<T>& operator=(UniquePointer<T>&& anotherPtr);
     UniquePointer<T>& operator=(const UniquePointer<T>&) = delete;
 
 private:
-    T* ptr_;
+    T* ptr_{nullptr};
 };
 
 template <class T>
-UniquePointer<T>::UniquePointer() {}
-
-template <class T>
-UniquePointer<T>::UniquePointer(T* ptr) : ptr_(ptr) {}
-
-template <class T>
 UniquePointer<T>::~UniquePointer() {
-    if (ptr_) {
-        delete ptr_;
-    }
+    delete ptr_;
 }
 
 template <typename T>
-T UniquePointer<T>::operator*() const {
-    if (!ptr_) {
-        throw NullPtrException("Dereference null pointer !");
-    }
-    return *ptr_;
-}
-
-template <typename T>
-UniquePointer<T>& UniquePointer<T>::operator=(UniquePointer<T>&& anotherPtr) {
-    if (this != &anotherPtr) {
-        delete ptr_;
-        ptr_ = anotherPtr.ptr_;
-        anotherPtr.ptr_ = nullptr;
-    }
-
-    return *this;
-}
-
-template <typename T>
-T* UniquePointer<T>::operator->() const {
-    return ptr_;
-}
+UniquePointer<T>::UniquePointer(UniquePointer&& anotherPtr) noexcept : ptr_(anotherPtr.release()) {}
 
 template <typename T>
 T* UniquePointer<T>::get() {
-    return ptr_ ? ptr_ : nullptr;
+    return ptr_;
 }
 
 template <typename T>
@@ -78,13 +48,30 @@ T* UniquePointer<T>::release() {
 
 template <typename T>
 void UniquePointer<T>::reset(T* ptr) {
-    if (ptr_) {
-        delete ptr_;
-    }
+    delete ptr_;
     ptr_ = ptr;
 }
 
 template <typename T>
-UniquePointer<T>::UniquePointer(UniquePointer&& anotherPtr) {
-    ptr_ = anotherPtr.release();
+T& UniquePointer<T>::operator*() const {
+    if (!ptr_) {
+        throw NullPtrException("Dereference null pointer !");
+    }
+    return *ptr_;
+}
+
+template <typename T>
+T* UniquePointer<T>::operator->() const {
+    return ptr_;
+}
+
+template <typename T>
+UniquePointer<T>& UniquePointer<T>::operator=(UniquePointer<T>&& anotherPtr) {
+    if (this != &anotherPtr) {
+        delete ptr_;
+        ptr_ = anotherPtr.ptr_;
+        anotherPtr.ptr_ = nullptr;
+    }
+
+    return *this;
 }
