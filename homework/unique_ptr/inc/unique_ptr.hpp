91658feae85ca3950823ab6bca1465b9c9b58674
WojtekMs <wojtek.atmo@gmail.com>
2020-09-24 21:22:29 +0200

    full unique_ptr interface added

--- homework/unique_ptr/inc/unique_ptr.hpp
+++ homework/unique_ptr/inc/unique_ptr.hpp
@@ -3,7 +3,21 @@
 namespace cs {
 template<typename T>
 class unique_ptr {
+    T* data_{nullptr};
     public:
+    unique_ptr() = default;
     unique_ptr(T* data) {};
+    unique_ptr(const unique_ptr&) = delete;
+    unique_ptr(unique_ptr&& rhs) {}
+    unique_ptr& operator=(const unique_ptr&) = delete;
+    unique_ptr& operator=(unique_ptr&& rhs) {
+        
+    }
+    T& operator*() const {}
+    T* operator->() const {}
+    T* get() const {}
+    T* release() {}
+    void reset() {}
+    ~unique_ptr() { delete data_; }
 };
 };  // namespace cs
