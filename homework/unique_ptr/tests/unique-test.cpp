684cae35300b1cca2a4adfb5ce4e2c14695d7322
WojtekMs <wojtek.atmo@gmail.com>
2020-09-24 21:29:09 +0200

    canBeMoved test added

--- homework/unique_ptr/tests/unique-test.cpp
+++ homework/unique_ptr/tests/unique-test.cpp
@@ -1,6 +1,17 @@
 #include "gtest/gtest.h"
 #include "unique_ptr.hpp"
 
-TEST(unique, canBeCreated) {
+TEST(unique, canBeDefaultCreated) {
+    cs::unique_ptr<int> ptr{};
+}
+
+TEST(unique, canBeCreatedWithValue) {
     cs::unique_ptr<int> ptr{new int{3}};
 }
+
+TEST(unique, canBeMoved) {
+    cs::unique_ptr<int> ptr{cs::unique_ptr<int>(new int{3})};
+    cs::unique_ptr<int> ptr2{std::move(ptr)};
+}
+
+
