644f53cc9ea393b2f622559477d040baebeeb45a
mdus <marcin.dus@nokia.com>
2020-09-25 15:10:05 +0200

    changed test overloading operator= will bie added later

--- homework/unique_ptr/test.cpp
+++ homework/unique_ptr/test.cpp
@@ -1,15 +1,10 @@
 #include "md_unique_ptr.hpp"
 #include "gtest/gtest.h"
 
-class Test : public testing::Test {
-public:
-  void SetUp() override {
-  }
-};
 
-TEST_F(Test, xxxx) {
-    int expected = 1;
-    int result = 1;
-        EXPECT_EQ(expected, result);
+
+TEST(Test, uniquePtrIncrement)
+{
+   my_unique_ptr ptr  = my_unique_ptr<int>(new int(10));
 }
 
