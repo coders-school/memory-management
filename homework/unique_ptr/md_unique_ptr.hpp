4d7206a99dbef113184ee1befa3c407c853da322
mdus <marcin.dus@nokia.com>
2020-09-28 13:38:34 +0200

    further ideas for unique pointer

--- homework/unique_ptr/md_unique_ptr.hpp
+++ homework/unique_ptr/md_unique_ptr.hpp
@@ -8,12 +8,38 @@ class my_unique_ptr
 public:
     T* get() { return ptr_; }
 
-    my_unique_ptr(T* ptr) :  ptr_(ptr) {};
+    my_unique_ptr(T* ptr) : ptr_(ptr){};
+    my_unique_ptr()
+    {
+        T someObject = T();
+        ptr_ = &someObject;
+        //        *ptr_ = T();
+    };
+
+    //    my_unique_ptr(my_unique_ptr<T> ) = delete;  //copy not allowed so we should delete auto generated copy ctor
+    //    error: invalid constructor; you probably meant ‘my_unique_ptr<T> (const my_unique_ptr<T>&)’
+
+    my_unique_ptr(const my_unique_ptr<T>&) = delete; // copy not allowed so we should delete auto generated copy ctor
 
     ~my_unique_ptr() { delete ptr_; }
 
-    T operator*() { return *ptr_;} ; //what if nullptr
+    T operator*() { return *ptr_; }; // we can return object
+/* very interesting error
+fatal error: template instantiation depth exceeds maximum of 900 (use -ftemplate-depth= to increase the maximum)
+    EXPECT_EQ(ptr->str, "");
 
+    my_unique_ptr<T*> operator->()
+    {
+        return this;
+    };
+*/
+    // here we can not return *ptr_ it is raw ptr so no gain we can do this in get() function when user knows what is
+       // he doing
+
+    T* operator->()
+    {
+        return this->ptr_;
+    };
 
 private:
     T* ptr_ = nullptr;
--- homework/unique_ptr/test.cpp
+++ homework/unique_ptr/test.cpp
@@ -1,28 +1,63 @@
+#include <string>
+#include <iostream>
 #include "md_unique_ptr.hpp"
 #include "gtest/gtest.h"
 
-class SomeUsefulClass { };
+struct SomeUsefulClass {
+    SomeUsefulClass() :str("") {
+        std::cout << "default ctro called" << "\n";
+    };
+    std::string str;
+
+};
 
 TEST(Test, dereferencingAfterGet)
 {
    my_unique_ptr<int> ptr  = my_unique_ptr<int>(new int(10));
    EXPECT_EQ(*(ptr.get()), 10);
 }
-/*
-TEST(Test, dereferencing)
+
+TEST(Test, emptyDefaultCtor)
 {
-   my_unique_ptr ptr  = my_unique_ptr<int>(nullptr);
-   EXPECT_EQ(*ptr, 10);
+//   my_unique_ptr<SomeUsefulClass> ptr = my_unique_ptr<SomeUsefulClass>();
+//   EXPECT_EQ(ptr, "");
 }
-*/
-TEST(Test, emptyConstrustor)
+
+
+TEST(Test, emptyCtorForClassAndDereferencing)
 {
-   my_unique_ptr<int> ptr;
-   my_unique_ptr<SomeUsefulClass> ptr_clas;
-   EXPECT_EQ(*ptr, 0);
+   my_unique_ptr<SomeUsefulClass> ptr;
+   EXPECT_EQ((*ptr).str, "");
+}
+
+TEST(Test, emptyCtorForClassAndAccecViaPtr)
+{
+   my_unique_ptr<SomeUsefulClass> ptr;
+   EXPECT_EQ(ptr->str, "");
 }
 
 TEST(Test, ctrowithParam)
 {
 
 }
+
+/*
+
+TEST(Test, dereferencing_shouldSegfault)
+{
+   my_unique_ptr ptr  = my_unique_ptr<int>(nullptr);
+   EXPECT_EQ(*ptr, 10);
+}
+
+
+TEST(Test, deletedCopyConstructor_shouldNotCompile)
+{
+In member function ‘virtual void Test_deletedCopyConstructor_Test::TestBody()’:
+error: use of deleted function ‘my_unique_ptr<T>::my_unique_ptr(const my_unique_ptr<T>&) [with T = int]’
+my_unique_ptr<int> copy_ptr = ptr;
+
+my_unique_ptr<int> ptr  = my_unique_ptr<int>(new int(10));
+my_unique_ptr<int> copy_ptr = ptr;
+EXPECT_EQ(*(ptr.get()), 10);
+}
+*/
