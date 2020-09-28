10b11d8936dbea3ccfaa3d0d47fda37c9ffe2360
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-09-28 12:18:33 +0100

    Modify implementation according to code review suggestions

--- homework/unique_ptr/main.cpp
+++ homework/unique_ptr/main.cpp
@@ -8,25 +8,19 @@ int main() {
     
     unique_ptr<int> ptr2(new int(77));
     
-    unique_ptr<int> ptr3(ptr2);
-    unique_ptr<int> ptr9(ptr0);
-
-    unique_ptr<int> ptr4(std::move(ptr3));
+    unique_ptr<int> ptr3(std::move(ptr2));
     
-    ptr3 = nullptr;
-    ptr2 = nullptr;
+    // int result;
+    int result = *(ptr3->get());
+    int value = *ptr3;
+    (void)result;
+    (void)value;
     
-    unique_ptr<int> ptr5(ptr4);
-    ptr5 = std::move(ptr4);
+    int* ptr = ptr3.release();
+    delete ptr;
 
-    int result;
-    result = *(ptr5->get());
-    result = *ptr5;
-
-    int* ptr = ptr5.release();
-
-    ptr5.reset(new int(99));
-    ptr5.reset(nullptr);
+    ptr3.reset(new int(99));
+    ptr3.reset(nullptr);
 
     return 0;
 }
--- homework/unique_ptr/unique_ptr.hpp
+++ homework/unique_ptr/unique_ptr.hpp
@@ -6,21 +6,19 @@ template <typename T>
 class unique_ptr {
 public:
     unique_ptr();
-    unique_ptr(std::nullptr_t);
     explicit unique_ptr(T*);
-    unique_ptr(unique_ptr const&);
+    unique_ptr(unique_ptr const&) = delete;
     unique_ptr(unique_ptr&&) noexcept;
     ~unique_ptr();
 
     unique_ptr& operator=(unique_ptr const&) = delete;
-    unique_ptr& operator=(std::nullptr_t);
     unique_ptr& operator=(unique_ptr&&) noexcept;
     unique_ptr<T>* operator->();
     T& operator*() const;
 
     T* get() const;
     T* release() noexcept;
-    void reset(T*);
+    void reset(T* = nullptr);
 
 private:
     T* data_;
@@ -31,50 +29,27 @@ unique_ptr<T>::unique_ptr()
     : data_(nullptr) {}
 
 template <typename T>
-unique_ptr<T>::unique_ptr(std::nullptr_t)
-    : data_(nullptr) {}
-
-template <typename T>
 unique_ptr<T>::unique_ptr(T* data)
     : data_(data) {}
 
 template <typename T>
-unique_ptr<T>::unique_ptr(unique_ptr const& rhs) {
-    if (rhs.get()) { 
-        data_ = new T(*rhs.data_);
-    } else {
-        data_ = nullptr;
-    }
-}
-
-template <typename T>
 unique_ptr<T>::unique_ptr(unique_ptr&& rhs) noexcept {
-    this->data_ = new T(*rhs.data_);
-    delete rhs.data_;
+    data_ = rhs.data_;
     rhs.data_ = nullptr;
 }
 
 template <typename T>
 unique_ptr<T>::~unique_ptr() {
-    if (data_ != nullptr) {
         delete data_;
-    }
-}
-
-template <typename T>
-unique_ptr<T>& unique_ptr<T>::operator=(std::nullptr_t) {
-    if (data_) {
-        delete data_;
-        data_ = nullptr;
-    }
-    return *this;
 }
 
 template <typename T>
 unique_ptr<T>& unique_ptr<T>::operator=(unique_ptr&& rhs) noexcept {
-    this->data_ = new T(*rhs.data_);
-    delete rhs.data_;
-    rhs.data_ = nullptr;
+    if (this != &rhs) {
+        delete data_;
+        data_ = rhs.data_;
+        rhs.data_ = nullptr;
+    }
     return *this;
 }
 
@@ -90,22 +65,18 @@ T& unique_ptr<T>::operator*() const {
 
 template <typename T>
 T* unique_ptr<T>::get() const {
-    if (data_) {
         return data_;
-    }
-    return nullptr;
 }
 
 template <typename T>
 T* unique_ptr<T>::release() noexcept {
     T* released = data_;
-    delete data_;
     data_ = nullptr;
     return released;
 }
 
 template <typename T>
 void unique_ptr<T>::reset(T* data) {
-    this->release();
+    delete data_;
     data_ = data;
 }
