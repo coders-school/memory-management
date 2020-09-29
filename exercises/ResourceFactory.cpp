30431ceeb2cbba99e9e486d46185c97eab29beef
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-09-29 16:27:37 +0100

    Detect & remove memory leaks

--- exercises/ResourceFactory.cpp
+++ exercises/ResourceFactory.cpp
@@ -9,7 +9,7 @@ struct Resource
     Resource(char* byte) : byte_(byte) {}
     char* byte() const { return byte_; }
     virtual string name() const = 0;
-    ~Resource() { delete byte_; }
+    virtual ~Resource() { delete byte_; }
 
 protected:
     char* byte_ = nullptr;
@@ -18,13 +18,13 @@ protected:
 struct ResourceA : Resource
 {
     ResourceA(char* byte) : Resource(byte) {}
-    string name() const override { return string("ResourceA ").append(byte_); }
+    string name() const override { return string("ResourceA ").append(byte_, 1); }
 };
 
 struct ResourceB : Resource
 {
     ResourceB(char* byte) : Resource(byte) {}
-    string name() const override { return string("ResourceB ").append(byte_); }
+    string name() const override { return string("ResourceB ").append(byte_, 1); }
 };
 
 struct ResourceFactory
@@ -36,7 +36,12 @@ struct ResourceFactory
 struct ResourceCollection
 {
     void add(Resource* r) { resources.push_back(r); }
-    void clear() { resources.clear(); }
+    void clear() {
+        for (const auto & res : resources) {
+            delete res;
+        }
+        resources.clear();
+    }
     Resource* operator[](int index) { return resources[index]; }
     void printAll()
     {
@@ -54,14 +59,13 @@ int main()
 {
     ResourceCollection collection;
     ResourceFactory rf;
-    collection.add(rf.makeResourceA(new char{0x01}));
-    collection.add(rf.makeResourceB(new char{0x02}));
+    collection.add(rf.makeResourceA(new char{0x41}));
+    collection.add(rf.makeResourceB(new char{0x42}));
     collection.printAll();
 
     auto firstByte = collection[0]->byte();
-    collection.clear();
     cout << *firstByte << endl;
+    collection.clear();
 
     return 0;
 }
-
