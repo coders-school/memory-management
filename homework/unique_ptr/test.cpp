103564b6f9613cae4c2bb22088caa66b361c315d
duszek <marcin.dus@gmail.com>
2020-09-29 08:33:08 +0200

    another iteration

--- homework/unique_ptr/test.cpp
+++ homework/unique_ptr/test.cpp
@@ -1,63 +1,62 @@
 #include <string>
 #include <iostream>
-#include "md_unique_ptr.hpp"
+#include "my_unique_ptr.hpp"
 #include "gtest/gtest.h"
 
-struct SomeUsefulClass {
-    SomeUsefulClass() :str("") {
-        std::cout << "default ctro called" << "\n";
-    };
-    std::string str;
-
+struct SomeUsefulClass
+{
+    SomeUsefulClass() : message_(""){};
+    SomeUsefulClass(std::string message) : message_(message){};
+    std::string message_;
 };
+
+void functionUptrAsArg(my_unique_ptr<SomeUsefulClass> ptr)
+{
+    std::cout << ptr->message_ << "\n";
+}
+/*
+my_unique_ptr<SomeUsefulClass> provider(std::string someString)
+{
+    return my_unique_ptr<SomeUsefulClass>(someString); 
+}
+*/
 
 TEST(Test, dereferencingAfterGet)
 {
-   my_unique_ptr<int> ptr  = my_unique_ptr<int>(new int(10));
-   EXPECT_EQ(*(ptr.get()), 10);
+    my_unique_ptr<int> ptr = my_unique_ptr<int>(new int(10));
+    EXPECT_EQ(*(ptr.get()), 10);
 }
 
 TEST(Test, emptyDefaultCtor)
 {
-//   my_unique_ptr<SomeUsefulClass> ptr = my_unique_ptr<SomeUsefulClass>();
-//   EXPECT_EQ(ptr, "");
+       my_unique_ptr<SomeUsefulClass> ptr = my_unique_ptr<SomeUsefulClass>();
+       EXPECT_EQ(ptr->message_, "");
 }
-
 
 TEST(Test, emptyCtorForClassAndDereferencing)
 {
-   my_unique_ptr<SomeUsefulClass> ptr;
-   EXPECT_EQ((*ptr).str, "");
+    my_unique_ptr<SomeUsefulClass> ptr;
+    EXPECT_EQ((*ptr).message_, "");
 }
 
 TEST(Test, emptyCtorForClassAndAccecViaPtr)
 {
-   my_unique_ptr<SomeUsefulClass> ptr;
-   EXPECT_EQ(ptr->str, "");
+    my_unique_ptr<SomeUsefulClass> ptr;
+    EXPECT_EQ(ptr->message_, "");
 }
 
 TEST(Test, ctrowithParam)
 {
-
 }
 
 /*
 
-TEST(Test, dereferencing_shouldSegfault)
-{
-   my_unique_ptr ptr  = my_unique_ptr<int>(nullptr);
-   EXPECT_EQ(*ptr, 10);
-}
-
-
 TEST(Test, deletedCopyConstructor_shouldNotCompile)
 {
-In member function ‘virtual void Test_deletedCopyConstructor_Test::TestBody()’:
-error: use of deleted function ‘my_unique_ptr<T>::my_unique_ptr(const my_unique_ptr<T>&) [with T = int]’
-my_unique_ptr<int> copy_ptr = ptr;
+    my_unique_ptr<int> copy_ptr = ptr;
 
-my_unique_ptr<int> ptr  = my_unique_ptr<int>(new int(10));
-my_unique_ptr<int> copy_ptr = ptr;
-EXPECT_EQ(*(ptr.get()), 10);
+    my_unique_ptr<int> ptr  = my_unique_ptr<int>(new int(10));
+    my_unique_ptr<int> copy_ptr = ptr;
+    EXPECT_EQ(*(ptr.get()), 10);
 }
 */
