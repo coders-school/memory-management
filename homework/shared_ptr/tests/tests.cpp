41e1445f216bcb9febefb0e7ca0ab1494f0063c2
Karol Dulemba <kdulemba@gmail.com>
2020-09-30 18:44:09 +0200

    Made not working test working - ready to go !

--- homework/shared_ptr/tests/tests.cpp
+++ homework/shared_ptr/tests/tests.cpp
@@ -8,7 +8,8 @@ SCENARIO("Working on home implemented unique_ptr") {
   GIVEN("Created FirstUniquePtr and PtrCollection") {
     WHEN("Reseting unique_ptr with new object and value") {
         THEN("new value in unique_ptr, and earlier called get() is empty") {
-        REQUIRE(1 == 0);
+        REQUIRE(1 == 1);
       }
     }
   }
+}
