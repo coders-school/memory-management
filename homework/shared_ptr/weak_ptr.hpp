02e53f4659db0ddab3f34155e63292cdb2517cda
Kuba C <jakub.cieplinski@outlook.com>
2020-10-01 19:32:33 +0200

    maybe fixed segfault?

--- homework/shared_ptr/weak_ptr.hpp
+++ homework/shared_ptr/weak_ptr.hpp
@@ -26,7 +26,17 @@ public:
 private:
     control_block* counter_{nullptr};
     T* ptr_{nullptr};
+
+    void checkAndDeletePointers();
 };
+
+
+template <typename T>
+void weak_ptr<T>::checkAndDeletePointers() {
+    if (!counter_->getWeakRefs() && !counter_->getRefs()) {
+            delete counter_;
+        }
+}
 
 template <typename T>
 weak_ptr<T>::weak_ptr(const weak_ptr& ptr) noexcept
@@ -59,11 +69,8 @@ weak_ptr<T>::weak_ptr(weak_ptr<T>&& previousOwner) noexcept
 template <typename T>
 weak_ptr<T>::~weak_ptr() {
     if (counter_ != nullptr) {
-        if (!counter_->getWeakRefs() && !counter_->getRefs()) {
-            delete counter_;
-        } else {
-            counter_->decreaseWeakRefs();
-        }
+        counter_->decreaseWeakRefs();
+        checkAndDeletePointers();
     }
 }
 
