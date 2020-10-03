5463da262f6e87289eab9323926dc8241bdbb948
WojtekMs <wojtek.atmo@gmail.com>
2020-10-03 22:16:17 +0200

    shared can be copied

--- homework/shared_ptr/shared.cpp
+++ homework/shared_ptr/shared.cpp
@@ -1,10 +1,44 @@
-namespace cs {
+#include <atomic>
+
+namespace cs
+{
+class control_block
+{
+    std::atomic_size_t sharedRef_{1};
+    std::atomic_size_t weakRef_{0};
+
+   public:
+    control_block() = default;
+    int getSharedRef() const noexcept { return sharedRef_; }
+    int getWeakRef() const noexcept { return weakRef_; }
+    void incrementSharedRef() noexcept { sharedRef_++; }
+    void incrementWeakRef() noexcept { weakRef_++; }
+};
 
 template <typename T>
-class shared_ptr {
+class shared_ptr
+{
     T* data_{nullptr};
-    public:
-    shared_ptr() = default;
+    control_block* controlBlock{nullptr};
 
+   public:
+    shared_ptr() = default;
+    shared_ptr(T* data);
+    shared_ptr(const shared_ptr&);
+
+    T* get() const noexcept { return data_; }
+    int use_count() const noexcept { return controlBlock->getSharedRef(); }
 };
-};
+
+template <typename T>
+shared_ptr<T>::shared_ptr(T* data) : data_(data)
+{
+    controlBlock = new control_block();
+}
+
+template <typename T>
+shared_ptr<T>::shared_ptr(const shared_ptr& rhs) : data_(rhs.data_), controlBlock(rhs.controlBlock)
+{
+    controlBlock->incrementSharedRef();
+}
+};  // namespace cs
--- homework/shared_ptr/tests/shared_tests.cpp
+++ homework/shared_ptr/tests/shared_tests.cpp
@@ -3,10 +3,23 @@
 
 class SharedTest :public ::testing::Test {
     protected:
-
+    int defaultValue{5};
 
 };
 
-TEST_F(SharedTest, canBeCreated) {
+TEST_F(SharedTest, defaultSharedShouldBeNullptr) {
     cs::shared_ptr<int> test;
+    EXPECT_EQ(test.get(), nullptr);
+}
+
+TEST_F(SharedTest, sharedWhenCreatedFromRawPtrShouldAllocateControlBlock) {
+    cs::shared_ptr<int> test(new int{defaultValue});
+    EXPECT_EQ(test.use_count(), 1);
+}
+
+TEST_F(SharedTest, sharedWhenCopiedShouldIncreaseRefCount) {
+    cs::shared_ptr<int> test(new int{defaultValue});
+    cs::shared_ptr<int> test2(test);
+    EXPECT_EQ(test.use_count(), 2);
+    EXPECT_EQ(test2.use_count(), 2);
 }
