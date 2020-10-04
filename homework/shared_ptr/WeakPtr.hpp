810bb8a4616ce3976e2d374290e23b10b95cc09c
MonikaZ <monika.zelechowska@o2.pl>
2020-10-04 19:59:25 +0200

    Adding WeakPtr member functions

--- homework/shared_ptr/WeakPtr.hpp
+++ homework/shared_ptr/WeakPtr.hpp
@@ -12,10 +12,28 @@ public:
     WeakPtr(WeakPtr&& otherWeakPtr) noexcept;
     ~WeakPtr();
 
+    size_t use_count() const noexcept;
+    bool expired() const noexcept;
+    SharedPtr<T> lock() const noexcept;
+    void reset() noexcept;
+
+    void handleWeakPtrAndControlBlockDelete();
+
 private:
     T* ptr_ = nullptr;
     SharedControlBlock<T>* controlBlock_ = nullptr;
 };
+
+template <typename T>
+void WeakPtr<T>::handleWeakPtrAndControlBlockDelete() {
+    if (controlBlock_ != nullptr) {
+        controlBlock_->decrementWeakRefsCount();
+        if (controlBlock_->getSharedRefsCount() == 0 && controlBlock_->getWeakRefsCount() == 0) {
+            delete controlBlock_;
+        }
+    }
+}
+
 template <typename T>
 WeakPtr<T>::WeakPtr(const WeakPtr& otherWeakPtr) noexcept
     : ptr_(otherWeakPtr.ptr_), controlBlock_(otherWeakPtr.controlBlock_) {
@@ -38,12 +56,34 @@ WeakPtr<T>::WeakPtr(WeakPtr&& otherWeakPtr) noexcept
 
 template <typename T>
 WeakPtr<T>::~WeakPtr() {
+    handleWeakPtrAndControlBlockDelete();
+}
+
+template <typename T>
+size_t WeakPtr<T>::use_count() const noexcept {
     if (controlBlock_ != nullptr) {
-        controlBlock_->decrementWeakRefsCount();
-        if (controlBlock_->getSharedRefsCount() == 0 && controlBlock_->getWeakRefsCount() == 0) {
-            delete controlBlock_;
-        }
+        return controlBlock_->getSharedRefsCount();
+    }
+    return 0;
+}
+
+template <typename T>
+bool WeakPtr<T>::expired() const noexcept {
+    return (use_count() == 0);
+}
+
+template <typename T>
+SharedPtr<T> WeakPtr<T>::lock() const noexcept {
+    if (expired()) {
+        SharedPtr<T>();
+    } else {
+        SharedPtr<T>(ptr_);
     }
 }
 
-// * Member functions: `use_count()`, `expired()`, `lock()`, `reset()`
+template <typename T>
+void WeakPtr<T>::reset() noexcept {
+    handleWeakPtrAndControlBlockDelete();
+    ptr_ = nullptr;
+    controlBlock_ = nullptr;
+}
