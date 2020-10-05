a34087b672634e06ac34092d3d1cc818728ad79e
Kasper Biegun <kasper.biegun95@gmail.com>
2020-10-05 13:29:43 +0200

    Control Block added to shared_ptr

--- homework/shared_ptr/main.cpp
+++ homework/shared_ptr/main.cpp
@@ -10,11 +10,11 @@ int main() {
     std::cout << *ptr2.get() << "\n";
     std::cout << ptr2.use_count() << "\n";
     SharedPointer<int> ptr3(ptr2);
-    {
-        SharedPointer<int> ptr4(ptr2);
-        std::cout << ptr2.use_count() << "\n";
-    }
     *ptr3 += 5;
+
+    SharedPointer<int> ptr4(ptr2);
+    std::cout << ptr2.use_count() << "\n";
+
     std::cout << *ptr3.get() << "\n";
     std::cout << *ptr2.get() << "\n";
     std::cout << ptr3.use_count() << "\n";
