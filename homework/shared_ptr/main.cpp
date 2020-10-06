30d0404c76932384078863d71d0d201a7c7e075c
Paweł Losik <pablox377@gmail.com>
2020-10-06 16:30:14 +0200

    Update main.cpp

--- homework/shared_ptr/main.cpp
+++ homework/shared_ptr/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "SharedPointer.hpp"
+#include "WeakPointer.hpp"
 
 int main() {
     std::cout << "Shared pointers: \n";
@@ -18,5 +19,29 @@ int main() {
     std::cout << *ptr3.get() << "\n";
     std::cout << *ptr2.get() << "\n";
     std::cout << ptr3.use_count() << "\n";
+
+    std::cout << "\n\nWeak pointers: \n";
+    SharedPointer<int> s_ptr(new int(33));
+    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    WeakPointer<int> w_ptr(s_ptr);
+    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+
+    //w_ptr = s_ptr;
+    auto sp = w_ptr.lock();
+    if (!w_ptr.expired()) {
+        std::cout << "sp = " << *sp << "\n";
+    } else {
+        std::cout << "Unable to lock \n";
+    }
+
+    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+
+    w_ptr.reset();
+
+    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+
     return 0;
 }
