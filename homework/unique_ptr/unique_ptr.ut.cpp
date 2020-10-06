f31571ccc7de96ea449baf8c679abef1aa9bb8ea
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-10-06 11:10:24 +0200

    added tests

---
+++ homework/unique_ptr/unique_ptr.ut.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+#include "unique_ptr.hpp"
+
+constexpr int expected = 42;
+constexpr int newExpected = 21;
+
+struct unique_ptr_test : public ::testing::Test {
+    unique_ptr<int> ptr = new int{expected};
+};
+
+TEST_F(unique_ptr_test, shouldDereference) {
+    ASSERT_EQ(*ptr, expected);
+}
+
+TEST_F(unique_ptr_test, shouldChangeValue) {
+    *ptr = newExpected;
+    ASSERT_EQ(*ptr, newExpected);
+}
+
+TEST_F(unique_ptr_test, shouldCreateNullptr) {
+    unique_ptr<int> null_ptr;
+    ASSERT_EQ(null_ptr.get(), nullptr);
+}
+
+TEST_F(unique_ptr_test, shouldRelease) {
+    auto ptr2 = ptr.release();
+    ASSERT_EQ(*ptr2, expected);
+    ASSERT_EQ(ptr.get(), nullptr);
+}
+
+TEST_F(unique_ptr_test, shouldReset) {
+    ptr.reset(new int(newExpected));
+    ASSERT_EQ(*ptr, newExpected);
+}
+
+TEST_F(unique_ptr_test, shouldMove) {
+    auto ptr2 = std::move(ptr);
+    ASSERT_EQ(ptr.get(), nullptr);
+    ASSERT_EQ(*ptr2, expected);
+}
