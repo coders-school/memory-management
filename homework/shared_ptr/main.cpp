dc4b767ec16d06bde2f55fe0e9df81c68f66b960
Kasper Biegun <kasper.biegun95@gmail.com>
2020-10-10 21:06:42 +0200

    Some weak ptr tests added

--- homework/shared_ptr/main.cpp
+++ homework/shared_ptr/main.cpp
@@ -4,41 +4,41 @@
 #include "WeakPointer.hpp"
 
 int main() {
-    std::cout << "\n\nWeak pointers: \n";
-    SharedPointer<int> s_ptr(new int(33));
-    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
-    WeakPointer<int> w_ptr(s_ptr);
-    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
-    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+    // std::cout << "\n\nWeak pointers: \n";
+    // SharedPointer<int> s_ptr(new int(33));
+    // std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    // WeakPointer<int> w_ptr(s_ptr);
+    // std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    // std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+    // // w_ptr = s_ptr;
+    // auto sp = w_ptr.lock();
+    // if (!w_ptr.expired()) {
+    //     std::cout << "sp = " << *sp << "\n";
+    // } else {
+    //     std::cout << "Unable to lock \n";
+    // }
+    // std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    // std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+    // w_ptr.reset();
+    // std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    // std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+    // auto ssp = w_ptr.lock();
+    // if (!w_ptr.expired()) {
+    //     std::cout << "ssp = " << *ssp << "\n";
+    // } else {
+    //     std::cout << "Unable to lock \n";
+    // }
+    // std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    // std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
     // w_ptr = s_ptr;
-    auto sp = w_ptr.lock();
-    if (!w_ptr.expired()) {
-        std::cout << "sp = " << *sp << "\n";
-    } else {
-        std::cout << "Unable to lock \n";
-    }
-    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
-    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
-    w_ptr.reset();
-    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
-    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
-    auto ssp = w_ptr.lock();
-    if (!w_ptr.expired()) {
-        std::cout << "ssp = " << *ssp << "\n";
-    } else {
-        std::cout << "Unable to lock \n";
-    }
-    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
-    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
-    w_ptr = s_ptr;
-    auto sssp = w_ptr.lock();
-    if (!w_ptr.expired()) {
-        std::cout << "sssp = " << *sssp << "\n";
-    } else {
-        std::cout << "Unable to lock \n";
-    }
-    std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
-    std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
+    // auto sssp = w_ptr.lock();
+    // if (!w_ptr.expired()) {
+    //     std::cout << "sssp = " << *sssp << "\n";
+    // } else {
+    //     std::cout << "Unable to lock \n";
+    // }
+    // std::cout << "Shared counter = " << s_ptr.use_count() << "\n";
+    // std::cout << "Weak counter = " << w_ptr.use_count() << "\n";
 
     return 0;
 }
