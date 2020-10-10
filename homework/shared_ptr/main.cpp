24363fce1b7c846ee2b778caa7a877ade7275acd
damixiorek <damixiorek@gmail.com>
2020-10-10 12:08:41 +0200

    add time test to standard library shared pointer

--- homework/shared_ptr/main.cpp
+++ homework/shared_ptr/main.cpp
@@ -52,6 +52,11 @@ int main()
     testVector1.reserve(50000);
     testVector2.reserve(50000);
 
+    std::vector<std::shared_ptr<int>> testVector3{};
+    std::vector<std::shared_ptr<int>> testVector4{};
+    testVector3.reserve(50000);
+    testVector4.reserve(50000);
+
     {
         high_resolution_clock::time_point timeStart2 = high_resolution_clock::now();
 
@@ -76,6 +81,33 @@ int main()
         duration<double> fullTime = duration_cast<duration<double>>(timeStop - timeStart);
 
         std::cout << "Timer SharedPointer: " << fullTime.count() << " seconds.\n";
+   
+    }
+
+    {
+        high_resolution_clock::time_point timeStart2 = high_resolution_clock::now();
+
+        for (int i = 0; i < 50000; ++i) {
+            testVector3.emplace_back(std::make_shared<int>(i));
+        }
+
+        high_resolution_clock::time_point timeStop2 = high_resolution_clock::now();
+
+        duration<double> fullTime2 = duration_cast<duration<double>>(timeStop2 - timeStart2);
+
+        std::cout << "Timer LibraryMakeShared: " << fullTime2.count() << " seconds.\n";
+
+        high_resolution_clock::time_point timeStart = high_resolution_clock::now();
+
+        for (int i = 0; i < 50000; ++i) {
+            testVector4.emplace_back(new int{i});
+        }
+
+        high_resolution_clock::time_point timeStop = high_resolution_clock::now();
+
+        duration<double> fullTime = duration_cast<duration<double>>(timeStop - timeStart);
+
+        std::cout << "Timer LibrarySharedPointer: " << fullTime.count() << " seconds.\n";
    
     }
 
