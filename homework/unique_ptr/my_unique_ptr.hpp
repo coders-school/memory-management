7212f50053ba3bcd7ea46adf6a7ee8d4e13592ec
duszek <marcin.dus@gmail.com>
2020-10-14 21:52:46 +0200

    fix after review uniq_ptr class

--- homework/unique_ptr/my_unique_ptr.hpp
+++ homework/unique_ptr/my_unique_ptr.hpp
@@ -1,40 +1,40 @@
 #pragma once
 
-template <typename T>
-class my_unique_ptr
-{
+template <typename T> class my_unique_ptr {
 
 public:
-    explicit my_unique_ptr(T* ptr) : ptr_(ptr){};
+  explicit my_unique_ptr(T *ptr) : ptr_(ptr){};
 
-    my_unique_ptr() : ptr_{nullptr} {}
+  my_unique_ptr() : ptr_(nullptr) {}
 
-    my_unique_ptr(const my_unique_ptr<T>&) = delete;
+  my_unique_ptr(my_unique_ptr<T> &&ptr_m) { ptr_ = ptr_m.release(); }
 
-    my_unique_ptr(my_unique_ptr<T>&& ptr_m) { ptr_ = ptr_m.release(); }
+  T &operator&&(my_unique_ptr<T> &&ptr_m) {
+    this->ptr_ = ptr_.release();
+    return *(this->ptr_);
+  }
 
-    void operator=(const my_unique_ptr<T>&) = delete;
+  my_unique_ptr(const my_unique_ptr<T> &) = delete;
+  void operator=(const my_unique_ptr<T> &) = delete;
 
-    T* get() { return ptr_; }
+  T *get() { return ptr_; }
 
-    T* release()
-    {
-        T* tmp = ptr_;
-        ptr_ = nullptr;
-        return tmp;
-    };
+  T *release() {
+    T *tmp = ptr_;
+    ptr_ = nullptr;
+    return tmp;
+  };
 
-    void reset(T* new_ptr = nullptr)
-    {
-        delete ptr_;
-        ptr_ = new_ptr;
-    };
+  void reset(T *new_ptr = nullptr) {
+    delete ptr_;
+    ptr_ = new_ptr;
+  };
 
-    ~my_unique_ptr() { delete ptr_; }
+  ~my_unique_ptr() { delete ptr_; }
 
-    T operator*() { return *ptr_; };
-    T* operator->() { return this->ptr_; };
+  T &operator*() { return *ptr_; };
+  T *operator->() { return this->ptr_; };
 
 private:
-    T* ptr_ = nullptr;
+  T *ptr_ = nullptr;
 };
