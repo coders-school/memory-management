2d4aaa41fd036edcd3f1712b189fdc9c9811bc01
damixiorek <damixiorek@gmail.com>
2020-10-29 21:16:18 +0100

    add tests with more arguments make unique

--- homework/unique_ptr/MakeUnique.ut.cpp
+++ homework/unique_ptr/MakeUnique.ut.cpp
@@ -5,6 +5,7 @@
 
 #include <cstddef>
 #include <string>
+#include <utility>
 
 SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
 {
@@ -20,6 +21,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
         auto uniqueIntLValue = MakeUnique<int>(firstNumber);
         auto uniqueTestingClassRValue = MakeUnique<TestingClass>("any text");
         auto uniqueTestingClassLValue = MakeUnique<TestingClass>(firstText);
+        auto uniquePointerForMoreArguments = MakeUnique<std::pair<std::string, int>>("r value text", firstNumber);
 
         WHEN("Using dereference")
         {
@@ -29,6 +31,8 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE(*uniqueIntLValue == firstNumber);
                 REQUIRE((*uniqueTestingClassRValue).testingGetter() == "any text");
                 REQUIRE((*uniqueTestingClassLValue).testingGetter() == firstText);
+                REQUIRE((*uniquePointerForMoreArguments).first == "r value text");
+                REQUIRE((*uniquePointerForMoreArguments).second == firstNumber);
             }
         }
 
@@ -38,6 +42,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
             UniquePointer<int> uniqueIntLValue2(std::move(uniqueIntLValue));
             UniquePointer<TestingClass> uniqueTestingClassRValue2(std::move(uniqueTestingClassRValue));
             UniquePointer<TestingClass> uniqueTestingClassLValue2(std::move(uniqueTestingClassLValue));
+            UniquePointer<std::pair<std::string, int>> uniquePointerForMoreArguments2(std::move(uniquePointerForMoreArguments));
 
             THEN("Should create unique pointer using move constructor and free memory")
             {
@@ -52,6 +57,10 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
 
                 REQUIRE(uniqueTestingClassLValue.get() == nullptr);
                 REQUIRE((*uniqueTestingClassLValue2).testingGetter() == firstText);
+
+                REQUIRE(uniquePointerForMoreArguments.get() == nullptr);
+                REQUIRE((*uniquePointerForMoreArguments2).first == "r value text");
+                REQUIRE((*uniquePointerForMoreArguments2).second == firstNumber);
             }
         }
 
@@ -61,11 +70,13 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
             UniquePointer<int> anyPtr2;
             UniquePointer<TestingClass> anyPtr3;
             UniquePointer<TestingClass> anyPtr4;
+            UniquePointer<std::pair<std::string, int>> anyPtr5;
 
             anyPtr = std::move(uniqueIntRValue);
             anyPtr2 = std::move(uniqueIntLValue);
             anyPtr3 = std::move(uniqueTestingClassRValue);
             anyPtr4 = std::move(uniqueTestingClassLValue);
+            anyPtr5 = std::move(uniquePointerForMoreArguments);
 
             THEN("Should move data to a new pointer using move assigment operator and free memory")
             {
@@ -78,6 +89,10 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE((*anyPtr3).testingGetter() == "any text");
                 REQUIRE(uniqueTestingClassLValue.get() == nullptr);
                 REQUIRE((*anyPtr4).testingGetter() == firstText);
+
+                REQUIRE(uniquePointerForMoreArguments.get() == nullptr);
+                REQUIRE((*uniquePointerForMoreArguments).first == "r value text");
+                REQUIRE((*uniquePointerForMoreArguments).second == firstNumber);
             }
         }
 
@@ -87,6 +102,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
             int* rawPtr2 = uniqueIntLValue.get();
             TestingClass* rawPtr3 = uniqueTestingClassRValue.get();
             TestingClass* rawPtr4 = uniqueTestingClassLValue.get();
+            std::pair<std::string, int>* rawPtr5 = uniquePointerForMoreArguments.get();
 
             THEN("Should return raw pointer")
             {
@@ -94,6 +110,8 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE(*rawPtr2 == firstNumber);
                 REQUIRE((*rawPtr3).testingGetter() == "any text");
                 REQUIRE((*rawPtr4).testingGetter() == firstText);
+                REQUIRE((*rawPtr5).first == "r value text");
+                REQUIRE((*rawPtr5).second == firstNumber);
             }
 
             THEN("Shouldnt move memory managment to a raw pointer, just create a raw pointer")
@@ -102,6 +120,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE_NOTHROW(*uniqueIntLValue);
                 REQUIRE_NOTHROW((*uniqueTestingClassRValue).testingGetter());
                 REQUIRE_NOTHROW((*uniqueTestingClassLValue).testingGetter());
+                REQUIRE_NOTHROW((*uniquePointerForMoreArguments).first);
             }
         }
 
@@ -111,6 +130,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
             int* rawPtr2 = uniqueIntLValue.release();
             TestingClass* rawPtr3 = uniqueTestingClassRValue.release();
             TestingClass* rawPtr4 = uniqueTestingClassLValue.release();
+            std::pair<std::string, int>* rawPtr5 = uniquePointerForMoreArguments.release();
 
             THEN("Should return raw pointer")
             {
@@ -118,6 +138,8 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE(*rawPtr2 == firstNumber);
                 REQUIRE((*rawPtr3).testingGetter() == "any text");
                 REQUIRE((*rawPtr4).testingGetter() == firstText);
+                REQUIRE((*rawPtr5).first == "r value text");
+                REQUIRE((*rawPtr5).second == firstNumber);
             }
 
             THEN("Should throw exception when trying dereference because memory managment was move to a raw pointer")
@@ -126,12 +148,15 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE_THROWS_AS(*uniqueIntLValue, DereferenceNullPtr);
                 REQUIRE_THROWS_AS((*uniqueTestingClassRValue).testingGetter(), DereferenceNullPtr);
                 REQUIRE_THROWS_AS((*uniqueTestingClassLValue).testingGetter(), DereferenceNullPtr);
+                REQUIRE_THROWS_AS((*uniquePointerForMoreArguments).first, DereferenceNullPtr);
+                REQUIRE_THROWS_AS((*uniquePointerForMoreArguments).second, DereferenceNullPtr);
             }
 
             delete rawPtr1;
             delete rawPtr2;
             delete rawPtr3;
             delete rawPtr4;
+            delete rawPtr5;
         }
 
         WHEN("Using UniquePointer::reset() with argument, with object created by MakeUnique")
@@ -140,6 +165,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
             uniqueIntLValue.reset(new int(secondNumber));
             uniqueTestingClassRValue.reset(new TestingClass("something new!"));
             uniqueTestingClassLValue.reset(new TestingClass(secondText));
+            uniquePointerForMoreArguments.reset(new std::pair<std::string,int>(secondText,30));
 
             THEN("Should create a new class object on a heap and free old one memory")
             {
@@ -147,6 +173,9 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE(*uniqueIntLValue == secondNumber);
                 REQUIRE((*uniqueTestingClassRValue).testingGetter() == "something new!");
                 REQUIRE((*uniqueTestingClassLValue).testingGetter() == secondText);
+                REQUIRE((*uniquePointerForMoreArguments).first == secondText);
+                REQUIRE((*uniquePointerForMoreArguments).second == 30);
+                
             }
 
             THEN("Shouldnt throw exception because its a new memory alocated instead old which is free")
@@ -155,6 +184,8 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE_NOTHROW(*uniqueIntLValue);
                 REQUIRE_NOTHROW((*uniqueTestingClassRValue).testingGetter());
                 REQUIRE_NOTHROW((*uniqueTestingClassLValue).testingGetter());
+                REQUIRE_NOTHROW((*uniquePointerForMoreArguments).first);
+                REQUIRE_NOTHROW((*uniquePointerForMoreArguments).second);
             }
         }
 
@@ -164,6 +195,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
             uniqueIntLValue.reset();
             uniqueTestingClassRValue.reset();
             uniqueTestingClassLValue.reset();
+            uniquePointerForMoreArguments.reset();
 
             THEN("Should free memory")
             {
@@ -171,6 +203,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE(uniqueIntLValue.get() == nullptr);
                 REQUIRE(uniqueTestingClassRValue.get() == nullptr);
                 REQUIRE(uniqueTestingClassLValue.get() == nullptr);
+                REQUIRE(uniquePointerForMoreArguments.get() == nullptr);
             }
 
             THEN("Should throw exception because memory is free and pointer is set as nullptr")
@@ -179,6 +212,7 @@ SCENARIO("Using make unique to initialize unique pointers", "[makeUniquePtr]")
                 REQUIRE_THROWS_AS(*uniqueIntLValue, DereferenceNullPtr);
                 REQUIRE_THROWS_AS((*uniqueTestingClassRValue).testingGetter(), DereferenceNullPtr);
                 REQUIRE_THROWS_AS((*uniqueTestingClassLValue).testingGetter(), DereferenceNullPtr);
+                REQUIRE_THROWS_AS((*uniquePointerForMoreArguments).first, DereferenceNullPtr);
             }
         }
     }
