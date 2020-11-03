9649a067351a6d653cdc66bfc99a4cc8332c838b
Marcinati <marcin.kapusta.pk@gmail.com>
2020-11-03 11:43:49 +0100

    operator =

--- homework/unique_ptr/UniquePtr.hpp
+++ homework/unique_ptr/UniquePtr.hpp
@@ -1,24 +1,37 @@
 #pragma once
 
 template <typename T>
-class UniquePtr {
+class UniquePtr
+{
 public:
     UniquePtr() = default;
     UniquePtr(T * rawPtr);
     UniquePtr(UniquePtr<T> && otherPtr) noexcept;
-   
+
     UniquePtr(const UniquePtr<T> &) = delete;
-    UniquePtr<T> &operator = (const UniquePtr<T> &) = delete;
-    
+    UniquePtr<T> & operator=(const UniquePtr<T> &) = delete;
+
+    UniquePtr<T> & operator=(UniquePtr<T> && otherUniquePtr) noexcept;
+
 private:
-    T * rawPtr_ = nullptr;
+    T *rawPtr_ = nullptr;
 };
 
 template <typename T>
 UniquePtr<T>::UniquePtr(T * rawPtr) : rawPtr_(rawPtr) {}
 
 template <typename T>
-UniquePtr<T>::UniquePtr(UniquePtr<T> && otherPtr) noexcept 
-    : rawPtr_(other.rawPtr_) {
+UniquePtr<T>::UniquePtr(UniquePtr<T> && otherPtr) noexcept
+    : rawPtr_(otherPtr.rawPtr_) {
         other.rawPtr_ = nullptr;
     }
+
+template <typename T>
+UniquePtr<T> & UniquePtr<T>::operator=(UniquePtr<T> && otherUniquePtr) noexcept {
+    if (this != & otherUniquePtr) {
+        delete rawPtr_;
+        rawPtr_ = otherUniquePtr.rawPtr_;
+        otherUniquePtr = nullptr;
+    }
+    return * this;
+}
