50c6fd965ef9516a5f9debd2871fd2ecd2f0c97e
Marcinati <marcin.kapusta.pk@gmail.com>
2020-11-03 12:10:35 +0100

    d-tor

--- homework/unique_ptr/UniquePtr.hpp
+++ homework/unique_ptr/UniquePtr.hpp
@@ -7,6 +7,7 @@ public:
     UniquePtr() = default;
     UniquePtr(T * rawPtr);
     UniquePtr(UniquePtr<T> && otherPtr) noexcept;
+    ~UniquePtr();
 
     UniquePtr(const UniquePtr<T> &) = delete;
     UniquePtr<T> & operator=(const UniquePtr<T> &) = delete;
@@ -14,7 +15,7 @@ public:
     UniquePtr<T> & operator=(UniquePtr<T> && otherUniquePtr) noexcept;
 
 private:
-    T *rawPtr_ = nullptr;
+    T * rawPtr_ = nullptr;
 };
 
 template <typename T>
@@ -25,6 +26,11 @@ UniquePtr<T>::UniquePtr(UniquePtr<T> && otherPtr) noexcept
     : rawPtr_(otherPtr.rawPtr_) {
         otherPtr.rawPtr_ = nullptr;
     }
+
+template <typename T>
+UniquePtr<T>::~UniquePtr() {
+  delete rawPtr_;
+}
 
 template <typename T>
 UniquePtr<T> & UniquePtr<T>::operator=(UniquePtr<T> && otherUniquePtr) noexcept {
