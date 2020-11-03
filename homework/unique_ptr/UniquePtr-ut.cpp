1da826583b741d00e5f82c6cb3a4f9305a357824
Marcinati <marcin.kapusta.pk@gmail.com>
2020-11-03 13:25:31 +0100

    test preparation

--- homework/unique_ptr/UniquePtr-ut.cpp
+++ homework/unique_ptr/UniquePtr-ut.cpp
@@ -1,2 +1,20 @@
+#include "Exceptions.hpp"
 #include "gtest/gtest.h"
 #include "UniquePtr.hpp"
+
+class Velocity {
+    Velocity(int distance, int time) : distance_(distance), time_(time) {}
+
+    int distance_ = 0; //in km
+    int time_ = 0;     //in h
+    int countVelocity() { return distance_ / time_; }
+}
+
+constexpr int testDistance = 50;
+constexpr int testTime = 1;
+
+class UniquePtrTest : public ::testing::Test {
+protected:
+    UniquePtrTest() : uniqueIntPtr(new int{testDistance}) {}
+    UniquePtr<int> uniqueIntPtr;
+};
