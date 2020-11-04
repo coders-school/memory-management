22db416919bc60eba5dd2c05ec73b42e924def06
Marcinati <marcin.kapusta.pk@gmail.com>
2020-11-04 20:23:09 +0100

    :recycle: refactoring

--- homework/unique_ptr/UniquePtr-ut.cpp
+++ homework/unique_ptr/UniquePtr-ut.cpp
@@ -6,34 +6,53 @@ constexpr int testDistance = 50;
 constexpr int newTestDistance = 100;
 
 class Velocity {
+public:
     Velocity(int distance, int time) : distance_(distance), time_(time) {}
+    int countVelocity() { return distance_ / time_; }
 
+protected:
     int distance_ = 0; //in km
     int time_ = 1;     //in h
-    int countVelocity() { return distance_ / time_; }
 };
 
 class UniquePointerTest : public ::testing::Test {
 protected:
-    UniquePointerTest() : uniqueIntPtr(new int{testDistance}) {}
-    UniquePtr<int> uniqueIntPtr;
+    UniquePointerTest() : uniquePtr(new int{testDistance}) {}
+    UniquePtr<int> uniquePtr;
 };
 
 TEST_F(UniquePointerTest, ShouldDereferencePointerWhichIsNotANullptr) {
-    ASSERT_EQ(*uniqueIntPtr, testDistance);
+    ASSERT_EQ(*uniquePtr, testDistance);
 }
 
 TEST_F(UniquePointerTest, ShouldUseReset) {
-    uniqueIntPtr.reset(new int{newTestDistance});
-    ASSERT_EQ(*uniqueIntPtr, newTestDistance);
+    uniquePtr.reset(new int{newTestDistance});
+    ASSERT_EQ(*uniquePtr, newTestDistance);
 }
 
 TEST_F(UniquePointerTest, ShouldNotDereferenceANullptr) {
-    uniqueIntPtr.reset(nullptr);
-    ASSERT_THROW(*uniqueIntPtr, InvalidDereference);
+    uniquePtr.reset(nullptr);
+    ASSERT_THROW(*uniquePtr, InvalidDereference);
 }
 
 TEST_F(UniquePointerTest, ShouldDereferenceAndChangeValue) {
-    *uniqueIntPtr = newTestDistance;
-    ASSERT_EQ(*uniqueIntPtr, newTestDistance);
+    *uniquePtr = newTestDistance;
+    ASSERT_EQ(*uniquePtr, newTestDistance);
+}
+
+TEST_F(UniquePointerTest, ShouldUseRelease) {
+    auto releasedPtr = uniquePtr.release();
+    ASSERT_EQ(uniquePtr.get(), nullptr);
+}
+
+TEST_F(UniquePointerTest, ShouldGetAPointer) {
+    auto rawPtr = uniquePtr.get();
+    ASSERT_EQ(rawPtr, uniquePtr.get());
+}
+
+TEST_F(UniquePointerTest, ShouldCallMethodByArrowOperator) {
+    int otherDistance = 30;
+    int otherTime = 2;
+    UniquePtr<Velocity> VelocityPtr = new Velocity(otherDistance, otherTime);
+    ASSERT_EQ(VelocityPtr->countVelocity(), otherDistance / otherTime);
 }
