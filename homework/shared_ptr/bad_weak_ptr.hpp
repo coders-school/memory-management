14d85cec509d6c2c23f2f6dabfc00486df6bcd77
WojtekMs <wojtek.atmo@gmail.com>
2020-11-05 22:14:35 +0100

    what override removed from bad_weak_ptr

--- homework/shared_ptr/bad_weak_ptr.hpp
+++ homework/shared_ptr/bad_weak_ptr.hpp
@@ -3,12 +3,7 @@
 
 namespace cs {
     class bad_weak_ptr :public std::exception {
-        std::string message{"weak ptr is expired!\n"};
         public:
         bad_weak_ptr() = default;
-        explicit bad_weak_ptr(const std::string& what) :message(what) {}
-        const char* what() const noexcept override {
-            return message.c_str();
-        }
     };
 };
