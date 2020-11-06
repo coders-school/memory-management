1a8b799bb65ce8e930a802b59cf70edfe7fc4a4a
Andrzej Rafalski <anraf1001@gmail.com>
2020-11-06 22:13:04 +0100

    Make_shared tests - use_count

--- homework/shared_ptr/make_shared-ut.cpp
+++ homework/shared_ptr/make_shared-ut.cpp
@@ -21,8 +21,11 @@ struct B {
 
 TEST(MakeSharedTest, shouldCreateSharedPtrUsingMakeShared) {
     constexpr int firstValue = 1;
-    constexpr double secondValue = 2;
+    constexpr double secondValue = 2.3;
     constexpr int thirdValue = 3;
+
+    constexpr int numOfPtrsAfterCopy = 2;
+    constexpr int numOfPtrsAfterDelete = 1;
 
     cs::shared_ptr<A> ptr = cs::make_shared<A>(firstValue, secondValue, thirdValue);
 
@@ -30,8 +33,18 @@ TEST(MakeSharedTest, shouldCreateSharedPtrUsingMakeShared) {
     ASSERT_EQ(ptr->b_, secondValue);
     ASSERT_EQ(ptr->c_, thirdValue);
 
+    auto ptr_copy = ptr;
+    ASSERT_EQ(ptr.use_count(), numOfPtrsAfterCopy);
+    ptr_copy.reset();
+    ASSERT_EQ(ptr.use_count(), numOfPtrsAfterDelete);
+
     cs::shared_ptr<B> ptr2 = cs::make_shared<B>(firstValue, secondValue);
 
     ASSERT_EQ(ptr2->a_, firstValue);
     ASSERT_EQ(ptr2->b_, secondValue);
+
+    auto ptr2_copy = ptr;
+    ASSERT_EQ(ptr.use_count(), numOfPtrsAfterCopy);
+    ptr2_copy.reset();
+    ASSERT_EQ(ptr.use_count(), numOfPtrsAfterDelete);
 }
