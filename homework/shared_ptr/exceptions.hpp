3721428688b18999145823398bc93acf238aa255
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-11-08 13:17:11 +0100

    remove virtual destrutor and what() method

--- homework/shared_ptr/exceptions.hpp
+++ homework/shared_ptr/exceptions.hpp
@@ -9,10 +9,6 @@ public:
         : msg_(message) {}
     explicit ExpiredWeakPtr(const std::string& message)
         : msg_(message) {}
-    virtual ~ExpiredWeakPtr() noexcept {}
-    virtual const char* what() const noexcept {
-        return msg_.c_str();
-    }
 
 protected:
     std::string msg_;
