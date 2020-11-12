593cdb0b0df49e19a383dca8ea514686e9e60240
Karol Dulemba <kdulemba@gmail.com>
2020-11-12 20:49:19 +0100

    shared ready, weak still with problems

--- homework/shared_ptr/SharedPtr.hpp
+++ homework/shared_ptr/SharedPtr.hpp
@@ -3,149 +3,124 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
-class ControlBlock
-{
+template <typename T> class ControlBlock {
 public:
-    std::atomic<size_t> sharedRefsCounter_{1};
-    std::atomic<size_t> weakRefsCounter_{0};
-    void deleter(T *rawPtr) { delete rawPtr; };
+  std::atomic<size_t> sharedRefsCounter_{1};
+  std::atomic<size_t> weakRefsCounter_{0};
+  void deleter(T *rawPtr) {
+    if (rawPtr != nullptr) {
+      delete rawPtr;
+    }
+  };
 };
 
-template <typename T>
-class SharedPtr
-{
+template <typename T> class SharedPtr {
 public:
-    SharedPtr(T *rawPtr = nullptr);
-    SharedPtr(SharedPtr<T> &&other) noexcept; // move constructor
-    SharedPtr(const SharedPtr<T> &other);     // copy constructor
-    ~SharedPtr();
-    T *get() const;
-    T &operator*() const noexcept;
-    T *operator->() const noexcept;
-    SharedPtr<T> &operator=(const SharedPtr<T> &other);
-    SharedPtr<T> &operator=(SharedPtr<T> &&other) noexcept;
-    operator bool() const noexcept;
-    void reset(T *NewRawPtr = nullptr);
-    size_t use_count() const;
+  SharedPtr(T *rawPtr = nullptr);
+  SharedPtr(SharedPtr<T> &&other) noexcept; // move constructor
+  SharedPtr(const SharedPtr<T> &other);     // copy constructor
+  ~SharedPtr();
+  T *get() const;
+  T &operator*() const noexcept;
+  T *operator->() const noexcept;
+  SharedPtr<T> &operator=(const SharedPtr<T> &other);
+  SharedPtr<T> &operator=(SharedPtr<T> &&other);
+  operator bool() const noexcept;
+  void reset(T *NewRawPtr = nullptr);
+  size_t use_count() const;
 
 private:
-    T *rawPtr_;
-    ControlBlock<T> *controlBlock_;
+  T *rawPtr_;
+  ControlBlock<T> *controlBlock_;
+  void deleteAllIfCountersZero();
 };
 
 template <typename T>
 SharedPtr<T>::SharedPtr(T *rawPtr)
-    : rawPtr_(rawPtr),
-      controlBlock_(new ControlBlock<T>) {}
+    : rawPtr_(rawPtr), controlBlock_(new ControlBlock<T>{}) {}
 
 template <typename T>
 SharedPtr<T>::SharedPtr(SharedPtr<T> &&other) noexcept // move constructor
-    : rawPtr_(other.rawPtr_), controlBlock_(other.controlBlock_)
-{
-    other.rawPtr_ = nullptr;
-    other.controlBlock_ = nullptr;
+    : rawPtr_(other.rawPtr_), controlBlock_(other.controlBlock_) {
+  other.rawPtr_ = nullptr;
+  other.controlBlock_ = nullptr;
 }
 
 template <typename T>
 SharedPtr<T>::SharedPtr(const SharedPtr<T> &other) // copy constructor
-    : rawPtr_(other.rawPtr_), controlBlock_(other.controlBlock_)
-{
-    controlBlock_->sharedRefsCounter_++;
+    : rawPtr_(other.rawPtr_), controlBlock_(other.controlBlock_) {
+  controlBlock_->sharedRefsCounter_++;
 }
 
-template <typename T>
-SharedPtr<T>::~SharedPtr()
-{
-    if (controlBlock_ != nullptr)
-    {
-        controlBlock_->sharedRefsCounter_--;
-        if (controlBlock_->sharedRefsCounter_ == 0 && controlBlock_->weakRefsCounter_ == 0)
-        {
-            controlBlock_->deleter(rawPtr_);
-            delete controlBlock_;
-        }
-    }
-}
-
-template <typename T>
-T *SharedPtr<T>::get() const
-{
-    return rawPtr_;
-}
-
-template <typename T>
-T &SharedPtr<T>::operator*() const noexcept
-{
-    return *rawPtr_;
-}
-
-template <typename T>
-T *SharedPtr<T>::operator->() const noexcept
-{
-    return rawPtr_;
-}
-
-template <typename T>
-SharedPtr<T> &SharedPtr<T>::operator=(const SharedPtr<T> &other)
-{
-    if (other.rawPtr_ != rawPtr_)
-    {
-        controlBlock_->sharedRefsCounter_--;
-
-        if (controlBlock_->sharedRefsCounter_ == 0 && controlBlock_->weakRefsCounter_ == 0)
-        {
-            controlBlock_->deleter(rawPtr_);
-            delete controlBlock_;
-            rawPtr_ = other.rawPtr_;
-            controlBlock_ = other.controlBlock_;
-            controlBlock_->sharedRefsCounter_++;
-        }
-    }
-    return *this;
-}
-
-template <typename T>
-SharedPtr<T> &SharedPtr<T>::operator=(SharedPtr<T> &&other) noexcept
-{
+template <typename T> SharedPtr<T>::~SharedPtr() {
+  if (controlBlock_ != nullptr) {
     controlBlock_->sharedRefsCounter_--;
+    deleteAllIfCountersZero();
+  }
+}
 
-    if (controlBlock_->sharedRefsCounter_ == 0 && controlBlock_->weakRefsCounter_ == 0)
-    {
-        controlBlock_->deleter(rawPtr_);
-        delete controlBlock_;
-        rawPtr_ = other.rawPtr_;
-        other.rawPtr_ = nullptr;
-        other.controlBlock_ = nullptr;
+template <typename T> void SharedPtr<T>::deleteAllIfCountersZero() {
+  if (controlBlock_->sharedRefsCounter_ == 0) {
+    controlBlock_->deleter(rawPtr_);
+    rawPtr_ = nullptr;
+  }
+  if (controlBlock_->weakRefsCounter_ == 0 &&
+      controlBlock_->sharedRefsCounter_ == 0) {
+    delete controlBlock_;
+    controlBlock_ = nullptr;
+  }
+}
+
+template <typename T> T *SharedPtr<T>::get() const { return rawPtr_; }
+
+template <typename T> T &SharedPtr<T>::operator*() const noexcept {
+  return *rawPtr_;
+}
+
+template <typename T> T *SharedPtr<T>::operator->() const noexcept {
+  return rawPtr_;
+}
+
+template <typename T>
+SharedPtr<T> &SharedPtr<T>::operator=(const SharedPtr<T> &other) {
+  if (this != &other) {
+    controlBlock_->sharedRefsCounter_--;
+      deleteAllIfCountersZero();
+      rawPtr_ = other.rawPtr_;
+      controlBlock_ = other.controlBlock_;
+      controlBlock_->sharedRefsCounter_++;
     }
-    return *this;
+  return *this;
 }
 
 template <typename T>
-SharedPtr<T>::operator bool() const noexcept
-{
-    return (rawPtr_ != nullptr);
+SharedPtr<T> &SharedPtr<T>::operator=(SharedPtr<T> &&other) {
+
+  if (other.rawPtr_ != rawPtr_) {
+    controlBlock_->sharedRefsCounter_--;
+      deleteAllIfCountersZero();
+      rawPtr_ = other.rawPtr_;
+      controlBlock_ = other.controlBlock_;
+      other.rawPtr_ = nullptr;
+      other.controlBlock_ = nullptr;
+  }
+  return *this;
 }
 
-template <typename T>
-void SharedPtr<T>::reset(T *NewRawPtr)
-{
-    if (NewRawPtr != rawPtr_)
-    {
-        controlBlock_->sharedRefsCounter_--;
+template <typename T> SharedPtr<T>::operator bool() const noexcept {
+  return (rawPtr_ != nullptr);
+}
 
-        if (controlBlock_->sharedRefsCounter_ == 0 && controlBlock_->weakRefsCounter_ == 0)
-        {
-            controlBlock_->deleter(rawPtr_);
-            delete controlBlock_;
-            rawPtr_ = NewRawPtr;
-            controlBlock_ = new ControlBlock<T>{};
-        }
+template <typename T> void SharedPtr<T>::reset(T *NewRawPtr) {
+    
+    controlBlock_->sharedRefsCounter_--;
+    deleteAllIfCountersZero();
+    if(controlBlock_ == nullptr){
+    controlBlock_ = new ControlBlock<T>;
     }
+    rawPtr_ = NewRawPtr;
 }
 
-template <typename T>
-size_t SharedPtr<T>::use_count() const
-{
-    return controlBlock_->sharedRefsCounter_;
+template <typename T> size_t SharedPtr<T>::use_count() const {
+  return controlBlock_->sharedRefsCounter_;
 }
--- homework/shared_ptr/tests/tests.cpp
+++ homework/shared_ptr/tests/tests.cpp
@@ -1,192 +1,130 @@
 
-#include <vector>
 #include "../Scooter.hpp"
 #include "../SharedPtr.hpp"
 #include "catch.hpp"
+#include <vector>
 
-SCENARIO("Testing all SharedPtr functions")
-{
+SCENARIO("Testing all SharedPtr functions") {
 
-    GIVEN("SharedPtr with int = 200")
-    {
-        int *power = new int{200};
-        SharedPtr<int> SharedPower(power);
-        WHEN("using get() to get int value")
-        {
-            auto result = SharedPower.get();
-            THEN("int given is equal to int from get()")
-            {
-                REQUIRE(result == power);
-            }
-        }
+  GIVEN("SharedPtr with int = 200") {
+    int *power = new int{200};
+    SharedPtr<int> SharedPower(power);
+
+    WHEN("using get() to get int value") {
+      auto result = SharedPower.get();
+      THEN("int given is equal to int from get()") { REQUIRE(result == power); }
     }
 
-    GIVEN("SharedPtr with int = 200")
-    {
-        int *power = new int{200};
-        SharedPtr<int> SharedPower(power);
-        WHEN("Using move constructor from SharedPtr")
-        {
-            SharedPtr<int> NewSharedPower(std::move(SharedPower));
-            THEN("New Shared has value from old one, old one is nullptr")
-            {
-                REQUIRE(*(NewSharedPower.get()) == *power);
-                REQUIRE(SharedPower.get() == nullptr);
-            }
-        }
+    WHEN("Using move constructor from SharedPtr") {
+      REQUIRE(SharedPower.use_count() == 1);
+      SharedPtr<int> NewSharedPower(std::move(SharedPower));
+      THEN("New Shared has value from old one, old one is nullptr") {
+        REQUIRE(*(NewSharedPower.get()) == *power);
+        REQUIRE(SharedPower.get() == nullptr);
+        REQUIRE(NewSharedPower.use_count() == 1);
+      }
     }
 
-    GIVEN("SharedPtr with int = 200")
-    {
-        int *power = new int{200};
-        SharedPtr<int> SharedPower(power);
-        WHEN("Using copy constructor from SharedPtr")
-        {
-            SharedPtr<int> NewSharedPower(SharedPower);
-            THEN("New Shared has value from old one, old one is intact, use count = 2")
-            {
-                REQUIRE(*(NewSharedPower.get()) == *power);
-                REQUIRE(*(SharedPower.get()) == *power);
-                REQUIRE(SharedPower.use_count() == 2);
-            }
-        }
+    WHEN("Using copy constructor from SharedPtr") {
+      SharedPtr<int> NewSharedPower(SharedPower);
+      THEN("New Shared has value from old one, old one is intact, use count = "
+           "2") {
+        REQUIRE(*(NewSharedPower.get()) == *power);
+        REQUIRE(*(SharedPower.get()) == *power);
+        REQUIRE(SharedPower.use_count() == 2);
+      }
     }
 
-    GIVEN("SharedPtr with int = 200")
-    {
-        int *power = new int{200};
-        SharedPtr<int> SharedPower(power);
-        WHEN(
-            "dereferencing SharedPtr with "
-            "*"
-            " to get member power_")
-        {
-            auto result = *SharedPower;
-            THEN("value inside SharedPtr is same value as passed")
-            {
-                REQUIRE(result == *power);
-            }
-        }
+    WHEN("dereferencing SharedPtr with "
+         "*"
+         " to get member power_") {
+      auto result = *SharedPower;
+      THEN("value inside SharedPtr is same value as passed") {
+        REQUIRE(result == *power);
+      }
     }
+  }
 
-    GIVEN("SharedPtr with Scooter(power) where power = 100")
-    {
-        int power{100};
-        SharedPtr<Scooter> scooter(new Scooter(power));
-        WHEN(
-            "dereferencing SharedPtr with "
-            "->"
-            " to get member power_")
-        {
-            auto result = scooter->power_;
-            THEN("value inside SharedPtr is same value as passed")
-            {
-                REQUIRE(result == power);
-            }
-        }
+  GIVEN("SharedPtr with Scooter(power) where power = 100") {
+    int power{100};
+    SharedPtr<Scooter> scooter(new Scooter(power));
+    WHEN("dereferencing SharedPtr with "
+         "->"
+         " to get member power_") {
+      auto result = scooter->power_;
+      THEN("value inside SharedPtr is same value as passed") {
+        REQUIRE(result == power);
+      }
     }
+  }
 
-    GIVEN("Two SharedPtr with different int=100 and int=200")
-    {
-        int *power1 = new int{100};
-        SharedPtr<int> SharedPower1(power1);
-        int *power2 = new int{200};
-        SharedPtr<int> SharedPower2(power2);
+  GIVEN("Two SharedPtr with different int=100 and int=200") {
+    int *power1 = new int{100};
+    SharedPtr<int> SharedPower1(power1);
+    int *power2 = new int{200};
+    SharedPtr<int> SharedPower2(power2);
 
-        WHEN("using copy assigment operator from SharedPtr")
-        {
-            SharedPower1 = SharedPower2;
-            THEN("Both SharedPtr has int=200, and count() = 2")
-            {
-                REQUIRE(*(SharedPower1.get()) == *power2);
-                REQUIRE(*(SharedPower2.get()) == *power2);
-                REQUIRE(SharedPower1.use_count() == 2);
-            }
-        }
+    WHEN("using copy assignment operator from SharedPtr") {
+      SharedPower1 = SharedPower2;
+      THEN("Both SharedPtr has int=200, and count() = 2") {
+        REQUIRE(*(SharedPower1.get()) == *power2);
+        REQUIRE(SharedPower1.use_count() == 2);
+      }
     }
+  }
 
-    GIVEN("Empty SharedPtr")
-    {
-        SharedPtr<int> scooter(nullptr);
-        WHEN("Get is returning nullptr")
-        {
-            auto result = scooter.get();
-            THEN("empty SharedPtr is returning nullptr through get()")
-            {
-                REQUIRE(result == nullptr);
-            }
-        }
+  GIVEN("Empty SharedPtr") {
+    SharedPtr<int> Number(nullptr);
+    WHEN("Get is returning nullptr") {
+      auto result = Number.get();
+      THEN("empty SharedPtr is returning nullptr through get()") {
+        REQUIRE(result == nullptr);
+      }
     }
+  }
 
-    GIVEN("SharedPtr with int = 100")
-    {
-        int power1{100};
-        int power2{200};
-        SharedPtr<int> SharedPower1(new int(power1));
-        WHEN("resetting scooter with TurboScooter")
-        {
-            int *SharedPower2 = new int(power2);
-            SharedPower1.reset(SharedPower2);
+  GIVEN("SharedPtr with Scooter(insertValue) where power = 100") {
+    int power{100};
+    SharedPtr<int> scooter(new int(power));
+    WHEN("Assigning poiter to bool variable") {
+      bool result = scooter;
+      THEN("When not empty, gives true") { REQUIRE(result); }
+    }
+  }
+  GIVEN("Empty SharedPtr scooter") {
+    SharedPtr<Scooter> scooter(nullptr);
+    WHEN("Assigning a pointer to bool variable") {
+      bool result = scooter;
+      THEN("When not empty, gives true") { REQUIRE_FALSE(result); }
+    }
+  }
 
-            THEN("ControlBlock Shared Counter is still = 1")
-            {
-                REQUIRE(*(SharedPower1.get()) == power2);
-                REQUIRE(SharedPower1.use_count() == 1);
-            }
-        }
+  GIVEN("Two Shared Ptr, one empty, one not") {
+    int power{100};
+    SharedPtr<int> OldSharedPtr(new int(power));
+    SharedPtr<int> NewSharedPtr(nullptr);
+    REQUIRE(OldSharedPtr.use_count() == 1);
+    REQUIRE(NewSharedPtr.use_count() == 1);
+    WHEN("Using move assignment operator from SharedPtr") {
+      NewSharedPtr = std::move(OldSharedPtr);
+      THEN("Switched ownership from OldSharedPtr to NewSharedPtr") {
+        REQUIRE(NewSharedPtr.use_count() == 1);
+        REQUIRE(*(NewSharedPtr.get()) == power);
+        REQUIRE(OldSharedPtr.get() == nullptr);
+      }
     }
-    
-    GIVEN("SharedPtr with Scooter(insertValue) where power = 100")
-    {
-        int power{100};
-        SharedPtr<Scooter> scooter(new Scooter(power));
-        bool result;
-        WHEN("Assigning poiter to bool variable")
-        {
-            result = scooter;
-            THEN("When not empty, gives true") { REQUIRE(result); }
-        }
-    }
-    GIVEN("Empty SharedPtr scooter")
-    {
-        SharedPtr<Scooter> scooter(nullptr);
-        bool result;
-        WHEN("Assigning poiter to bool variable")
-        {
-            result = scooter;
-            THEN("When not empty, gives true") { REQUIRE_FALSE(result); }
-        }
-    }
+  }
 
-    GIVEN("Two Shared Ptr, one empty, one not")
-    {
-        int power{100};
-        SharedPtr<Scooter> scooter(new Scooter(power));
-        SharedPtr<Scooter> NewScooter(nullptr);
-        WHEN("Using move assignment operator from SharedPtr")
-        {
-            NewScooter = std::move(scooter);
-            THEN("ControlBlock Shared Counter is incremented to 2")
-            {
-                REQUIRE(NewScooter.get()->power_ == power);
-                REQUIRE(scooter.get() == nullptr);
-            }
-        }
+  GIVEN("One SharedPtr and one RawPtr") {
+    int power1{100};
+    int power2{200};
+    SharedPtr<int> SharedOne(new int(power1));
+    int *Power2Ptr = new int(power2);
+    WHEN("Using reset to insert NewScooter") {
+      SharedOne.reset(Power2Ptr);
+      THEN("scooter has power of NewScooter") {
+        REQUIRE(*(SharedOne.get()) == power2);
+      }
     }
-
-    GIVEN("One SharedPtr and one RawPtr")
-    {
-        int power1{100};
-        int power2{200};
-        SharedPtr<Scooter> scooter(new Scooter(power1));
-        Scooter *NewScooter = new Scooter(power2);
-        WHEN("Using reset to insert NewScooter")
-        {
-            scooter.reset(NewScooter);
-            THEN("scooter has power of NewScooter")
-            {
-                REQUIRE(scooter.get()->power_ == power2);
-            }
-        }
-    }
+  }
 }
