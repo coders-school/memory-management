a4be3110f0ba6cfeb01020ce4d3fe80b642e0a16
Łukasz Ziobroń <lukasz@coders.school>
2021-09-20 12:15:35 +0200

    workflow added

---
+++ .github/scripts/check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "$0" )" &> /dev/null && pwd )"
+source ${SCRIPT_DIR}/functions.sh
+
+FILE=$1
+fileExist ${FILE}
+
+echo "🔎 Performing checks"
+echo
+pattern ${FILE} "Looking for LEAK SUMMARY" "LEAK SUMMARY:"
+pattern ${FILE} "Looking for definitely lost section" "definitely lost: 1 bytes in 1 blocks"
+pattern ${FILE} "Looking for the allocation place" "main \(resourceD.cpp:30\)"
+pattern ${FILE} "Looking for the proper command" "valgrind\s+.*\s*./resourceD\s+d"
+echo
+
+summary
