1c400e33ffc96f6202bfe195f933f867bceff77f
lukasz-kukulka <qqla83@gmail.com>
2021-09-27 05:10:21 +0100

    Homework memory leek

--- homework/resourceD/resourceD.cpp
+++ homework/resourceD/resourceD.cpp
@@ -29,13 +29,12 @@ int main(int argc, char* argv[])
     {
         rsc = new Resource();
         rsc->use(N);
-        
+        delete rsc;
     }
     catch (logic_error & e)
     {
         cout << e.what() << endl;
     }
-    delete rsc;
     return 0;
 }
 
