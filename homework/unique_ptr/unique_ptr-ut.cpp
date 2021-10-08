bd69c0c16da5a66814acf259e0ccb3ddcbd28dd1
aniolm <maciej.aniol.programista@gmail.comgit config --global user.name Your>
2021-10-08 20:47:47 +0300

    Unit tests added

--- homework/unique_ptr/unique_ptr-ut.cpp
+++ homework/unique_ptr/unique_ptr-ut.cpp
@@ -1,9 +1,87 @@
 #include <gtest/gtest.h>
+#include "unique_ptr.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(UniquePointerTest, ObjectInitialization) {
+  
+  //arrange
+  ma::unique_ptr<int> ptr{new int{10}};
+
+  //act
+
+  //assert
+  EXPECT_EQ(*ptr, 10);
+}
+
+TEST(UniquePointerTest, ObjectValueModification) {
+  
+  //arrange
+  ma::unique_ptr<int> ptr{new int{10}};
+
+  //act
+  *ptr = 5;
+
+  //assert
+  EXPECT_EQ(*ptr, 5);
+}
+
+TEST(UniquePointerTest, MoveCtor) {
+  
+  //arrange
+  ma::unique_ptr<int> ptr{new int{10}};
+
+  //act
+  auto ptr2 = std::move(ptr);
+
+  //assert
+  EXPECT_EQ(*ptr2, 10);
+}
+
+TEST(UniquePointerTest, MoveAssignmentOperator) {
+  
+  //arrange
+  ma::unique_ptr<int> ptr{new int{10}};
+  ma::unique_ptr<int> ptr2{new int{5}};
+  //act
+  ptr2 = std::move(ptr);
+
+  //assert
+  EXPECT_EQ(*ptr2, 10);
+  EXPECT_EQ(ptr.get(), nullptr);
+}
+
+TEST(UniquePointerTest, PtrGet) {
+
+  //arrange
+  ma::unique_ptr<int> ptr{new int{10}};
+
+  //act
+  auto * ptr2 = ptr.get(); 
+
+  //assert
+ EXPECT_NE(ptr2, nullptr);
+}
+
+TEST(UniquePointerTest, PtrReset) {
+
+  //arrange
+  ma::unique_ptr<int> ptr{new int{10}};
+
+  //act
+  ptr.reset(new int{5}); 
+
+  //assert
+ EXPECT_EQ(*ptr, 5);
+}
+
+TEST(UniquePointerTest, PtrRelease) {
+
+  //arrange
+  ma::unique_ptr<int> ptr{new int{10}};
+
+  //act
+  auto * ptr2 = ptr.release(); 
+
+  //assert
+ EXPECT_EQ(*ptr2, 10);
+ EXPECT_EQ(ptr.get(), nullptr);
 }
--- homework/unique_ptr/unique_ptr.hpp
+++ homework/unique_ptr/unique_ptr.hpp
@@ -4,23 +4,29 @@
 #include <iostream>
 #include <memory>
 
-namespace ma{ 
+namespace ma {
 
-template<typename T>
+template <typename T>
 class unique_ptr {
-
 private:
-    T *rawPointer_;
+    T* rawPointer_;
 
 public:
-    explicit unique_ptr() : rawPointer_(nullptr) {}
+    explicit unique_ptr()
+        : rawPointer_(nullptr) {}
 
-    explicit unique_ptr(T *rawPointer) : rawPointer_(rawPointer) {}
+    explicit unique_ptr(T* rawPointer)
+        : rawPointer_(rawPointer) {}
 
-    ~unique_ptr() { if (rawPointer_) delete rawPointer_; }
+    ~unique_ptr() {
+        if (rawPointer_)
+            delete rawPointer_;
+    }
 
     //move constructor
-    unique_ptr(unique_ptr&& sourceUniquePointer) { rawPointer_ = sourceUniquePointer.release(); }
+    unique_ptr(unique_ptr&& sourceUniquePointer) { 
+        rawPointer_ = sourceUniquePointer.release();
+    }
 
     //copy constructor
     unique_ptr(const unique_ptr& sourceUniquePointer) = delete;
@@ -30,7 +36,7 @@ public:
 
     //move assignment operator
     unique_ptr& operator=(unique_ptr&& sourceUniquePointer) {
-        if(this != &sourceUniquePointer) {
+        if (this != &sourceUniquePointer) {
             delete rawPointer_;
             rawPointer_ = sourceUniquePointer.rawPointer_;
             sourceUniquePointer.rawPointer_ = nullptr;
@@ -45,18 +51,17 @@ public:
     T* get() { return rawPointer_; }
 
     T* release() {
-        T *tempPointer = rawPointer_;
+        T* tempPointer = rawPointer_;
         rawPointer_ = nullptr;
         return tempPointer;
     }
 
-    void reset(T *newPointer = nullptr) {
+    void reset(T* newPointer = nullptr) {
         delete rawPointer_;
         rawPointer_ = newPointer;
     }
-
 };
 
-} // namespace ma
+}  // namespace ma
 
 #endif
