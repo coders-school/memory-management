91e14251ab4fd9be9733d1a279d29dcfe2e97861
Wojtek <plociennik.wojtek@gmail.com>
2021-10-19 09:29:07 +0200

    Implement move constructor and function realse

--- homework/unique_ptr/uniqe_ptr.hpp
+++ homework/unique_ptr/uniqe_ptr.hpp
@@ -1,23 +1,34 @@
-#pragma once 
+#pragma once
 
+#include <cstddef>
 
 template <typename T>
-class uniqe_ptr {
+class uniqe_ptr
+{
 public:
-    uniqe_ptr();
-    ~uniqe_ptr();
+    uniqe_ptr() : ptr_(nullptr);
+    explicit uniqe_ptr(T* ptr) : ptr_(ptr);
 
-    uniqe_ptr(const uniqe_ptr& other); // copy constructor
-    uniqe_ptr(uniqe_ptr&& other) noexcept; // move constructor
-    uniqe_ptr& operator = (const uniqe_ptr&); // copy assigment
-    uniqe_ptr& operator= (uniqe_ptr&& other) noexcept;  // move assigment
+    uniqe_ptr(const uniqe_ptr& other) = delete;     // copy constructor
+    uniqe_ptr(uniqe_ptr&& other) noexcept : uniqe_ptr(other.ptr_) {} // move constructor
+    ~uniqe_ptr() {
+        delete ptr;
+    };
+
+    uniqe_ptr &operator=(const uniqe_ptr &);          // copy assigment
+    uniqe_ptr &operator=(uniqe_ptr &&other) noexcept; // move assigment
 
     uniqe_ptr<T> operator*() const noexcept;
-    T* operator->() const noexcept;
+    T *operator->() const noexcept;
 
-    T* get();
-    T* release();
-    T* reset();
+    T *get();
+    T *release() {
+        T* tempPtr = ptr_;
+        ptr_ = nullptr;
+        return temp;
+    };
+    T *reset();
+
 private:
-    T* var_;
+    T* ptr_;
 };
