066d900f10d617405202771df4a75c09b0ef579f
Wojtek <plociennik.wojtek@gmail.com>
2021-10-25 18:36:02 +0200

    adding ControlBloc c'tor

--- homework/shared_ptr/controlBlock.hpp
+++ homework/shared_ptr/controlBlock.hpp
@@ -3,11 +3,13 @@
 #include <atomic>
 #include <cstddef>
 
-class controlBlock {
+class ControlBlock {
 public:
-
+    ControlBlock() : sharedRef_(0), weakRef_(0) {}
+    ControlBlock(const ControlBlock& ) = delete;
+    ControlBlock& operator=(const ControlBlock& ) = delete;
 
 private:
-    std::atomic<int> sharedRef{1};
-    std::atomic<int> weakRed{0};
+    std::atomic<int> sharedRef_;
+    std::atomic<int> weakRef_;
 };
