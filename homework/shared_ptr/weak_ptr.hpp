371d9eb8728ce7110ae4c08c21ff0543be0122bd
Wojtek <plociennik.wojtek@gmail.com>
2021-10-27 22:54:57 +0200

    implemning c'tors and operator in weak_ptr

--- homework/shared_ptr/weak_ptr.hpp
+++ homework/shared_ptr/weak_ptr.hpp
@@ -9,6 +9,7 @@ class Weak_Ptr
 private:
     T *rawPtr_;
     ControlBlock<T> *cb_;
+    void releaseMemory();
 
 public:
     Weak_Ptr() = default;
@@ -24,3 +25,60 @@ public:
     shared_ptr<T> lock() const noexcept;
     void reset() noexcept;
 };
+
+template <typename T>
+void Weak_Ptr<T>::releaseMemory() {
+    if (cb_) {
+        cb_->decreaseWeakRef();
+        if (cb_->getSharedRef() == 0 && cb_->getWeakRef() == 0)
+        {
+            delete cb_;
+            cb_ = nullptr;
+        }
+    }
+}
+
+
+template <typename T>
+Weak_Ptr<T>::Weak_Ptr(const Weak_Ptr &ptr) noexcept : rawPtr_(ptr.rawPtr_), cb_(ptr.cb_)
+{
+    cb_->increasWeakRef();
+}
+
+template <typename T>
+Weak_Ptr<T>::Weak_Ptr(Weak_Ptr &&ptr) noexcept : rawPtr_(ptr.rawPtr_), cb_(ptr.cb_)
+{
+    ptr.rawPtr_ = nullptr;
+    ptr.cb_ = nullptr;
+}
+
+template <typename T>
+Weak_Ptr<T>::~Weak_Ptr()
+{
+    releaseMemory();
+}
+
+template <typename T>
+Weak_Ptr<T> &Weak_Ptr<T>::operator=(const Weak_Ptr &ptr) noexcept
+{
+    if (ptr != this)
+    {
+        releaseMemory();
+        rawPtr_ = ptr.rawPtr_;
+        cb_ = ptr.cb_;
+        cb_->increasWeakRef();
+    }
+    return *this;
+}
+
+template <typename T>
+Weak_Ptr<T> &Weak_Ptr<T>::operator=(Weak_Ptr &&ptr) noexcept
+{
+    if (ptr != this)
+    {
+        rawPtr_ = ptr.rawPtr_;
+        cb_ = ptr.cb_;
+        
+
+    }
+}
