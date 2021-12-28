311b147b19c099968c54d3f678b65d6cff103206
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-12-28 23:08:00 +0100

    Adding tests checking copy

--- homework/unique_ptr/test/test.cpp
+++ homework/unique_ptr/test/test.cpp
@@ -1,25 +1,37 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch_amalgamated.hpp"
 #include "../uniquePTR.hpp"
+#include <type_traits>
 
-// SCENARIO("")
-// {
-//     GIVEN(")
-//     {
-        
+SCENARIO("Testing unique pointer")
+{
+    GIVEN("Made pointer")
+    {
+        auto unique2 = unique_ptr<int>(new int(5));
+        WHEN("Checking if it is possible to copy by copy constructor")
+        {
+            auto copy = std::is_copy_constructible<unique_ptr<int>>::value;
+            THEN("Result should be false")
+            {
+                REQUIRE_FALSE(copy);
+            }
+        }
+        WHEN("Checking if it is possible to copy by copy assignment operator"){
+            auto copy2 = std::is_copy_assignable<unique_ptr<int>>::value;
+            THEN("Result should be false"){
+                REQUIRE_FALSE(copy2);
+            }
+        }
 
-//         REQUIRE();
-
-//         WHEN("Find student with surname Nowak")
-//         {
-//             db->GetStudentsWithSurname("Nowak");
-//             THEN("vector contains Nowak")
-//             {
-//                 REQUIRE(db->person.size());
-//                 auto Surname0 = db->person[0]->GetSurname();
-//                 CHECK(Surname0 == "Nowak");
-//             }
-//         }
-    
-//     }
-// }
+        // WHEN("Find student with surname Nowak")
+        // {
+        //     db->GetStudentsWithSurname("Nowak");
+        //     THEN("vector contains Nowak")
+        //     {
+        //         REQUIRE(db->person.size());
+        //         auto Surname0 = db->person[0]->GetSurname();
+        //         CHECK(Surname0 == "Nowak");
+        //     }
+        // }
+    }
+}
