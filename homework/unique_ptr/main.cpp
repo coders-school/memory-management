518e97b7e751017541084ed9c24b9628d5408a04
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-12-28 22:40:15 +0100

    Adding files for tests - catch2

--- homework/unique_ptr/main.cpp
+++ homework/unique_ptr/main.cpp
@@ -1,8 +1,13 @@
 #include "uniquePTR.hpp"
 #include <string>
 #include <memory>
+#include <iostream>
 
 
 int main() {
+    auto unique2 = unique_ptr<int>(new int (5));
+    std::cout << *unique2;
+    auto b = unique2.get();
+    std::cout << *b;
 
 } 
---
+++ homework/unique_ptr/test/test.cpp
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch_amalgamated.hpp"
+#include "../uniquePTR.hpp"
+
+// SCENARIO("")
+// {
+//     GIVEN(")
+//     {
+        
+
+//         REQUIRE();
+
+//         WHEN("Find student with surname Nowak")
+//         {
+//             db->GetStudentsWithSurname("Nowak");
+//             THEN("vector contains Nowak")
+//             {
+//                 REQUIRE(db->person.size());
+//                 auto Surname0 = db->person[0]->GetSurname();
+//                 CHECK(Surname0 == "Nowak");
+//             }
+//         }
+    
+//     }
+// }
