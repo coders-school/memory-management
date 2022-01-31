2f956e4ef9d159cf366703baf8f9c309f86ea30f
raxonpc <tomeczekorlowski1@gmail.com>
2022-01-31 22:42:47 +0100

    Add unit tests for get() and reset()

--- homework/unique_ptr/Tests/tests.cpp
+++ homework/unique_ptr/Tests/tests.cpp
@@ -16,3 +16,23 @@ TEST(unique_ptr, destructor_test) {
 
     EXPECT_CALL(*ptr, Destructor).Times(1);
 }
+
+TEST(unique_ptr, get_test) {
+    auto ptr = new int{ 5 };
+    auto smartPtr = UniquePointer<int>(ptr);
+
+    EXPECT_EQ(ptr, smartPtr.get());
+}
+
+TEST(unique_ptr, reset_test) {
+    auto ptr = new int{ 5 };
+    auto smartPtr = UniquePointer<int>(ptr);
+
+    smartPtr.reset(nullptr);
+    EXPECT_EQ(nullptr, smartPtr.get());
+
+    auto otherPtr = new int{ 6 };
+    smartPtr.reset(otherPtr);
+    EXPECT_EQ(otherPtr, smartPtr.get());
+}
+
