53041444fa5b7155cacc585a7d79cc97ab6be3fb
miczel <michal.soloducha@gmail.com>
2022-02-01 14:59:42 +0100

    Added operator*(). Separated const & non-const.

--- homework/unique_ptr/UniquePointer.hpp
+++ homework/unique_ptr/UniquePointer.hpp
@@ -5,10 +5,7 @@ class UniquePointer {
 public:
     using pointer = T*;
     using element_type = T;
-private:
-    pointer pointer_{ nullptr };
 
-public:
     UniquePointer() = default;
     explicit UniquePointer(pointer ptr)
         : pointer_(ptr){};
@@ -18,16 +15,27 @@ public:
         pointer_ = nullptr;
     }
 
-    pointer get() const noexcept {
+    const pointer get() const noexcept {
         return pointer_;
     }
-    
+
+    pointer get() noexcept {
+        return pointer_;
+    }
+
     void reset(pointer ptr = pointer()) noexcept {
         delete pointer_;
         pointer_ = ptr;
-    }   
+    }
 
-    // UniquePointer& operator*() const noexcept {
-    //     return *get();
-    // }
+    const UniquePointer& operator*() const noexcept {
+        return *get();
+    }
+
+    UniquePointer& operator*() noexcept {
+        return *get();
+    }
+
+private:
+    pointer pointer_{nullptr};
 };
