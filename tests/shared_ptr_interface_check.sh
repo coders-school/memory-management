9f34c2cfe17f9fb8416c8aa73b33e23976553e96
Andrzej Rafalski <anraf1001@gmail.com>
2022-03-25 11:52:52 +0100

    Checking if shared_ptr is fully implemented

---
+++ tests/shared_ptr_interface_check.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# 1: file, 2: regex, 3: method_to_find
+check_regex () {
+    grep -oP "$2" $1 > /dev/null
+
+    if [ ! $? -eq 0 ]; then
+        echo "$3 is not implemented ❌"
+        return 1
+    fi
+
+    return 0
+}
+
+file=../homework/shared_ptr/shared_ptr.hpp
+exit_status=0
+echo "Checking if shared_ptr interface is fully implemented..."
+
+check_regex $file 'shared_ptr\s*\(const shared_ptr& \w+\)' 'Copy constructor'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'shared_ptr\s*\(shared_ptr&& \w+\)' 'Move constructor'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'shared_ptr& operator=\s*\(const shared_ptr& \w+\)' 'Copy assignment operator'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'shared_ptr& operator=\s*\(shared_ptr&& \w+\)' 'Move assignment operator'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'T&\s*operator\*\s*\(\s*\)' 'operator*()'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'T\*\s*operator-\>\s*\(\s*\)' 'operator->()'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'T\*\s*get\s*\(\s*\)' 'get()'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'void\s*reset\s*\(' 'reset()'
+exit_status=$(($exit_status | $?))
+
+check_regex $file '\w+\s*use_count\s*\(\s*\)' 'use_count()'
+exit_status=$(($exit_status | $?))
+
+check_regex $file 'operator\s*bool\s*\(\s*\)' 'operator bool()'
+exit_status=$(($exit_status | $?))
+
+if [ $exit_status -eq 0 ]; then
+    echo shared_ptr interface is fully implemented ✅
+else
+    echo shared_ptr interface is not fully implemented ❌
+fi
+
+exit $((exit_status))
