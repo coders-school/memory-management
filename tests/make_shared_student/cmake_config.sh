7e6f39ce4ab8aa9884ee493be04cfc975066dea3
Andrzej Rafalski <anraf1001@gmail.com>
2022-03-26 08:32:50 +0100

    Running students tests

---
+++ tests/make_shared_student/cmake_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+file=$(cat ../../../homework/shared_ptr/make_shared_tests.cpp)
+regex="#include [<|\"]((catch2)|(gtest))"
+
+if [[ $file =~ $regex ]]
+then
+    framework=${BASH_REMATCH[1]}
+    cmake -DTEST_FRAMEWORK=$framework ..
+fi
