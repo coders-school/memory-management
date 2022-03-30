0da83d4f950669e0f29f75f770619d1ea4c581f6
Andrzej Rafalski <anraf1001@gmail.com>
2022-03-30 15:16:04 +0200

    Regex for function with parameters written in newlines

--- tests/shared_ptr_interface_check.sh
+++ tests/shared_ptr_interface_check.sh
@@ -2,7 +2,7 @@
 
 # 1: file, 2: regex, 3: method_to_find
 check_regex () {
-    grep -oP "$2" $1 > /dev/null
+    grep -zoP "$2" $1 > /dev/null
 
     if [ ! $? -eq 0 ]; then
         echo "$3 is not implemented ❌"
@@ -16,25 +16,25 @@ file=../homework/shared_ptr/shared_ptr.hpp
 exit_status=0
 echo "Checking if shared_ptr interface is fully implemented..."
 
-check_regex $file 'shared_ptr\s*\(const shared_ptr& \w+\)' 'Copy constructor'
+check_regex $file 'shared_ptr\s*\(\n?\s*?((const shared_ptr& \w+)|(shared_ptr const& \w+))\n?\s*?\)' 'Copy constructor'
 exit_status=$(($exit_status | $?))
 
-check_regex $file 'shared_ptr\s*\(shared_ptr&& \w+\)' 'Move constructor'
+check_regex $file 'shared_ptr\s*\(\n?\s*?shared_ptr&& \w+\n?\s*?\)' 'Move constructor'
 exit_status=$(($exit_status | $?))
 
-check_regex $file 'shared_ptr& operator=\s*\(const shared_ptr& \w+\)' 'Copy assignment operator'
+check_regex $file 'shared_ptr& operator=\s*\(\n?\s*?((const shared_ptr& \w+)|(shared_ptr const& \w+))\n?\s*?\)' 'Copy assignment operator'
 exit_status=$(($exit_status | $?))
 
-check_regex $file 'shared_ptr& operator=\s*\(shared_ptr&& \w+\)' 'Move assignment operator'
+check_regex $file 'shared_ptr& operator=\s*\(\n?\s*?shared_ptr&& \w+\n?\s*?\)' 'Move assignment operator'
 exit_status=$(($exit_status | $?))
 
 check_regex $file 'T&\s*operator\*\s*\(\s*\)' 'operator*()'
 exit_status=$(($exit_status | $?))
 
-check_regex $file 'T\*\s*operator-\>\s*\(\s*\)' 'operator->()'
+check_regex $file 'T\*\s*operator-\>\s*\(' 'operator->()'
 exit_status=$(($exit_status | $?))
 
-check_regex $file 'T\*\s*get\s*\(\s*\)' 'get()'
+check_regex $file 'T\*\s*get\s*\(' 'get()'
 exit_status=$(($exit_status | $?))
 
 check_regex $file 'void\s*reset\s*\(' 'reset()'
@@ -43,7 +43,7 @@ exit_status=$(($exit_status | $?))
 check_regex $file '\w+\s*use_count\s*\(\s*\)' 'use_count()'
 exit_status=$(($exit_status | $?))
 
-check_regex $file 'operator\s*bool\s*\(\s*\)' 'operator bool()'
+check_regex $file 'operator\s*bool\s*\(' 'operator bool()'
 exit_status=$(($exit_status | $?))
 
 if [ $exit_status -eq 0 ]; then
