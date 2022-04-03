eac1a2b9940fdb912fceee3b978d0b1ad0738ef7
Andrzej Rafalski <anraf1001@gmail.com>
2022-04-03 09:23:12 +0200

    Merged CMakeLists files

--- .github/scripts/cmake_config.sh
+++ .github/scripts/cmake_config.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-file=$(cat ../../../homework/shared_ptr/weak_ptr_tests.cpp)
+file=$(cat "$1")
 regex="#include [<|\"]((catch2)|(gtest))"
 
-if [[ $file =~ $regex ]]
-then
+if [[ $file =~ $regex ]]; then
     framework=${BASH_REMATCH[1]}
     cmake -DTEST_FRAMEWORK=$framework ..
 else
