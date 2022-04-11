2af7733cf5c61193a5cd4462d334b1cb28ebf5f2
Andrzej Rafalski <anraf1001@gmail.com>
2022-04-11 08:58:01 +0200

    Deleted coverage script parameter

--- .github/scripts/coverage.sh
+++ .github/scripts/coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file=$(cat "../../../homework/shared_ptr/$1_tests.cpp")
+file=$(cat "../../../homework/shared_ptr/shared_tests.cpp")
 pattern_template="template\s*class\s*my::shared_ptr<\s*\w+\s*>"
 
 if [[ ! ${file//$'\n'/' '} =~ $pattern_template ]]; then
@@ -12,7 +12,7 @@ lcov --directory . --capture --output-file coverage.info > /dev/null
 lcov --remove coverage.info '/usr/*' '*/_deps/*' --output-file coverage.info > /dev/null
 
 report=$(lcov --list coverage.info)
-pattern_lcov="$1\.hpp\s*\|\s*([0-9]{2,3}(\.[0-9])?)%"
+pattern_lcov="shared_ptr\.hpp\s*\|\s*([0-9]{2,3}(\.[0-9])?)%"
 
 if [[ ${report//$'\n'/' '} =~ $pattern_lcov ]]; then
     if (( $(echo "${BASH_REMATCH[1]} > 90.0" | bc -l) )); then
