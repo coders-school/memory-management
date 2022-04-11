f191250937bd230a1bb2da9fb2ac1bfb26560d0f
Andrzej Rafalski <anraf1001@gmail.com>
2022-04-11 19:27:54 +0200

    Printing which lines are not covered

--- .github/scripts/coverage.sh
+++ .github/scripts/coverage.sh
@@ -19,6 +19,15 @@ if [[ ${report//$'\n'/' '} =~ $pattern_lcov ]]; then
         echo "✅ Test coverage rate is higher than 90%"
     else
         echo "❌ Test coverage rate is lower than 90%"
+        echo "Lines from shared_ptr.hpp that are not covered:"
+
+        genhtml coverage.info -o report > /dev/null
+        no_cov_line_pattern="([0-9]+).*?lineNoCov"
+        while IFS= read -r line; do
+            if [[ $line =~ $no_cov_line_pattern ]]; then
+                echo ${BASH_REMATCH[1]}
+            fi
+        done < "./report/shared_ptr/shared_ptr.hpp.gcov.html"
         exit 1
     fi
 else
