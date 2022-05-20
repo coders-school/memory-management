9027eaef7ba704a6a61ac3a3d2e60606196b00e4
Andrzej Rafalski <anraf1001@gmail.com>
2022-05-20 10:29:13 +0200

    add check_coverage script

--- .github/actions/check_coverage/check_coverage.sh
+++ .github/actions/check_coverage/check_coverage.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+file=$(cat "$1")
+
+if [[ $# = 3 ]]; then
+    pattern_template="$3"
+
+    if [[ ! ${file//$'\n'/' '} =~ $pattern_template ]]; then
+        echo "❌ You haven't instantiated template class"
+        exit 1
+    fi
+fi
+
+lcov --directory . --capture --output-file coverage.info > /dev/null
+lcov --extract coverage.info '*/unique_ptr.hpp' --output-file coverage.info > /dev/null
+
+report=$(lcov --list coverage.info)
+pattern_lcov="$2"
+
+if [[ ${report//$'\n'/' '} =~ $pattern_lcov ]]; then
+    if (( $(echo "${BASH_REMATCH[1]} > 90.0" | bc -l) )); then
+        echo "✅ Test coverage rate is higher than 90%"
+    else
+        echo "❌ Test coverage rate is lower than 90%"
+        genhtml coverage.info -o report > /dev/null
+        exit 1
+    fi
+else
+    echo "Couldn't find coverage rate"
+    exit 1
+fi
