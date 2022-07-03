49b71faa64c6fdf9e6859e665e9d3f610030c943
Aleksiiej <aleksander.ramut@wp.pl>
2022-07-03 23:24:50 +0200

    code changed with clang-format

--- homework/unique_ptr/unique_ptr.hpp
+++ homework/unique_ptr/unique_ptr.hpp
@@ -1,60 +1,54 @@
 #include <iostream>
 
 template <typename T>
-class unique_ptr
-{
+class unique_ptr {
 public:
     unique_ptr() noexcept;
-    unique_ptr(T *ptr) noexcept;
-    unique_ptr(unique_ptr &) = delete;
-    unique_ptr(unique_ptr &&other) noexcept;
+    unique_ptr(T* ptr) noexcept;
+    unique_ptr(unique_ptr&) = delete;
+    unique_ptr(unique_ptr&& other) noexcept;
 
     ~unique_ptr();
 
-    unique_ptr &operator=(unique_ptr &) = delete;
-    unique_ptr &operator=(unique_ptr &&other) noexcept;
+    unique_ptr& operator=(unique_ptr&) = delete;
+    unique_ptr& operator=(unique_ptr&& other) noexcept;
 
-    T &operator*() const noexcept;
-    T *operator->() const noexcept;
-    T *get() const noexcept;
-    T *release() noexcept;
-    void reset(T *ptr) noexcept;
+    T& operator*() const noexcept;
+    T* operator->() const noexcept;
+    T* get() const noexcept;
+    T* release() noexcept;
+    void reset(T* ptr) noexcept;
 
 private:
-    T *ptr_;
+    T* ptr_;
 };
 
 template <typename T>
-unique_ptr<T>::unique_ptr() noexcept : ptr_(nullptr)
-{
+unique_ptr<T>::unique_ptr() noexcept
+    : ptr_(nullptr) {
 }
 
 template <typename T>
-unique_ptr<T>::unique_ptr(T *ptr) noexcept : ptr_(ptr)
-{
+unique_ptr<T>::unique_ptr(T* ptr) noexcept
+    : ptr_(ptr) {
 }
 
 template <typename T>
-unique_ptr<T>::unique_ptr(unique_ptr &&other) noexcept
-{
-    if (&other != this)
-    {
+unique_ptr<T>::unique_ptr(unique_ptr&& other) noexcept {
+    if (&other != this) {
         ptr_ = std::move(other.ptr_);
     }
     other.ptr_ = nullptr;
 }
 
 template <typename T>
-unique_ptr<T>::~unique_ptr()
-{
+unique_ptr<T>::~unique_ptr() {
     delete ptr_;
 }
 
 template <typename T>
-unique_ptr<T> &unique_ptr<T>::operator=(unique_ptr &&other) noexcept
-{
-    if (&other != this)
-    {
+unique_ptr<T>& unique_ptr<T>::operator=(unique_ptr&& other) noexcept {
+    if (&other != this) {
         ptr_ = std::move(other.ptr_);
         other.ptr_ = nullptr;
     }
@@ -62,34 +56,29 @@ unique_ptr<T> &unique_ptr<T>::operator=(unique_ptr &&other) noexcept
 }
 
 template <typename T>
-T &unique_ptr<T>::operator*() const noexcept
-{
+T& unique_ptr<T>::operator*() const noexcept {
     return *ptr_;
 }
 
 template <typename T>
-T *unique_ptr<T>::operator->() const noexcept
-{
+T* unique_ptr<T>::operator->() const noexcept {
     return ptr_;
 }
 
 template <typename T>
-T *unique_ptr<T>::get() const noexcept
-{
+T* unique_ptr<T>::get() const noexcept {
     return ptr_;
 }
 
 template <typename T>
-T *unique_ptr<T>::release() noexcept
-{
-    T *tempPtr = std::move(ptr_);
+T* unique_ptr<T>::release() noexcept {
+    T* tempPtr = std::move(ptr_);
     ptr_ = nullptr;
     return tempPtr;
 }
 
 template <typename T>
-void unique_ptr<T>::reset(T *ptr) noexcept
-{
+void unique_ptr<T>::reset(T* ptr) noexcept {
     delete ptr_;
     ptr_ = std::move(ptr);
 }
