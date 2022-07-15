0d1662e1afd32361cfe22edd707e80756d91785d
Piotr Rogalski <progalski89@gmail.com>
2022-07-15 22:41:02 +0200

    Implemented rule of 5

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -19,8 +19,8 @@ class shared_ptr {
 
 public:
     explicit shared_ptr(T* ptr) noexcept {
-        control_ptr = new control_block;
         object_ptr = ptr;
+        control_ptr = new control_block;
     }
 
     shared_ptr(const shared_ptr& other) noexcept
@@ -28,14 +28,34 @@ public:
         ++control_ptr->shared_refs;
     }
 
+    shared_ptr& operator=(const shared_ptr& other) noexcept {
+        if (this != &other) {
+            --control_ptr->shared_refs;
+            delete_content_if_needed();
+            object_ptr = other.object_ptr;
+            control_ptr = other.control_ptr;
+            ++control_ptr->shared_refs;
+        }
+        return *this;
+    }
+
+    shared_ptr(shared_ptr&& other) noexcept
+        : object_ptr{other.object_ptr}, control_ptr{other.control_ptr} {
+        other.object_ptr = nullptr;
+        other.control_ptr = new control_block;
+    }
+
+    shared_ptr& operator=(shared_ptr&& other) noexcept {
+        object_ptr = other.object_ptr;
+        control_ptr = other.control_ptr;
+        other.object_ptr = nullptr;
+        other.control_ptr = new control_block;
+        return *this;
+    }
+
     ~shared_ptr() noexcept {
         --control_ptr->shared_refs;
-        if (!control_ptr->shared_refs) {
-            delete object_ptr;
-            if (!control_ptr->weak_refs) {
-                delete control_ptr;
-            }
-        }
+        delete_content_if_needed();
     }
 
     T* get() const noexcept {
@@ -45,6 +65,15 @@ public:
 private:
     T* object_ptr{nullptr};
     control_block* control_ptr{nullptr};
+
+    inline void delete_content_if_needed() noexcept {
+        if (!control_ptr->shared_refs) {
+            delete object_ptr;
+            if (!control_ptr->weak_refs) {
+                delete control_ptr;
+            }
+        }
+    }
 };
 
 }  // namespace my
