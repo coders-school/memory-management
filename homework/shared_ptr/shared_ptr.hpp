24cfce6c04b3141bbda93b2be0708d9666499524
Aleksiiej <aleksander.ramut@wp.pl>
2022-07-24 17:24:06 +0200

    Function of shared_ptr put insine template class body

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -28,178 +28,138 @@ private:
 template <typename T>
 class shared_ptr {
 public:
-    shared_ptr() noexcept;
-    shared_ptr(T* ptr) noexcept;
-    shared_ptr(const shared_ptr& other) noexcept;
-    shared_ptr(shared_ptr&& other) noexcept;
-    explicit shared_ptr(const weak_ptr<T>& other) noexcept;
+    shared_ptr() noexcept
+        : ptr_(nullptr), ptrToControlBlock_{nullptr} {
+    }
 
-    ~shared_ptr();
+    shared_ptr(T* ptr) noexcept
+        : ptr_(ptr) {
+        if (ptr_) {
+            ptrToControlBlock_ = new controlBlock;
+        } else {
+            ptrToControlBlock_ = nullptr;
+        }
+    }
 
-    shared_ptr& operator=(shared_ptr&) noexcept;
-    shared_ptr& operator=(shared_ptr&& other) noexcept;
-    explicit operator bool() const noexcept;
+    shared_ptr(const shared_ptr& other) noexcept {
+        if (other.ptr_ == nullptr) {
+            ptr_ = nullptr;
+            ptrToControlBlock_ = nullptr;
+        } else {
+            ptr_ = other.ptr_;
+            ptrToControlBlock_ = other.ptrToControlBlock_;
+            ptrToControlBlock_->incrementSharedRefs();
+        }
+    }
 
-    T& operator*() const noexcept;
-    T* operator->() const noexcept;
-    T* get() const noexcept;
-    T* release() noexcept;
-    void reset(T* ptr) noexcept;
-    void reset() noexcept;
+    shared_ptr(shared_ptr&& other) noexcept {
+        if (&other != this) {
+            ptr_ = std::move(other.ptr_);
+            ptrToControlBlock_ = std::move(other.ptrToControlBlock_);
+            other.ptr_ = nullptr;
+            other.ptrToControlBlock_ = nullptr;
+        }
+    }
 
-    int use_count() const noexcept;
+    explicit shared_ptr(const weak_ptr<T>& other) noexcept {
+        ptr_ = other.ptr_;
+        ptrToControlBlock_ = other.ptrToControlBlock_;
+        if (ptr_) {
+            ptrToControlBlock_->incrementSharedRefs();
+        }
+    }
 
-    controlBlock* getControlBlockPtr() const noexcept;
+    ~shared_ptr() {
+        if (ptrToControlBlock_) {
+            ptrToControlBlock_->decrementSharedRefs();
+            if (ptrToControlBlock_->getSharedRefs() == 0) {
+                delete ptr_;
+            }
+            if (ptrToControlBlock_->getSharedRefs() == 0 && ptrToControlBlock_->getWeakRefs() == 0) {
+                delete ptrToControlBlock_;
+            }
+        }
+    }
+
+    shared_ptr<T>& operator=(shared_ptr& other) noexcept {
+        if (&other != this && other.ptr_ != nullptr) {
+            ptr_ = other.ptr_;
+            ptrToControlBlock_ = other.ptrToControlBlock_;
+            ptrToControlBlock_->incrementSharedRefs();
+            return *this;
+        } else if (&other != this && other.ptr_ == nullptr) {
+            delete ptr_;
+            ptr_ = nullptr;
+            delete ptrToControlBlock_;
+            return *this;
+        }
+        return *this;
+    }
+
+    shared_ptr<T>& operator=(shared_ptr&& other) noexcept {
+        if (&other != this && other.ptr_ != nullptr) {
+            ptr_ = std::move(other.ptr_);
+            ptrToControlBlock_ = std::move(other.ptrToControlBlock_);
+            other.ptr_ = nullptr;
+            other.ptrToControlBlock_ = nullptr;
+        } else if (&other != this && other.ptr_ == nullptr) {
+            delete ptr_;
+            ptr_ = nullptr;
+            delete ptrToControlBlock_;
+        }
+        return *this;
+    }
+
+    operator bool() const noexcept {
+        return ptr_ != nullptr;
+    }
+
+    T& operator*() const noexcept {
+        return *ptr_;
+    }
+
+    T* operator->() const noexcept {
+        return ptr_;
+    }
+
+    T* get() const noexcept {
+        return ptr_;
+    }
+
+    T* release() noexcept {
+        T* tempPtr = std::move(ptr_);
+        ptr_ = nullptr;
+        return tempPtr;
+    }
+
+    void reset(T* ptr) noexcept {
+        delete ptr_;
+        ptr_ = std::move(ptr);
+    }
+
+    void reset() noexcept {
+        delete ptr_;
+        ptr_ = nullptr;
+    }
+
+    int use_count() const noexcept {
+        if (!ptrToControlBlock_) {
+            return 0;
+        } else {
+            return static_cast<int>(ptrToControlBlock_->getSharedRefs());
+        }
+    }
+
+    controlBlock* getControlBlockPtr() const noexcept {
+        if (!ptrToControlBlock_) {
+            return nullptr;
+        } else
+            return ptrToControlBlock_;
+    }
 
 private:
     T* ptr_;
     controlBlock* ptrToControlBlock_;
 };
-
-template <typename T>
-shared_ptr<T>::shared_ptr() noexcept
-    : ptr_(nullptr), ptrToControlBlock_{nullptr} {
-}
-
-template <typename T>
-shared_ptr<T>::shared_ptr(T* ptr) noexcept
-    : ptr_(ptr) {
-    if (ptr_) {
-        ptrToControlBlock_ = new controlBlock;
-    } else {
-        ptrToControlBlock_ = nullptr;
-    }
-}
-
-template <typename T>
-shared_ptr<T>::shared_ptr(const shared_ptr& other) noexcept {
-    if (other.ptr_ == nullptr) {
-        ptr_ = nullptr;
-        ptrToControlBlock_ = nullptr;
-    } else {
-        ptr_ = other.ptr_;
-        ptrToControlBlock_ = other.ptrToControlBlock_;
-        ptrToControlBlock_->incrementSharedRefs();
-    }
-}
-
-template <typename T>
-shared_ptr<T>::shared_ptr(shared_ptr&& other) noexcept {
-    if (&other != this) {
-        ptr_ = std::move(other.ptr_);
-        ptrToControlBlock_ = std::move(other.ptrToControlBlock_);
-        other.ptr_ = nullptr;
-        other.ptrToControlBlock_ = nullptr;
-    }
-}
-
-template <typename T>
-shared_ptr<T>::shared_ptr(const weak_ptr<T>& other) noexcept {
-    ptr_ = other.ptr_;
-    ptrToControlBlock_ = other.ptrToControlBlock_;
-    if (ptr_) {
-        ptrToControlBlock_->incrementSharedRefs();
-    }
-}
-
-template <typename T>
-shared_ptr<T>::~shared_ptr() {
-    if (ptrToControlBlock_) {
-        ptrToControlBlock_->decrementSharedRefs();
-        if (ptrToControlBlock_->getSharedRefs() == 0) {
-            delete ptr_;
-        }
-        if (ptrToControlBlock_->getSharedRefs() == 0 && ptrToControlBlock_->getWeakRefs() == 0) {
-            delete ptrToControlBlock_;
-        }
-    }
-}
-
-template <typename T>
-shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr& other) noexcept {
-    if (&other != this && other.ptr_ != nullptr) {
-        ptr_ = other.ptr_;
-        ptrToControlBlock_ = other.ptrToControlBlock_;
-        ptrToControlBlock_->incrementSharedRefs();
-        return *this;
-    } else if (&other != this && other.ptr_ == nullptr) {
-        delete ptr_;
-        ptr_ = nullptr;
-        delete ptrToControlBlock_;
-        return *this;
-    }
-    return *this;
-}
-
-template <typename T>
-shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr&& other) noexcept {
-    if (&other != this && other.ptr_ != nullptr) {
-        ptr_ = std::move(other.ptr_);
-        ptrToControlBlock_ = std::move(other.ptrToControlBlock_);
-        other.ptr_ = nullptr;
-        other.ptrToControlBlock_ = nullptr;
-    } else if (&other != this && other.ptr_ == nullptr) {
-        delete ptr_;
-        ptr_ = nullptr;
-        delete ptrToControlBlock_;
-    }
-    return *this;
-}
-
-template <typename T>
-shared_ptr<T>::operator bool() const noexcept {
-    return ptr_ != nullptr;
-}
-
-template <typename T>
-T& shared_ptr<T>::operator*() const noexcept {
-    return *ptr_;
-}
-
-template <typename T>
-T* shared_ptr<T>::operator->() const noexcept {
-    return ptr_;
-}
-
-template <typename T>
-T* shared_ptr<T>::get() const noexcept {
-    return ptr_;
-}
-
-template <typename T>
-T* shared_ptr<T>::release() noexcept {
-    T* tempPtr = std::move(ptr_);
-    ptr_ = nullptr;
-    return tempPtr;
-}
-
-template <typename T>
-void shared_ptr<T>::reset(T* ptr) noexcept {
-    delete ptr_;
-    ptr_ = std::move(ptr);
-}
-
-template <typename T>
-void shared_ptr<T>::reset() noexcept {
-    delete ptr_;
-    ptr_ = nullptr;
-}
-
-template <typename T>
-int shared_ptr<T>::use_count() const noexcept {
-    if (!ptrToControlBlock_) {
-        return 0;
-    } else {
-        return static_cast<int>(ptrToControlBlock_->getSharedRefs());
-    }
-}
-
-template <typename T>
-controlBlock* shared_ptr<T>::getControlBlockPtr() const noexcept {
-    if (!ptrToControlBlock_) {
-        return nullptr;
-    } else
-        return ptrToControlBlock_;
-}
 
 }  // namespace my
