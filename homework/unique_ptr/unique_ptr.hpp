8039c17d8806c670438ffa1998cfb7eceea4e3ec
Maciej Białczak <m.bialczak@protonmail.com>
2022-11-02 21:33:13 +0100

    Version 1.0 ready

--- homework/unique_ptr/unique_ptr.hpp
+++ homework/unique_ptr/unique_ptr.hpp
@@ -1,37 +1,92 @@
 #pragma once
 
+#include <algorithm>
+
 namespace my_unique_ptr {
 
 template <typename T>
 class unique_ptr {
 public:
-    // TODO: implementation and tests
     unique_ptr() noexcept;
-    // TODO: implementation and tests
     unique_ptr(T* ptr) noexcept;
     unique_ptr(const unique_ptr&) = delete;
-    // TODO: implementation and tests
-    unique_ptr(unique_ptr&& uPtr) noexcept;
-    // TODO: implementation and tests
+    unique_ptr(unique_ptr&& otherUptr) noexcept;
     ~unique_ptr();
     unique_ptr& operator=(const unique_ptr&) = delete;
-    // TODO: implementation and tests
-    unique_ptr& operator=(unique_ptr&& uPtr) noexcept;
-    // TODO: implementation and tests
+    unique_ptr& operator=(unique_ptr&& otherUptr) noexcept;
     T* get() const noexcept;
-    // TODO: implementation and tests
     T* release() noexcept;
-    // TODO: implementation and tests
-    void reset(T* ptr = nullptr) noexcept;
-    // TODO: implementation and tests
-    void swap(unique_ptr& other) noexcept;
-    // TODO: implementation and tests
-    explicit operator bool() const noexcept;
-    // TODO: implementation and tests
+    void reset(T* newPtr = nullptr) noexcept;
+    T& operator*() const noexcept;
     T* operator->() const noexcept;
+    explicit operator bool() const noexcept;
 
 private:
     T* ptr_;
 };
+
+template <typename T>
+T* unique_ptr<T>::get() const noexcept {
+    return ptr_;
+}
+
+template <typename T>
+unique_ptr<T>::unique_ptr() noexcept
+    : ptr_(nullptr) {}
+
+template <typename T>
+unique_ptr<T>::unique_ptr(T* ptr) noexcept
+    : ptr_(ptr) {}
+
+template <typename T>
+unique_ptr<T>::unique_ptr(unique_ptr&& otherUptr) noexcept
+    : ptr_(otherUptr.ptr_) {
+    otherUptr.ptr_ = nullptr;
+}
+
+template <typename T>
+unique_ptr<T>& unique_ptr<T>::operator=(unique_ptr&& otherUptr) noexcept {
+    ptr_ = otherUptr.ptr_;
+    otherUptr.ptr_ = nullptr;
+
+    return *this;
+}
+
+template <typename T>
+unique_ptr<T>::~unique_ptr() {
+    delete ptr_;
+}
+
+template <typename T>
+T* unique_ptr<T>::release() noexcept {
+    T* releasedPtr = ptr_;
+    ptr_ = nullptr;
+
+    return releasedPtr;
+}
+
+template <typename T>
+void unique_ptr<T>::reset(T* ptr) noexcept {
+    T* oldPtr = ptr_;
+    ptr_ = ptr;
+    if (oldPtr) {
+        delete oldPtr;
+    }
+}
+
+template <typename T>
+T& unique_ptr<T>::operator*() const noexcept {
+    return *ptr_;
+}
+
+template <typename T>
+T* unique_ptr<T>::operator->() const noexcept {
+    return ptr_;
+}
+
+template <typename T>
+unique_ptr<T>::operator bool() const noexcept {
+    return static_cast<bool>(ptr_);
+}
 
 }  // namespace my_unique_ptr
