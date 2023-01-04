86a1586362844dc8a5e9c1051c5d9dc79190298a
KrzucH <115778673+KrzucH@users.noreply.github.com>
2023-01-04 19:57:15 +0100

    Update resourceD.cpp

--- homework/resourceD/resourceD.cpp
+++ homework/resourceD/resourceD.cpp
@@ -7,18 +7,18 @@ class Resource
 { 
 public:
     void use(const char* N)
-    {
-        cout << "Using resource. Passed " << *N << endl;
-        if (*N == 'd')
+    {   
+        cout << "Using resource. Passed " << endl;
         {
             throw logic_error("Passed d. d is prohibited.");
         }
+        delete N;
     };
 };
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2)
+    if(argc == 2)
     {
         cerr << "You need to pass 1 argument" << endl;
         exit(-1);
@@ -30,11 +30,11 @@ int main(int argc, char* argv[])
         rsc = new Resource();
         rsc->use(N);
         delete rsc;
-    }
+    }   
     catch (logic_error & e)
     {
         cout << e.what() << endl;
     }
     return 0;
+    
 }
-
