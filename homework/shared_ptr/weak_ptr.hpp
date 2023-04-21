631022b006d00a60807c89a1732401107db0f240
Michal Soloducha <michal.soloducha@snom.com>
2023-04-21 19:17:45 +0200

    Initialize weak_ptr based on shared_ptr files

---
+++ homework/shared_ptr/weak_ptr.hpp
@@ -0,0 +1,143 @@
+#pragma once
+#include <atomic>
+#include <cstddef>
+
+namespace my {
+
+// void deleter() {}
+
+struct ControlBlock {
+    std::atomic<size_t> shared_refs{0};
+    std::atomic<size_t> weak_refs{0};
+    // void (*deleter_pointer)(int){nullptr};
+};
+
+template <typename Type>
+class shared_ptr {
+public:
+    shared_ptr()
+        : pointer_(nullptr), control_block_pointer_(new ControlBlock) {
+        control_block_pointer_->shared_refs++;
+    }
+
+    explicit shared_ptr(Type* pointer)
+        : pointer_(pointer), control_block_pointer_(new ControlBlock) {
+        control_block_pointer_->shared_refs++;
+    }
+
+    ~shared_ptr() noexcept {
+        if (control_block_pointer_) {  // delete only for objects with allocated control block, i.e. not for moved objects
+            control_block_pointer_->shared_refs--;
+            if (control_block_pointer_->shared_refs == 0) {
+                if (control_block_pointer_->weak_refs == 0) {
+                    if (control_block_pointer_) {
+                        delete control_block_pointer_;
+                        control_block_pointer_ = nullptr;
+                    }
+                }
+                if (pointer_) {
+                    delete pointer_;
+                    pointer_ = nullptr;
+                }
+            }
+        }
+    }
+
+    // copy constructor
+    shared_ptr(const shared_ptr& other)
+        : pointer_(other.pointer_), control_block_pointer_(other.control_block_pointer_) {
+        control_block_pointer_->shared_refs++;
+    }
+
+    // copy assignment operator
+    shared_ptr& operator=(const shared_ptr& other) {
+        control_block_pointer_->shared_refs--;
+        // while overwriting an object we may to erase the last object pointing to a resource
+        if (control_block_pointer_->shared_refs == 0) {
+            if (control_block_pointer_->weak_refs == 0) {
+                delete control_block_pointer_;
+                control_block_pointer_ = nullptr;
+            }
+            if (pointer_) {
+                delete pointer_;
+                pointer_ = nullptr;
+            }
+        }
+        pointer_ = other.pointer_;
+        control_block_pointer_ = other.control_block_pointer_;
+        control_block_pointer_->shared_refs++;
+        return *this;
+    }
+
+    // move constructor
+    shared_ptr(shared_ptr&& other) noexcept {
+        pointer_ = nullptr;
+        control_block_pointer_ = nullptr;
+        std::swap(pointer_, other.pointer_);
+        std::swap(control_block_pointer_, other.control_block_pointer_);
+    }
+
+    // move assignment operator
+    shared_ptr& operator=(shared_ptr&& other) noexcept {
+        control_block_pointer_->shared_refs--;
+        // while overwriting an object we may to erase the last object pointing to a resource
+        if (control_block_pointer_->shared_refs == 0) {
+            if (control_block_pointer_->weak_refs == 0) {
+                delete control_block_pointer_;
+                control_block_pointer_ = nullptr;
+            }
+            if (pointer_) {
+                delete pointer_;
+                pointer_ = nullptr;
+            }
+        }
+        pointer_ = nullptr;
+        control_block_pointer_ = nullptr;
+        std::swap(pointer_, other.pointer_);
+        std::swap(control_block_pointer_, other.control_block_pointer_);
+        return *this;
+    }
+
+    Type* get() const noexcept {
+        return pointer_;
+    }
+
+    void reset(Type* pointer = nullptr) noexcept {
+        control_block_pointer_->shared_refs--;
+        // while overwriting an object we may to erase the last object pointing to a resource
+        if (control_block_pointer_->shared_refs == 0) {
+            if (control_block_pointer_->weak_refs == 0) {
+                delete control_block_pointer_;
+                control_block_pointer_ = nullptr;
+            }
+            if (pointer_) {
+                delete pointer_;
+                pointer_ = nullptr;
+            }
+        }
+        pointer_ = pointer;
+        control_block_pointer_ = new ControlBlock;
+        control_block_pointer_->shared_refs++;
+    }
+
+    Type& operator*() const noexcept {
+        return *pointer_;
+    }
+
+    Type* operator->() const noexcept {
+        return pointer_;
+    }
+
+    explicit operator bool() const {
+        return pointer_;
+    }
+
+    size_t use_count() const noexcept {
+        return control_block_pointer_->shared_refs;
+    }
+
+private:
+    Type* pointer_{nullptr};
+    ControlBlock* control_block_pointer_{nullptr};
+};
+}  // namespace my
---
+++ homework/shared_ptr/weak_ptr_tests.cpp.cpp
@@ -0,0 +1,212 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <utility>
+#include "shared_ptr.hpp"
+
+template class my::shared_ptr<int>;
+
+class TestType {
+public:
+    TestType() = default;
+    ~TestType() noexcept { Destructor(); }
+
+    MOCK_METHOD(void, Destructor, (), (noexcept));
+};
+
+TEST(shared_ptr, destructor) {
+    auto ptr = new TestType();
+    auto smartPtr = my::shared_ptr<TestType>(ptr);
+
+    EXPECT_CALL(*ptr, Destructor);
+}
+
+TEST(shared_ptr, default_constructor) {
+    my::shared_ptr<TestType> smartPtr;
+
+    EXPECT_EQ(smartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, parametric_constructor) {
+    auto ptr = new int{5};
+    auto smartPtr = my::shared_ptr<int>(ptr);
+
+    EXPECT_EQ(smartPtr.get(), ptr);
+}
+
+TEST(shared_ptr, copy_constructor_with_nullptr) {
+    my::shared_ptr<TestType> otherSmartPtr;
+    my::shared_ptr<TestType> smartPtr(otherSmartPtr);
+
+    EXPECT_EQ(smartPtr.get(), nullptr);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, copy_constructor_with_ptr) {
+    auto ptr = new TestType();
+    auto otherSmartPtr = my::shared_ptr<TestType>(ptr);
+
+    EXPECT_CALL(*ptr, Destructor);
+
+    my::shared_ptr<TestType> smartPtr(otherSmartPtr);
+
+    EXPECT_EQ(smartPtr.get(), ptr);
+    EXPECT_EQ(otherSmartPtr.get(), ptr);
+}
+
+TEST(shared_ptr, copy_assignment_from_nullptr_to_nullptr) {
+    auto smartPtr = my::shared_ptr<TestType>();
+    auto otherSmartPtr = my::shared_ptr<TestType>();
+
+    smartPtr = otherSmartPtr;
+
+    EXPECT_EQ(smartPtr.get(), nullptr);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, copy_assignment_from_ptr_to_nullptr) {
+    my::shared_ptr<TestType> smartPtr;
+    auto otherPtr = new TestType();
+    auto otherSmartPtr = my::shared_ptr<TestType>(otherPtr);
+
+    EXPECT_CALL(*otherPtr, Destructor);
+
+    smartPtr = otherSmartPtr;
+
+    EXPECT_EQ(smartPtr.get(), otherPtr);
+    EXPECT_EQ(otherSmartPtr.get(), otherPtr);
+}
+
+TEST(shared_ptr, move_constructor_with_nullptr) {
+    my::shared_ptr<TestType> otherSmartPtr;
+    my::shared_ptr<TestType> smartPtr(std::move(otherSmartPtr));
+
+    EXPECT_EQ(smartPtr.get(), nullptr);
+    EXPECT_EQ(smartPtr.use_count(), 1);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+    // EXPECT_EQ(otherSmartPtr.use_count(), 0);  // -> should cause segmentation fault
+}
+
+TEST(shared_ptr, move_constructor_with_ptr) {
+    auto otherPtr = new TestType();
+    auto otherSmartPtr = my::shared_ptr<TestType>(otherPtr);
+
+    EXPECT_CALL(*otherPtr, Destructor);
+
+    my::shared_ptr<TestType> smartPtr(std::move(otherSmartPtr));
+
+    EXPECT_EQ(smartPtr.get(), otherPtr);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, move_assignment_from_nullptr_to_nullptr) {
+    auto smartPtr = my::shared_ptr<TestType>();
+    auto otherSmartPtr = my::shared_ptr<TestType>();
+
+    smartPtr = std::move(otherSmartPtr);
+
+    EXPECT_EQ(smartPtr.get(), nullptr);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, move_assignment_from_nullptr_to_ptr) {
+    auto otherPtr = new TestType();
+    auto smartPtr = my::shared_ptr<TestType>();
+    auto otherSmartPtr = my::shared_ptr<TestType>(otherPtr);
+
+    EXPECT_CALL(*otherPtr, Destructor);
+
+    smartPtr = std::move(otherSmartPtr);
+
+    EXPECT_EQ(smartPtr.get(), otherPtr);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, move_assignment_from_ptr_to_nullptr) {
+    auto ptr = new TestType();
+    auto smartPtr = my::shared_ptr<TestType>(ptr);
+    auto otherSmartPtr = my::shared_ptr<TestType>();
+
+    EXPECT_CALL(*ptr, Destructor);
+
+    smartPtr = std::move(otherSmartPtr);
+
+    EXPECT_EQ(smartPtr.get(), nullptr);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, move_assignment_from_ptr_to_ptr) {
+    auto ptr = new TestType();
+    auto otherPtr = new TestType();
+    auto smartPtr = my::shared_ptr<TestType>(ptr);
+    auto otherSmartPtr = my::shared_ptr<TestType>(otherPtr);
+
+    EXPECT_CALL(*ptr, Destructor);
+    EXPECT_CALL(*otherPtr, Destructor);
+
+    smartPtr = std::move(otherSmartPtr);
+
+    EXPECT_EQ(smartPtr.get(), otherPtr);
+    EXPECT_EQ(otherSmartPtr.get(), nullptr);
+}
+
+TEST(shared_ptr, reset) {
+    auto ptr = new int{5};
+    auto smartPtr = my::shared_ptr<int>(ptr);
+
+    smartPtr.reset();
+    EXPECT_EQ(smartPtr.get(), nullptr);
+
+    smartPtr.reset(nullptr);
+    EXPECT_EQ(smartPtr.get(), nullptr);
+
+    auto otherPtr = new int{6};
+    smartPtr.reset(otherPtr);
+    EXPECT_EQ(smartPtr.get(), otherPtr);
+}
+
+TEST(shared_ptr, star_operator) {
+    auto ptr = new int{5};
+    auto smartPtr = my::shared_ptr<int>(ptr);
+
+    EXPECT_EQ(*ptr, *smartPtr);
+}
+
+TEST(shared_ptr, arrow_operator) {
+    struct Foo {
+        int foo() { return 5; }
+    };
+    auto ptr = new Foo{};
+    my::shared_ptr<Foo> fooPtr{ptr};
+
+    EXPECT_EQ(fooPtr->foo(), 5);
+}
+
+TEST(shared_ptr, use_count) {
+    auto smartPtr = my::shared_ptr<int>();
+
+    EXPECT_EQ(smartPtr.use_count(), 1);
+
+    auto otherPtr = new int{5};
+    auto otherSmartPtr = my::shared_ptr<int>(otherPtr);
+
+    smartPtr = otherSmartPtr;
+
+    EXPECT_EQ(smartPtr.use_count(), 2);
+    EXPECT_EQ(otherSmartPtr.use_count(), 2);
+
+    smartPtr = std::move(otherSmartPtr);
+
+    EXPECT_EQ(smartPtr.use_count(), 1);
+    // EXPECT_EQ(otherSmartPtr.use_count(), 1);  // -> should cause segmentation fault
+}
+
+TEST(shared_ptr, bool) {
+    auto ptr = new int{5};
+    auto smartPtr = my::shared_ptr<int>(ptr);
+
+    EXPECT_EQ(bool(smartPtr), true);
+
+    smartPtr.reset();
+
+    EXPECT_EQ(bool(smartPtr), false);
+}
