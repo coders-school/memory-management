25ae70c257100191dc3277e108767e1b944f2e2a
Michal Soloducha <michal.soloducha@snom.com>
2023-04-21 17:12:26 +0200

    Fix reset()

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -105,8 +105,21 @@ public:
     }
 
     void reset(Type* pointer = nullptr) noexcept {
-        delete pointer_;
+        control_block_pointer_->shared_refs--;
+        // while overwriting an object we may to erase the last object pointing to a resource
+        if (control_block_pointer_->shared_refs == 0) {
+            if (control_block_pointer_->weak_refs == 0) {
+                delete control_block_pointer_;
+                control_block_pointer_ = nullptr;
+            }
+            if (pointer_) {
+                delete pointer_;
+                pointer_ = nullptr;
+            }
+        }
         pointer_ = pointer;
+        control_block_pointer_ = new ControlBlock;
+        control_block_pointer_->shared_refs++;
     }
 
     Type& operator*() const noexcept {
