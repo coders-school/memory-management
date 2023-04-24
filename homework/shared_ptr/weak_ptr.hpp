64345882793a45c9fc8b75665700462113ed38d1
Michal Soloducha <michal.soloducha@snom.com>
2023-04-24 14:03:45 +0200

    New version - fixed constructors, tests, CMakeLists.txt, - changed shared_refs increments/decrements to weak_refs

--- homework/shared_ptr/weak_ptr.hpp
+++ homework/shared_ptr/weak_ptr.hpp
@@ -1,76 +1,46 @@
 #pragma once
 #include <atomic>
 #include <cstddef>
+#include "shared_ptr.hpp"
 
 namespace my {
-
-// void deleter() {}
-
-struct ControlBlock {
-    std::atomic<size_t> shared_refs{0};
-    std::atomic<size_t> weak_refs{0};
-    // void (*deleter_pointer)(int){nullptr};
-};
 
 template <typename Type>
 class weak_ptr {
 public:
-    weak_ptr()
-        : pointer_(nullptr), control_block_pointer_(new ControlBlock) {
-        control_block_pointer_->shared_refs++;
-    }
+    weak_ptr() = default;
 
     explicit weak_ptr(Type* pointer)
-        : pointer_(pointer), control_block_pointer_(new ControlBlock) {
-        control_block_pointer_->shared_refs++;
+        : pointer_(pointer), control_block_pointer_(control_block_pointer_) {
+        control_block_pointer_->weak_refs++;
     }
 
     ~weak_ptr() noexcept {
-        if (control_block_pointer_) {  // delete only for objects with allocated control block, i.e. not for moved objects
-            control_block_pointer_->shared_refs--;
-            if (control_block_pointer_->shared_refs == 0) {
-                if (control_block_pointer_->weak_refs == 0) {
-                    if (control_block_pointer_) {
-                        delete control_block_pointer_;
-                        control_block_pointer_ = nullptr;
-                    }
-                }
-                if (pointer_) {
-                    delete pointer_;
-                    pointer_ = nullptr;
-                }
-            }
+        if (control_block_pointer_) {  // address only objects with allocated control block, i.e. not for moved objects
+            control_block_pointer_->weak_refs--;
         }
     }
 
     // copy constructor
     weak_ptr(const weak_ptr& other)
         : pointer_(other.pointer_), control_block_pointer_(other.control_block_pointer_) {
-        control_block_pointer_->shared_refs++;
+        control_block_pointer_->weak_refs++;
     }
 
     // copy assignment operator
     weak_ptr& operator=(const weak_ptr& other) {
-        control_block_pointer_->shared_refs--;
-        // while overwriting an object we may to erase the last object pointing to a resource
-        if (control_block_pointer_->shared_refs == 0) {
-            if (control_block_pointer_->weak_refs == 0) {
-                delete control_block_pointer_;
-                control_block_pointer_ = nullptr;
-            }
-            if (pointer_) {
-                delete pointer_;
-                pointer_ = nullptr;
-            }
-        }
+        control_block_pointer_->weak_refs--;
+
         pointer_ = other.pointer_;
         control_block_pointer_ = other.control_block_pointer_;
-        control_block_pointer_->shared_refs++;
+        control_block_pointer_->weak_refs++;
         return *this;
     }
 
     // move constructor
     weak_ptr(weak_ptr&& other) noexcept {
+        control_block_pointer_->weak_refs--;
+
         pointer_ = nullptr;
         control_block_pointer_ = nullptr;
         std::swap(pointer_, other.pointer_);
@@ -79,18 +49,8 @@ public:
 
     // move assignment operator
     weak_ptr& operator=(weak_ptr&& other) noexcept {
-        control_block_pointer_->shared_refs--;
-        // while overwriting an object we may to erase the last object pointing to a resource
-        if (control_block_pointer_->shared_refs == 0) {
-            if (control_block_pointer_->weak_refs == 0) {
-                delete control_block_pointer_;
-                control_block_pointer_ = nullptr;
-            }
-            if (pointer_) {
-                delete pointer_;
-                pointer_ = nullptr;
-            }
-        }
+        control_block_pointer_->weak_refs--;
+
         pointer_ = nullptr;
         control_block_pointer_ = nullptr;
         std::swap(pointer_, other.pointer_);
@@ -103,21 +63,11 @@ public:
     }
 
     void reset(Type* pointer = nullptr) noexcept {
-        control_block_pointer_->shared_refs--;
-        // while overwriting an object we may to erase the last object pointing to a resource
-        if (control_block_pointer_->shared_refs == 0) {
-            if (control_block_pointer_->weak_refs == 0) {
-                delete control_block_pointer_;
-                control_block_pointer_ = nullptr;
-            }
-            if (pointer_) {
-                delete pointer_;
-                pointer_ = nullptr;
-            }
-        }
+        control_block_pointer_->weak_refs--;
+
         pointer_ = pointer;
         control_block_pointer_ = new ControlBlock;
-        control_block_pointer_->shared_refs++;
+        control_block_pointer_->weak_refs++;
     }
 
     size_t use_count() const noexcept {
