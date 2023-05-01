f62ca79bd264a47ae9a7a76f86d256d07139822f
Dario Github <dario.github@wp.pl>
2023-05-01 19:08:29 +0200

    Added posibility to pass raw pointer

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -1,10 +1,47 @@
 #include <iostream>
 
+/*
+shared_ptr
+(50 XP) Implement your own shared_ptr (simplified).
+
+shared_ptr is a RAII class:
+
+V   Holds a pointer to managed object (template class)
+Holds a pointer to shared control block with 2 counters and a deleter:
+shared_refs count (as std::atomic<size_t>)
+weak_refs count (as std::atomic<size_t>)
+deleter (function pointer)
+Constructor copies a pointer and allocate the control block
+Destructor decrease shared_refs and:
+if shared_refs == 0 -> release the managed object
+if shared_refs == 0 and weak_refs == 0 -> release the control block
+Copying is allowed - it increments shared_refs
+Moving is allowed and it means:
+Copying original pointers to a new object
+Setting source pointer to nullptr
+Member functions: operator*(), operator->(), get(), reset(), use_count(), operator bool()
+Should be implemented in shared_ptr.hpp file inside my namespace
+Tests should be written inside shared_ptr_tests.cpp using GoogleTest or Catch2
+You should instantiate shared_ptr template class in shared_ptr_tests.cpp above your test cases, e.g. template class my::shared_ptr<int>; It's needed for code coverage check to work properly.
+Do not forget about CI - UT + Valgrind / ASAN. Work in pairs.
+
+Read the notes on cppreference.com
+*/
+#include <iostream>
+
 namespace my {
+
+void print_msg(const std::string& msg)
+{
+    std::cout << static_cast<std::string>(msg) << std::endl;
+}
 
 template <class T>
 class shared_ptr {
+    T* ptr_ = nullptr;
 public:
+    shared_ptr(T* ptr) : ptr_{ptr} { print_msg("constructor shared_ptr"); }
+    ~shared_ptr() { print_msg("destructor ~shared_ptr"); } 
 };
 
 }  // namespace my
--- homework/shared_ptr/shared_ptr_tests.cpp
+++ homework/shared_ptr/shared_ptr_tests.cpp
@@ -17,3 +17,39 @@ TEST(HelloTest, BasicAssertions) {
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
 }
+
+class SharedPtrClassTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if their bodies would
+  // be empty.
+
+  SharedPtrClassTest() {
+     // You can do set-up work for each test here.
+  }
+
+  ~SharedPtrClassTest() override {
+     // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  void SetUp() override {
+     // Code here will be called immediately after the constructor (right
+     // before each test).
+  }
+
+  void TearDown() override {
+     // Code here will be called immediately after each test (right
+     // before the destructor).
+  }
+
+  // Class members declared here can be used by all tests in the test suite
+  // for Foo.
+};
+
+TEST_F(SharedPtrClassTest, whenCreated_thenPossibleToDelete) {
+  my::shared_ptr<int> shared_ptr{nullptr};
+}
+
+
