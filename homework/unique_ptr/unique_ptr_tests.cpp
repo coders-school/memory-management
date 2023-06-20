fd8739ba52f28164fff417e9c0ece124ce23fbcc
BoskiJanusz <37317957+BoskiJanusz@users.noreply.github.com>
2023-06-20 15:36:59 +0200

    Update unique_ptr_tests.cpp

--- homework/unique_ptr/unique_ptr_tests.cpp
+++ homework/unique_ptr/unique_ptr_tests.cpp
@@ -13,18 +13,53 @@ struct TestObject {
     }
 };
 
-TEST(unique_ptrTest, ArrowOperatorTest) {
+TEST(unique_ptrTest, ArrowOperatorToFunctionTest) {
     my::unique_ptr<TestObject> unique_ptr(new TestObject{20});
     auto expected = 40;
+    auto value = unique_ptr->multiplyValueByTwo();
 
-    EXPECT_EQ(expected, unique_ptr->multiplyValueByTwo());
+    EXPECT_EQ(expected, value);
 }
 
-TEST(unique_ptrTest, DereferenceOperatorTest) {
+TEST(unique_ptrTest, ArrowOperatorToFunctionMemberValueEqualsZeroTest) {
+    my::unique_ptr<TestObject> unique_ptr(new TestObject{0});
+    auto expected = 0;
+    auto value = unique_ptr->multiplyValueByTwo();
+
+    EXPECT_EQ(expected, value);
+}
+
+TEST(unique_ptrTest, ArrowOperatorToMemberValueTest) {
+    my::unique_ptr<TestObject> unique_ptr(new TestObject{20});
+    auto expected = 20;
+    auto value = unique_ptr->value_;
+
+    EXPECT_EQ(expected, value);
+}
+
+TEST(unique_ptrTest, DereferenceOperatorToMemberFunctionTest) {
     my::unique_ptr<TestObject> unique_ptr(new TestObject(20));
     TestObject& dereferencedObj = *unique_ptr;
     auto result = dereferencedObj.multiplyValueByTwo();
     auto expected = 40;
+
+    EXPECT_EQ(expected, result);
+}
+
+TEST(unique_ptrTest, DereferenceOperatorToMemberFunctionEqualsZeroTest) {
+    my::unique_ptr<TestObject> unique_ptr(new TestObject(0));
+    TestObject& dereferencedObj = *unique_ptr;
+    auto result = dereferencedObj.multiplyValueByTwo();
+    auto expected = 0;
+
+    EXPECT_EQ(expected, result);
+}
+
+TEST(unique_ptrTest, DereferenceOperatorToMemberValueTest) {
+    my::unique_ptr<TestObject> unique_ptr(new TestObject(20));
+    TestObject& dereferencedObj = *unique_ptr;
+    auto result = dereferencedObj.value_;
+    auto expected = 20;
 
     EXPECT_EQ(expected, result);
 }
@@ -46,6 +81,14 @@ TEST(unique_ptrTest, FunctionGetExpectedValueTest) {
     EXPECT_EQ(expected, result);
 }
 
+TEST(unique_ptrTest, FunctionGetExpectedNullValueTest) {
+    my::unique_ptr<TestObject> unique_ptr;
+    TestObject* rawPtr = unique_ptr.get();
+    auto expected = nullptr;
+
+    EXPECT_EQ(expected, rawPtr);
+}
+
 TEST(unique_ptrTest, DefaultConstructorTest) {
     my::unique_ptr<TestObject> unique_ptr;
     auto value = unique_ptr.get();
@@ -54,11 +97,38 @@ TEST(unique_ptrTest, DefaultConstructorTest) {
     EXPECT_EQ(expected, value);
 }
 
-TEST(unique_ptrTest, FunctionResetUniquePtr1EqualsUniquePtr2) {
+TEST(unique_ptrTest, FunctionResetUniquePtr1EqualsNewPtr) {
     my::unique_ptr<TestObject> unique_ptr1(new TestObject(20));
     unique_ptr1.reset(new TestObject(30));
     auto value = unique_ptr1->value_;
     auto expected = 30;
+
+    EXPECT_EQ(value, expected);
+}
+
+TEST(unique_ptrTest, FunctionResetUniquePtr1EqualsNull) {
+    my::unique_ptr<TestObject> unique_ptr1(new TestObject(20));
+    unique_ptr1.reset(nullptr);
+    auto value = unique_ptr1.get();
+    auto expected = nullptr;
+
+    EXPECT_EQ(value, expected);
+}
+
+TEST(unique_ptrTest, FunctionResetUniquePtr1NotEqualsZero) {
+    my::unique_ptr<int> unique_ptr1(new int{10});
+    unique_ptr1.reset(new int{0});
+    auto value = unique_ptr1.get();
+    auto notExpected = nullptr;
+
+    EXPECT_NE(value, notExpected);
+}
+
+TEST(unique_ptrTest, FunctionResetUniquePtr1EqualsZero) {
+    my::unique_ptr<TestObject> unique_ptr1(new TestObject(20));
+    unique_ptr1.reset(new TestObject(0));
+    auto value = unique_ptr1->value_;
+    auto expected = 0;
 
     EXPECT_EQ(value, expected);
 }
