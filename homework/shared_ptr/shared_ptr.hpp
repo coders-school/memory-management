7271ac810f085ff8920d7d3203b810910b30e456
BoskiJanusz <37317957+BoskiJanusz@users.noreply.github.com>
2023-07-05 21:21:02 +0200

    Update shared_ptr.hpp

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -1,36 +1,49 @@
 #pragma once
+#include "weak_ptr.hpp"
 
 namespace my {
+template <class Y>
+class weak_ptr;
 template <class T>
 class shared_ptr {
-private:
+protected:
     T* ptr_ = nullptr;
-    size_t* counter_ = nullptr;
+    size_t* shared_refs_ = nullptr;
 
 public:
     shared_ptr()
-        : ptr_(nullptr), counter_(new size_t{0}) {}
+        : ptr_(nullptr), shared_refs_(new size_t{0}) {}
     shared_ptr(T* ptr)
-        : ptr_(ptr), counter_(new size_t{1}) {}
+        : ptr_(ptr), shared_refs_(new size_t{1}) {}
     shared_ptr(const shared_ptr& obj)
-        : ptr_(obj.ptr_), counter_(obj.counter_) {
+        : ptr_(obj.ptr_), shared_refs_(obj.shared_refs_) {
         if (obj.ptr_) {
-            (*counter_)++;
+            (*shared_refs_)++;
         }
     }
     shared_ptr(shared_ptr&& obj)
-        : ptr_(obj.ptr_), counter_(obj.counter_) {
+        : ptr_(obj.ptr_), shared_refs_(obj.shared_refs_) {
         obj.ptr_ = nullptr;
-        obj.counter_ = nullptr;
+        obj.shared_refs_ = nullptr;
+    }
+    shared_ptr(const weak_ptr<T>& obj)
+        : ptr_(obj.ptr_), shared_refs_(obj.shared_refs_) {
+        if (obj.ptr_) {
+            if (obj.shared_refs_) {
+                (*shared_refs_)++;
+            } else {
+                shared_refs_ = new size_t{1};
+            }
+        }
     }
     shared_ptr& operator=(const shared_ptr& obj) {
         if (this != &obj) {
             if (ptr_ != obj.ptr_) {
                 deleter();
                 ptr_ = obj.ptr_;
-                counter_ = obj.counter_;
+                shared_refs_ = obj.shared_refs_;
                 if (obj.ptr_) {
-                    (*counter_)++;
+                    (*shared_refs_)++;
                 }
             }
         }
@@ -40,9 +53,9 @@ public:
         if (this != &obj) {
             deleter();
             ptr_ = obj.ptr_;
-            counter_ = obj.counter_;
+            shared_refs_ = obj.shared_refs_;
             obj.ptr_ = nullptr;
-            obj.counter_ = nullptr;
+            obj.shared_refs_ = nullptr;
         }
         return *this;
     }
@@ -68,23 +81,23 @@ public:
             delete tempPtr;
             tempPtr = nullptr;
         }
-        if (*(counter_)) {
-            (*counter_)--;
+        if (*(shared_refs_)) {
+            (*shared_refs_)--;
         }
     }
     void reset() {
         if (ptr_) {
             delete ptr_;
             ptr_ = nullptr;
-            if (counter_) {
-                if (*(counter_)) {
-                    (*counter_)--;
+            if (shared_refs_) {
+                if (*(shared_refs_)) {
+                    (*shared_refs_)--;
                 }
             }
         }
     }
     long use_count() const {
-        return *counter_;
+        return *shared_refs_;
     }
     ~shared_ptr() {
         deleter();
@@ -92,19 +105,21 @@ public:
 
 private:
     void deleter() {
-        if (counter_) {
-            if (*(counter_) != 0) {
-                (*counter_)--;
+        if (shared_refs_) {
+            if (*(shared_refs_) != 0) {
+                (*shared_refs_)--;
             }
-            if ((*counter_) == 0) {
+            if ((*shared_refs_) == 0) {
                 if (ptr_) {
                     delete ptr_;
                     ptr_ = nullptr;
                 }
-                delete counter_;
-                counter_ = nullptr;
+                delete shared_refs_;
+                shared_refs_ = nullptr;
             }
         }
     }
+    template <class Y>
+    friend class weak_ptr;
 };
 }  // namespace my
