680c2d8b89b3f0b8472b776b88c1ea629355a8c1
paulaLes <paula.lesiczka@gmail.com>
2024-06-09 16:14:28 +0200

    Update shared_ptr.hpp

--- homework/shared_ptr/shared_ptr.hpp
+++ homework/shared_ptr/shared_ptr.hpp
@@ -1,7 +1,8 @@
-#ifndef MY_SHARED_PTR_HPP
-#define MY_SHARED_PTR_HPP
+#ifndef SHARED_PTR_HPP
+#define SHARED_PTR_HPP
 
 #include <atomic>
+#include <utility>
 #include <functional>
 
 namespace my {
@@ -9,118 +10,157 @@ namespace my {
 template <typename T>
 class shared_ptr {
 public:
-    // Constructor
-    explicit shared_ptr(T* ptr = nullptr) : ptr_(ptr), control_block_(nullptr) {
-        if (ptr) {
-            control_block_ = new control_block();
-            control_block_->shared_refs.store(1);
-            control_block_->weak_refs.store(0);
-            control_block_->deleter = [](T* ptr) { delete ptr; };
-        }
-    }
-
-    // Copy constructor
-    shared_ptr(const shared_ptr& other) : ptr_(other.ptr_), control_block_(other.control_block_) {
-        if (control_block_) {
-            control_block_->shared_refs.fetch_add(1);
-        }
-    }
-
-    // Move constructor
-    shared_ptr(shared_ptr&& other) noexcept : ptr_(other.ptr_), control_block_(other.control_block_) {
-        other.ptr_ = nullptr;
-        other.control_block_ = nullptr;
-    }
-
+    // Constructors
+    explicit shared_ptr(T* ptr = nullptr);
+    shared_ptr(const shared_ptr& other);
+    shared_ptr(shared_ptr&& other) noexcept;
+    
     // Destructor
-    ~shared_ptr() {
-        release();
-    }
-
-    // Copy assignment operator
-    shared_ptr& operator=(const shared_ptr& other) {
-        if (this != &other) {
-            release();
-            ptr_ = other.ptr_;
-            control_block_ = other.control_block_;
-            if (control_block_) {
-                control_block_->shared_refs.fetch_add(1);
-            }
-        }
-        return *this;
-    }
-
-    // Move assignment operator
-    shared_ptr& operator=(shared_ptr&& other) noexcept {
-        if (this != &other) {
-            release();
-            ptr_ = other.ptr_;
-            control_block_ = other.control_block_;
-            other.ptr_ = nullptr;
-            other.control_block_ = nullptr;
-        }
-        return *this;
-    }
-
-    // Dereference operator
-    T& operator*() const {
-        return *ptr_;
-    }
-
-    // Member access operator
-    T* operator->() const {
-        return ptr_;
-    }
-
-    // Get raw pointer
-    T* get() const {
-        return ptr_;
-    }
-
-    // Reset pointer
-    void reset(T* ptr = nullptr) {
-        release();
-        if (ptr) {
-            ptr_ = ptr;
-            control_block_ = new control_block();
-            control_block_->shared_refs.store(1);
-            control_block_->weak_refs.store(0);
-            control_block_->deleter = [](T* ptr) { delete ptr; };
-        }
-    }
-
-    // Get use count
-    size_t use_count() const {
-        return control_block_ ? control_block_->shared_refs.load() : 0;
-    }
-
-    // Conversion to bool
-    operator bool() const {
-        return ptr_ != nullptr;
-    }
+    ~shared_ptr();
+    
+    // Assignment operators
+    shared_ptr& operator=(const shared_ptr& other);
+    shared_ptr& operator=(shared_ptr&& other) noexcept;
+    
+    // Member functions
+    T& operator*() const;
+    T* operator->() const;
+    T* get() const;
+    void reset(T* ptr = nullptr);
+    size_t use_count() const;
+    explicit operator bool() const;
 
 private:
-    struct control_block {
+    void release();
+
+    T* ptr_;
+    struct ControlBlock {
         std::atomic<size_t> shared_refs;
         std::atomic<size_t> weak_refs;
         std::function<void(T*)> deleter;
-    };
 
-    void release() {
-        if (control_block_) {
-            if (control_block_->shared_refs.fetch_sub(1) == 1) {
-                control_block_->deleter(ptr_);
-                if (control_block_->weak_refs.load() == 0) {
-                    delete control_block_;
-                }
+        ControlBlock(T* ptr, std::function<void(T*)> del)
+            : shared_refs(1), weak_refs(0), deleter(del) {}
+    } *control_;
+};
+
+// Constructor
+template <typename T>
+shared_ptr<T>::shared_ptr(T* ptr) : ptr_(ptr) {
+    if (ptr) {
+        control_ = new ControlBlock(ptr, [](T* p) { delete p; });
+    } else {
+        control_ = nullptr;
+    }
+}
+
+// Copy constructor
+template <typename T>
+shared_ptr<T>::shared_ptr(const shared_ptr& other)
+    : ptr_(other.ptr_), control_(other.control_) {
+    if (control_) {
+        control_->shared_refs++;
+    }
+}
+
+// Move constructor
+template <typename T>
+shared_ptr<T>::shared_ptr(shared_ptr&& other) noexcept
+    : ptr_(other.ptr_), control_(other.control_) {
+    other.ptr_ = nullptr;
+    other.control_ = nullptr;
+}
+
+// Destructor
+template <typename T>
+shared_ptr<T>::~shared_ptr() {
+    release();
+}
+
+// Copy assignment
+template <typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr& other) {
+    if (this != &other) {
+        release();
+        ptr_ = other.ptr_;
+        control_ = other.control_;
+        if (control_) {
+            control_->shared_refs++;
+        }
+    }
+    return *this;
+}
+
+// Move assignment
+template <typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr&& other) noexcept {
+    if (this != &other) {
+        release();
+        ptr_ = other.ptr_;
+        control_ = other.control_;
+        other.ptr_ = nullptr;
+        other.control_ = nullptr;
+    }
+    return *this;
+}
+
+// Dereference operator
+template <typename T>
+T& shared_ptr<T>::operator*() const {
+    return *ptr_;
+}
+
+// Arrow operator
+template <typename T>
+T* shared_ptr<T>::operator->() const {
+    return ptr_;
+}
+
+// Get raw pointer
+template <typename T>
+T* shared_ptr<T>::get() const {
+    return ptr_;
+}
+
+// Reset pointer
+template <typename T>
+void shared_ptr<T>::reset(T* ptr) {
+    release();
+    ptr_ = ptr;
+    if (ptr) {
+        control_ = new ControlBlock(ptr, [](T* p) { delete p; });
+    } else {
+        control_ = nullptr;
+    }
+}
+
+// Get use count
+template <typename T>
+size_t shared_ptr<T>::use_count() const {
+    return control_ ? control_->shared_refs.load() : 0;
+}
+
+// Bool conversion
+template <typename T>
+shared_ptr<T>::operator bool() const {
+    return ptr_ != nullptr;
+}
+
+// Release resources
+template <typename T>
+void shared_ptr<T>::release() {
+    if (control_) {
+        if (--control_->shared_refs == 0) {
+            control_->deleter(ptr_);
+            if (control_->weak_refs == 0) {
+                delete control_;
             }
         }
     }
-
-    T* ptr_;
-    control_block* control_block_;
-};
+    ptr_ = nullptr;
+    control_ = nullptr;
+}
 
 } // namespace my
 
-#endif // MY_SHARED_PTR_HPP
+#endif // SHARED_PTR_HPP
